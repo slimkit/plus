0247029362517b55dc0be4e7c5b494132e500cb2
zhiyicx <lovevipdsw@vip.qq.com>
2016-12-22 03:34:25 +0000

    Apply fixes from StyleCI

--- app/Http/Middleware/VerifyPhoneNumber.php
+++ app/Http/Middleware/VerifyPhoneNumber.php
@@ -12,8 +12,9 @@ class VerifyPhoneNumber
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure                 $next
+     *
      * @return mixed
      */
     public function handle($request, Closure $next)
