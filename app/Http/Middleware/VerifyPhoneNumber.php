e55749f9a93bcc95996d20ee84aaa73c3a1d5e44
Seven <shiweidu@outlook.com>
2016-12-22 11:34:04 +0800

    添加验证手机号码中间件～

---
+++ app/Http/Middleware/VerifyPhoneNumber.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+/**
+ * 验证手机号码
+ */
+class VerifyPhoneNumber
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        return $next($request);
+    }
+}
