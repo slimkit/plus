642b19876ce74a7e4a82f89eeef640ba390e321e
Seven <shiweidu@outlook.com>
2016-12-23 17:26:50 +0800

    添加阿里大于短信配置

---
+++ config/alidayu.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * 阿里大于短信信息配置
+ */
+
+return [
+    'app_key'    => env('ALIDATY_APP_KEY', null),
+    'app_secret' => env('ALIDAYU_APP_SECRET', null),
+    'sandbox'    => false,
+    'sign_name'  => env('ALIDAYU_FREE_SIGN_NAME', '智艺创想'),
+];
