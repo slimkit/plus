a2443bc65bf162c9086e274bfec0af084023b400
Seven <shiweidu@outlook.com>
2016-12-23 17:15:20 +0800

    修改验证码表结构～

--- database/migrations/2016_12_21_081206_create_verify_codes.php
+++ database/migrations/2016_12_21_081206_create_verify_codes.php
@@ -14,12 +14,13 @@ class CreateVerifyCodes extends Migration
     public function up()
     {
         Schema::create('verify_codes', function (Blueprint $table) {
-            $table->increments('id');
             $table->string('account')->comment('账户');
             $table->integer('code')->comment('验证码');
             $table->string('content')->nullable()->default('')->comment('内容');
             $table->string('data')->nullable()->default('')->comment('数据');
+            $table->tinyInteger('state')->nullable()->default(0)->comment('状态');
             $table->timestamps();
+            $table->softDeletes();
 
             $table->index('account');
         });
