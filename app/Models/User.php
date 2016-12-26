2ab235b97587df73681a7512742829b36dc4178a
Seven <shiweidu@outlook.com>
2016-12-26 13:42:32 +0800

    用户表增加软删除～

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,11 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -15,7 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'phone', 'password',
     ];
 
     /**
@@ -26,4 +28,20 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * 复用设置手机号查询条件方法.
+     *
+     * @param Builder $query 查询对象
+     * @param string $phone 手机号码
+     * 
+     * @return Builder 查询对象
+     * 
+     * @author Seven Du <shiweidu@outlook.com>
+     * @homepage http://medz.cn
+     */
+    public function scopeByPhone(Builder $query, string $phone): Builder
+    {
+        return $query->where('phone', $phone);
+    }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('password')->comment('password.');
             $table->rememberToken()->comment('user auth token.');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
