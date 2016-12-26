467ba81b7b981147cec8d7f5b1c259dbdd779411
boxshadow <qiaobin@zhiyicx.com>
2016-12-26 22:07:22 +0800

    增加附件表migration

---
+++ database/migrations/2016_12_26_084807_create_attachs.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAttachs extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attachs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->commit('原文件名');
+            $table->string('path')->commit('存储路径');
+            $table->string('hash')->uniqid()->commit('文件hash值');
+            $table->string('mime')->commit('mime类型');
+            $table->string('extension')->commit('文件扩展名');
+            $table->integer('width')->nullable()->default(0)->commit('图片宽度');
+            $table->integer('height')->nullable()->default(0)->commit('图片高度');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('attachs');
+    }
+}
