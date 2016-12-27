05c77f1e0d76387ff7f70bc94a9e3ea45f8faed9
Seven <shiweidu@outlook.com>
2016-12-27 19:40:40 +0800

    修正路由中间件设置～

--- routes/api.php
+++ routes/api.php
@@ -20,13 +20,14 @@ Route::group([
     // 获取手机验证码
     Route::post('/auth/get-phone-code', 'AuthController@getPhoneCode')
         ->middleware(App\Http\Middleware\VerifyPhoneNumber::class)
-        ->middleware(App\Http\Middleware\CheckUserByPhoneExisted::class);
+        ->middleware(App\Http\Middleware\VeridySendPhoneCodeType::class)
+    ;
 
     Route::post('/auth/register', 'AuthController@register')
         ->middleware(App\Http\Middleware\VerifyPhoneNumber::class) // 验证手机号码是否正确
         ->middleware(App\Http\Middleware\VerifyUserNameRole::class) // 验证用户名规则是否正确
-        ->middleware(App\Http\Middleware\CheckUserByNameExisted::class) // 验证用户名是否被占用
-        ->middleware(App\Http\Middleware\CheckUserByPhoneExisted::class) // 验证手机号码是否被占用
+        ->middleware(App\Http\Middleware\CheckUserByNameNotExisted::class) // 验证用户名是否被占用
+        ->middleware(App\Http\Middleware\CheckUserByPhoneNotExisted::class) // 验证手机号码是否被占用
         ->middleware(App\Http\Middleware\VerifyPhoneCode::class) // 验证验证码释放正确
-;
+    ;
 });
