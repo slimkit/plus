2eac58c1fc42798c5bd0c68f9c33dd6f26d61163
Seven <shiweidu@outlook.com>
2016-12-27 19:38:05 +0800

    添加用户名用户不存在中间件

---
+++ app/Http/Middleware/CheckUserByNameNotExisted.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Exceptions\MessageResponseBody;
+use App\Models\User;
+use Closure;
+use Illuminate\Http\Request;
+
+class CheckUserByNameNotExisted
+{
+    /**
+     * 从检查用户是否不存在中间件.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $name = $request->input('name');
+        $user = User::byName($name)->withTrashed()->first();
+
+        // 用户名已被使用
+        if ($user) {
+            return app(MessageResponseBody::class, [
+                'code' => 1004,
+            ]);
+        }
+
+        return $next($request);
+    }
+}
