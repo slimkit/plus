4482f1dda8abd47b92eaa664d189bffa4b422f03
Seven <shiweidu@outlook.com>
2016-12-27 19:37:45 +0800

    增加检查手机号用户是否不存在中间件

---
+++ app/Http/Middleware/CheckUserByPhoneNotExisted.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Exceptions\MessageResponseBody;
+use App\Models\User;
+use Closure;
+
+class CheckUserByPhoneNotExisted
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $phone = $request->input('phone');
+        $user = User::byPhone($phone)->withTrashed()->first();
+
+        // 手机号已被使用
+        if ($user) {
+            return app(MessageResponseBody::class, [
+                'code' => 1010,
+            ]);
+        }
+
+        return $next($request);
+    }
+}
