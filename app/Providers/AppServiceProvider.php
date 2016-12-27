9d0927038c38eaf9b4c1628dc8dbeb257bcf78d6
Seven <shiweidu@outlook.com>
2016-12-27 14:54:42 +0800

    添加用户名验证规则.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,6 +18,11 @@ class AppServiceProvider extends ServiceProvider
         Validator::extend('cn_phone', function ($attribute, $value) {
             return preg_match('/^(\+?0?86\-?)?((13\d|14[57]|15[^4,\D]|17[678]|18\d)\d{8}|170[059]\d{7})$/', $value);
         });
+
+        // 添加用户名验证规则
+        Validator::extend('username', function ($attribute, $value) {
+            return preg_match('/[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*/', $value);
+        });
     }
 
     /**
