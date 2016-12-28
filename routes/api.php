0d1fc24108c464ee74ae65434ede54dae4f814fb
Seven <shiweidu@outlook.com>
2016-12-28 16:42:32 +0800

    修正路由

--- routes/api.php
+++ routes/api.php
@@ -19,8 +19,9 @@ Route::group([
 
     // 获取手机验证码
     Route::post('/auth/get-phone-code', 'AuthController@getPhoneCode')
-        ->middleware(App\Http\Middleware\VerifyPhoneNumber::class)
-        ->middleware(App\Http\Middleware\VeridySendPhoneCodeType::class);
+        ->middleware(App\Http\Middleware\VerifyPhoneNumber::class) // 验证手机号码是否正确
+        ->middleware(App\Http\Middleware\VeridySendPhoneCodeType::class) // 验证发送验证码类型等集合
+;
 
     Route::post('/auth/register', 'AuthController@register')
         ->middleware(App\Http\Middleware\VerifyPhoneNumber::class) // 验证手机号码是否正确
@@ -28,9 +29,11 @@ Route::group([
         ->middleware(App\Http\Middleware\CheckUserByNameNotExisted::class) // 验证用户名是否被占用
         ->middleware(App\Http\Middleware\CheckUserByPhoneNotExisted::class) // 验证手机号码是否被占用
         ->middleware(App\Http\Middleware\VerifyPhoneCode::class) // 验证验证码释放正确
-;
+    ;
+
     Route::post('/auth/login', 'AuthController@login')
         ->middleware(App\Http\Middleware\VerifyPhoneNumber::class) // 验证手机号码是否正确
         ->middleware(App\Http\Middleware\CheckUserByPhoneExisted::class) // 验证手机号码用户是否存在
-        ->middleware(App\Http\Middleware\VerifyPassword::class);
+        ->middleware(App\Http\Middleware\VerifyPassword::class) // 验证密码是否正确
+    ;
 });
