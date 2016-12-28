f76206f34b04b2211917bb5d91251babc4f31f27
Seven <shiweidu@outlook.com>
2016-12-28 16:59:31 +0800

    添加路由注释

--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,7 @@ Route::group([
         ->middleware(App\Http\Middleware\VeridySendPhoneCodeType::class) // 验证发送验证码类型等集合
 ;
 
+    // 用户注册
     Route::post('/auth/register', 'AuthController@register')
         ->middleware(App\Http\Middleware\VerifyPhoneNumber::class) // 验证手机号码是否正确
         ->middleware(App\Http\Middleware\VerifyUserNameRole::class) // 验证用户名规则是否正确
@@ -31,6 +32,7 @@ Route::group([
         ->middleware(App\Http\Middleware\VerifyPhoneCode::class) // 验证验证码释放正确
 ;
 
+    // 用户登录
     Route::post('/auth/login', 'AuthController@login')
         ->middleware(App\Http\Middleware\VerifyPhoneNumber::class) // 验证手机号码是否正确
         ->middleware(App\Http\Middleware\CheckUserByPhoneExisted::class) // 验证手机号码用户是否存在
