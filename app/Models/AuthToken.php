0991a98f2ba56f7905d1af22e9901bc3c79eaecf
Seven <shiweidu@outlook.com>
2016-12-29 17:15:55 +0800

    authtoken模型增加软的删除

--- app/Models/AuthToken.php
+++ app/Models/AuthToken.php
@@ -4,9 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class AuthToken extends Model
 {
+    use SoftDeletes;
+
     // 关联users表
     public function user()
     {
