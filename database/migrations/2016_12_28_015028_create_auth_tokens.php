6a73451452fe13399a6ba6b3e932b4d41f24604a
Seven <shiweidu@outlook.com>
2016-12-29 15:14:55 +0800

    修复迁移表问题～

--- database/migrations/2016_12_28_015028_create_auth_tokens.php
+++ database/migrations/2016_12_28_015028_create_auth_tokens.php
@@ -19,7 +19,7 @@ class CreateAuthTokens extends Migration
             $table->string('refresh_token', 255)->comment('刷新token');
             $table->integer('user_id')->comment('token所属用户id');
             $table->integer('expires')->nullable()->default(0)->comment('token生命周期');
-            $table->string('device_code')->
+            $table->string('device_code')->nullable()->default('')->comment('设备码');
             $table->timestamps();
             $table->softDeletes();
             $table->index('token');
