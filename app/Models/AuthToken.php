dd2375ffbec0bffa03046be7549a092af908b5db
Seven <shiweidu@outlook.com>
2016-12-29 17:06:31 +0800

    增加查找条件依照token的复用方法.

--- app/Models/AuthToken.php
+++ app/Models/AuthToken.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class AuthToken extends Model
@@ -10,5 +11,19 @@ class AuthToken extends Model
     public function user()
     {
         return $this->hasOne(User::class, 'user_id', 'id');
+    }
+
+    /**
+     * 查找条件依照token的复用方法.
+     *
+     * @param Builder $query 查询对象
+     * @param string $token Token值
+     * @return Builder 查询对象
+     * @author Seven Du <shiweidu@outlook.com>
+     * @homepage http://medz.cn
+     */
+    public function scopeByToken(Builder $query, $token)
+    {
+        return $query->where('token', $token);
     }
 }
