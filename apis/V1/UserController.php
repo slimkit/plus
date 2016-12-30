c0eba5a726ee66d3b5512de3c5fbe54b908536df
Seven <shiweidu@outlook.com>
2016-12-30 16:36:04 +0800

    增加用户密码修改接口～

--- apis/V1/UserController.php
+++ apis/V1/UserController.php
@@ -8,6 +8,14 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    /**
+     * 修改用户密码.
+     *
+     * @param Request $request 请求对象
+     * @return mixed
+     * @author Seven Du <shiweidu@outlook.com>
+     * @homepage http://medz.cn
+     */
     public function resetPassword(Request $request)
     {
         $password = $request->input('new_password', '');
