4a970415dd082c7ee88b1188f4dbbc809bb7a493
Seven <shiweidu@outlook.com>
2016-12-30 16:15:04 +0800

    增加user控制器

---
+++ apis/V1/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\APIs\V1;
+
+use App\Http\Controllers\Controller;
+use App\Exceptions\MessageResponseBody;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function resetPassword(Request $request)
+    {
+        $password = $request->input('new_password', '');
+        $user = $request->attributes->get('user');
+        $user->createPassword($password);
+        $user->save();
+
+        return app(MessageResponseBody::class, [
+            'status' => true,
+        ])->setStatusCode(201);
+    }
+}
