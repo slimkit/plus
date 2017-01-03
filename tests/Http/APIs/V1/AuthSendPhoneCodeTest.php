b1dc0cf117435885ef291e83ad0f94689ea3c260
Seven <shiweidu@outlook.com>
2017-01-03 15:24:15 +0800

    完成【错误请求手机号码】单测

--- tests/Http/APIs/V1/AuthSendPhoneCodeTest.php
+++ tests/Http/APIs/V1/AuthSendPhoneCodeTest.php
@@ -2,8 +2,40 @@
 
 namespace Ts\Test\Http\APIs\V1;
 
-use Ts\Test\TestCase;
-
 class AuthSendPhoneCodeTest extends TestCase
 {
+    /**
+     * API uri.
+     *
+     * @var string
+     */
+    protected $uri = '/api/v1/auth/phone/send-code';
+
+    /**
+     * 测试错误的请求手机号验证.
+     * Message code: 1000.
+     *
+     * @return [type] [description]
+     * @author Seven Du <shiweidu@outlook.com>
+     * @homepage http://medz.cn
+     */
+    public function testPostErrorPhoneNumber()
+    {
+        // create request data.
+        $requestBody = [
+            'phone' => '*****',
+        ];
+
+        // request api, send data.
+        $this->postJson($this->uri, $requestBody);
+
+        // Asserts that the status code of the response matches the given code.
+        $this->seeStatusCode(403);
+
+        // Assert that the response contains an exact JSON array.
+        $json = $this->createMessageResponseBody([
+            'code' => 1000,
+        ]);
+        $this->seeJsonEquals($json);
+    }
 }
