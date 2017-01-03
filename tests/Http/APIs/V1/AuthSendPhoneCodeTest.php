e0a321179ffdf5ce27ba296227f24c54bd9fdce1
Seven <shiweidu@outlook.com>
2017-01-03 15:31:53 +0800

    发送验证码增加【错误的获取类型】报错单元测试

--- tests/Http/APIs/V1/AuthSendPhoneCodeTest.php
+++ tests/Http/APIs/V1/AuthSendPhoneCodeTest.php
@@ -13,9 +13,8 @@ class AuthSendPhoneCodeTest extends TestCase
 
     /**
      * 测试错误的请求手机号验证.
-     * Message code: 1000.
-     *
-     * @return [type] [description]
+     * 
+     * Message code: 1000
      *
      * @author Seven Du <shiweidu@outlook.com>
      * @homepage http://medz.cn
@@ -36,6 +35,32 @@ class AuthSendPhoneCodeTest extends TestCase
         // Assert that the response contains an exact JSON array.
         $json = $this->createMessageResponseBody([
             'code' => 1000,
+        ]);
+        $this->seeJsonEquals($json);
+    }
+
+    /**
+     * 测试错误的发送类型返回数据.
+     * 
+     * Message code: 1011
+     *
+     * @author Seven Du <shiweidu@outlook.com>
+     * @homepage http://medz.cn
+     */
+    public function testSendType()
+    {
+        $requestBody = [
+            'phone' => '18781993583',
+        ];
+
+        $this->postJson($this->uri, $requestBody);
+
+        // Asserts that the status code of the response matches the given code.
+        $this->seeStatusCode(403);
+
+        // Assert that the response contains an exact JSON array.
+        $json = $this->createMessageResponseBody([
+            'code' => 1011,
         ]);
         $this->seeJsonEquals($json);
     }
