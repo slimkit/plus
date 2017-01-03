7639994042aa2d3a93f2b127adc9ac5b9b86a03b
boxshadow <idafoo@sina.com>
2017-01-03 19:04:09 +0800

    增加数据表
    增加超级管理员表
    增加用户字段纵向拓展表

---
+++ database/migrations/2017_01_03_082418_create_user_profile_settings.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserProfileSettings extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_profile_settings', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->integer('create_user_id')->comment('创建者uid');
+            $table->string('profile_name', 10)->comment('资料名称');
+            $table->string('type', 20)->nullable()->default('input')->comment('资料类型');
+            $table->tinyInteger('required')->nullable()->default(0)->comment('是否必填');
+            $table->tinyInteger('is_delable')->nullable()->default(0)->comment('是否可被删除0:不可被删除, 1:可被删除');
+            $table->tinyInteger('state')->nullable()->default(1)->comment('状态,1:在用，0:停用');
+            $table->mediumText('default_options')->nullable()->comment('预设选项：select,checkbox,radio类型的资料必填');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_profile_settings');
+    }
+}
