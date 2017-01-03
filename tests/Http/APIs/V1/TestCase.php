efe6116637d6ebf8554bb5a981d3b053511f8c91
Seven <shiweidu@outlook.com>
2017-01-03 15:23:43 +0800

    完成v1版本API基础复用～

---
+++ tests/Http/APIs/V1/TestCase.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Ts\Test\Http\APIs\V1;
+
+use Ts\Test\TestCase as BaseTestCase;
+use App\Exceptions\MessageResponseBody;
+
+class TestCase extends BaseTestCase
+{
+    /**
+     * 创建MessageResponseBody对象数据体.
+     *
+     * @param array $body 数据
+     * @return array 创建的数据
+     * @author Seven Du <shiweidu@outlook.com>
+     * @homepage http://medz.cn
+     */
+    protected function createMessageResponseBody(array $body): array
+    {
+        return app(MessageResponseBody::class, $body)->getBody();
+    }
+}
