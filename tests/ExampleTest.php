daf655b28edbb28adc38f64dd0c010c19c23caf8
Seven <shiweidu@outlook.com>
2017-01-03 15:23:55 +0800

    删除示例测试

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,8 +2,6 @@
 
 namespace Ts\Test;
 
-use App\Exceptions\MessageResponseBody;
-
 class ExampleTest extends TestCase
 {
     /**
@@ -15,13 +13,5 @@ class ExampleTest extends TestCase
     {
         $this->visit('/')
              ->see('Laravel');
-    }
-
-    public function testApiAuthGetPhoneCode()
-    {
-        $this->postJson('/api/v1/auth/phone/send-code')
-             ->seeJson(app(MessageResponseBody::class, [
-                'code' => 1000,
-            ])->getBody());
     }
 }
