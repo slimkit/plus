4e2b1b6f1b62cb0e4269d8c0802c9277695925fe
Seven <shiweidu@outlook.com>
2017-01-03 18:47:51 +0800

    添加测试组件～

---
+++ resources/views/tets.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>test</title>
+    <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
+</head>
+<body>
+    <div id="app">
+        <example></example>
+    </div>
+<script type="text/javascript" src="{{ elixir('js/app.js') }}"></script>
+</body>
+</html>
