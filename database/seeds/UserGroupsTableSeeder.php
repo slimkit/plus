a4f11847b3208e322b83b51715217f5e15bdb8e4
boxshadow <idafoo@sina.com>
2017-01-05 15:55:50 +0800

    增加seeder
    用户组默认seeder增加

---
+++ database/seeds/UserGroupsTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Models\UserGroup;
+use Illuminate\Database\Seeder;
+
+class UserGroupsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        UserGroup::create(['name' => '管理员']);
+        UserGroup::create(['name' => '普通用户']);
+    }
+}
