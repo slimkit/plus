cec7d355f2177e8cbf2a8382ee2a744457847b3c
boxshadow <idafoo@sina.com>
2017-01-05 15:58:08 +0800

    后台路由

--- routes/admin.php
+++ routes/admin.php
@@ -5,3 +5,19 @@
 Route::get('/', function () {
     return view('tets');
 });
+
+Route::get('/login', 'IndexController@login');
+Route::post('/login', 'IndexController@doLogin')
+	->name('admin.login')
+	->middleware(App\Http\Middleware\VerifyPhoneNumber::class)
+	->middleware(App\Http\Middleware\CheckUserByPhoneExisted::class)
+	->middleware(App\Http\Middleware\VerifyPassword::class)
+	->middleware(App\Http\Middleware\VerifyPermissionNode::class)
+;
+Route::group([
+	'middleware' => [
+		App\Http\Middleware\CheckIsAdmin::class,
+	]
+], function() {
+	Route::get('/index', 'IndexController@index')->name('admin.index');
+});
