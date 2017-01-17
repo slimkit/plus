028c07589d415154aa156ed7031428fb7311e792
Seven <shiweidu@outlook.com>
2017-01-17 14:38:40 +0800

    删除js～尝试修复GitHub代码识别问题～

--- resources/assets/admin/index.js
+++
@@ -1,9 +0,0 @@
-import 'assets/bootstrap';
-import Vue from 'vue';
-import App from 'admin/App';
-
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  render: h => h(App)
-});
--- resources/assets/utils/APIs.js
+++
@@ -1,4 +0,0 @@
-import { createRequestURI } from './request';
-
-// Administrator Login API.
-export const ADMIN_LOGIN = createRequestURI('login');
