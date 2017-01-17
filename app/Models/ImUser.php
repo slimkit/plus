708cdc875994bb813a2e8db23c284de95b84d34b
zhiyicx <lovevipdsw@vip.qq.com>
2017-01-17 06:51:04 +0000

    Apply fixes from StyleCI

--- app/Models/ImUser.php
+++ app/Models/ImUser.php
@@ -59,12 +59,12 @@ class ImUser extends Model
      */
     public $service_urls = [
         'base_url' => 'http://192.168.10.222:9900',
-        'apis' => [
-            'users' => '/users',
+        'apis'     => [
+            'users'        => '/users',
             'conversation' => '/conversations',
-            'member' => '/conversations/member',
-            'limited' => '/conversations/{cid}/limited-members',
-            'message' => '/conversations/{cid}/messages',
+            'member'       => '/conversations/member',
+            'limited'      => '/conversations/{cid}/limited-members',
+            'message'      => '/conversations/{cid}/messages',
         ],
     ];
 
@@ -74,10 +74,10 @@ class ImUser extends Model
      * @var array
      */
     protected $response_type = [
-        'post' => ['post', 'add', 'init'],
-        'put' => ['put', 'update', 'save'],
+        'post'   => ['post', 'add', 'init'],
+        'put'    => ['put', 'update', 'save'],
         'delete' => ['delete', 'del'],
-        'get' => ['get', 'select'],
+        'get'    => ['get', 'select'],
     ];
 
     /**
@@ -86,7 +86,7 @@ class ImUser extends Model
      * @var array
      */
     public $service_auth = [
-        'user' => 'admin',
+        'user'     => 'admin',
         'password' => '123456',
     ];
 
@@ -253,8 +253,8 @@ class ImUser extends Model
         if ($res->getStatusCode() == 201 || $res_data['code'] == 201) {
             //添加成功,保存记录
             $imUser = [
-                'user_id' => $user_id,
-                'username' => $this->params['name'] ?? '',
+                'user_id'     => $user_id,
+                'username'    => $this->params['name'] ?? '',
                 'im_password' => $res_data['data']['token'],
                 'is_disabled' => 0,
             ];
@@ -295,10 +295,10 @@ class ImUser extends Model
             'form_params' => [
                 'type' => $type,
                 'name' => isset($ext_data['name']) ? $ext_data['name'] : '',
-                'pwd' => isset($ext_data['pwd']) ? $ext_data['pwd'] : '',
+                'pwd'  => isset($ext_data['pwd']) ? $ext_data['pwd'] : '',
                 'uids' => [1001, 1002],
             ],
-            'auth' => array_values($this->service_auth),
+            'auth'        => array_values($this->service_auth),
             'http_errors' => false,
         ]);
         $body = $res->getBody();
@@ -323,6 +323,7 @@ class ImUser extends Model
     {
         return in_array($type, [0, 1, 2]) ? true : false;
     }
+
     /**
      * 请求方法.
      *
@@ -347,7 +348,7 @@ class ImUser extends Model
         // exit;
         $res = $client->request($this->requset_method, $this->getRequestUrl(), [
             'form_params' => $this->params,
-            'auth' => array_values($this->service_auth),
+            'auth'        => array_values($this->service_auth),
             'http_errors' => $this->service_debug,
         ]);
         // 判断并返回执行结果
