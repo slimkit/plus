b8eaf86201ee35c9ce02a5bb7514baa13136b375
martinsun <80171636@qq.com>
2017-01-18 18:51:18 +0800

    IM聊天
    添加聊天会话迁移文件

---
+++ app/Models/ImConversation.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ImConversation extends Model
+{
+    /**
+     * 定义表名.
+     *
+     * @var string
+     */
+    protected $table = 'im_conversations';
+
+    /**
+     * 定义允许更新的字段.
+     *
+     * @var array
+     */
+    protected $fillable = ['user_id', 'cid', 'name', 'pwd', 'type', 'uids', 'created_at'];
+
+    /**
+     * 定义隐藏的字段.
+     *
+     * @var array
+     */
+    protected $hidden = ['id', 'is_disabled', 'updated_at'];
+
+    public function setUidsAttribute($uids)
+    {
+        $uids = is_array($uids) ? $uids : explode(',', $uids);
+        sort($uids);
+        $this->attributes['uids'] = implode(',', $uids);
+    }
+}
---
+++ database/migrations/2017_01_18_093905_create_im_conversations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateImConversationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('im_conversations', function (Blueprint $table) {
+            $table->increments('id')->comment('对话表表ID');
+            $table->integer('user_id')->default(0)->comment('创建对话用户UID');
+            $table->bigInteger('cid')->default(0)->comment('对话id');
+            $table->string('name')->dafault(null)->comment('对话名称');
+            $table->string('pwd')->default(null)->comment('加入对话密码');
+            $table->tinyInteger('is_disabled')->default(0)->comment('是否被禁用,1:是 0:否');
+            $table->tinyInteger('type')->default(0)->comment('对话类型 0:私聊 1:群聊 2:聊天室');
+            $table->text('uids')->default(null)->comment('已加入聊天的用户UID');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::dropIfExists('im_conversations');
+    }
+}
