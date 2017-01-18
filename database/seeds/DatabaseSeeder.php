a645c6304601c230374833c920f9c86200bfd24a
Seven <shiweidu@outlook.com>
2017-01-18 18:03:06 +0800

    完成地区数据迁移

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call(NodesTableSeeder::class);
         $this->call(NodeLinksTableSeeder::class);
         $this->call(AdminUsersTableSeeder::class);
-        // $this->call(AreasTableSeeder::class);
+        $this->call(AreasTableSeeder::class);
         $this->call(UserProfileSettingsTableSeeder::class);
         $this->call(UserProfileSettingLinksTableSeeder::class);
     }
