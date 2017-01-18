2b0a8fb65c5e070acb571e51ea6ac91ea2c0b77a
Seven <shiweidu@outlook.com>
2017-01-18 14:35:23 +0800

    增加任务和附件的关系

--- app/Models/StorageTask.php
+++ app/Models/StorageTask.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class StorageTask extends Model
 {
-    //
+    public function storage()
+    {
+        return $this->hasOne(Storage::class, 'hash', 'hash');
+    }
 }
