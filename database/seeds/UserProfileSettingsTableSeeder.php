942d6aa091af6e0e5767684a17f021d721c32b39
Seven <shiweidu@outlook.com>
2017-01-18 16:19:04 +0800

    修改迁移，更友好的组装数据～

--- database/seeds/UserProfileSettingsTableSeeder.php
+++ database/seeds/UserProfileSettingsTableSeeder.php
@@ -12,13 +12,33 @@ class UserProfileSettingsTableSeeder extends Seeder
      */
     public function run()
     {
-        UserProfileSetting::create(['create_user_id' => '1', 'profile' => 'sex', 'profile_name' => '性别', 'type' => 'radio', 'required' => '1', 'is_delable' => '0', 'state' => '1', 'default_options' => '1:男|2:女|3:其他', 'is_show' => '1']);
-        UserProfileSetting::create(['create_user_id' => '1', 'profile' => 'location', 'profile_name' => '地区', 'type' => 'multiselect', 'required' => '1', 'is_delable' => '0', 'state' => '1', 'is_show' => '1']);
-        UserProfileSetting::create(['create_user_id' => '1', 'profile' => 'intro', 'profile_name' => '简介', 'type' => 'textarea', 'required' => '1', 'is_delable' => '0', 'state' => '1', 'is_show' => '1']);
-        UserProfileSetting::create(['create_user_id' => '1', 'profile' => 'province', 'profile_name' => '省', 'type' => 'input', 'required' => '1', 'is_delable' => '0', 'state' => '1', 'is_show' => '0']);
-        UserProfileSetting::create(['create_user_id' => '1', 'profile' => 'city', 'profile_name' => '市', 'type' => 'input', 'required' => '1', 'is_delable' => '0', 'state' => '1', 'is_show' => '0']);
-        UserProfileSetting::create(['create_user_id' => '1', 'profile' => 'area', 'profile_name' => '区', 'type' => 'input', 'required' => '1', 'is_delable' => '0', 'state' => '1', 'is_show' => '0']);
-        UserProfileSetting::create(['create_user_id' => '1', 'profile' => 'education', 'profile_name' => '学历', 'type' => 'checkbox', 'required' => 1, 'is_delable' => '0', 'state' => '1', 'default_options' => '1:高中|2:大专|3:本科', 'is_show' => '1']);
-        UserProfileSetting::create(['create_user_id' => '1', 'profile' => 'name', 'profile_name' => '昵称', 'type' => 'input', 'required' => '1', 'is_delable' => '0', 'state' => '1', 'is_show' => '1']);
+        $datas = [
+            // base.
+            ['create_user_id' => '1', 'profile' => 'sex', 'profile_name' => '性别', 'type' => 'radio', 'required' => '1', 'is_delable' => '0', 'state' => '1', 'default_options' => '1:男|2:女|3:其他', 'is_show' => '1'],
+            ['create_user_id' => '1', 'profile' => 'location', 'profile_name' => '地区', 'type' => 'multiselect', 'required' => '1', 'is_delable' => '0', 'state' => '1', 'is_show' => '1'],
+            ['create_user_id' => '1', 'profile' => 'intro', 'profile_name' => '简介', 'type' => 'textarea', 'required' => '1', 'is_delable' => '0', 'state' => '1', 'is_show' => '1'],
+            ['create_user_id' => '1', 'profile' => 'province', 'profile_name' => '省', 'type' => 'input', 'required' => '1', 'is_delable' => '0', 'state' => '1', 'is_show' => '0'],
+            ['create_user_id' => '1', 'profile' => 'city', 'profile_name' => '市', 'type' => 'input', 'required' => '1', 'is_delable' => '0', 'state' => '1', 'is_show' => '0'],
+            ['create_user_id' => '1', 'profile' => 'area', 'profile_name' => '区', 'type' => 'input', 'required' => '1', 'is_delable' => '0', 'state' => '1', 'is_show' => '0'],
+            ['create_user_id' => '1', 'profile' => 'education', 'profile_name' => '学历', 'type' => 'checkbox', 'required' => 1, 'is_delable' => '0', 'state' => '1', 'default_options' => '1:高中|2:大专|3:本科', 'is_show' => '1'],
+            ['create_user_id' => '1', 'profile' => 'name', 'profile_name' => '昵称', 'type' => 'input', 'required' => '1', 'is_delable' => '0', 'state' => '1', 'is_show' => '1'],
+
+            // 用户头像
+            [
+                'create_user_id' => 0,
+                'profile' => 'avatar',
+                'profile_name' => '用户头像',
+                'type' => 'input',
+                'required' => 0,
+                'is_delable' => 0,
+                'state' => 1,
+                'is_show' => 0,
+                // 'default_options' => ''
+            ],
+        ];
+
+        foreach ($datas as $data) {
+            UserProfileSetting::create($data);
+        }
     }
 }
