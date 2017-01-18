abd77858fb5a750c53a58be3f4fe4f4c65f5dcea
zhiyicx <lovevipdsw@vip.qq.com>
2017-01-18 10:14:34 +0000

    Apply fixes from StyleCI

--- tests/Http/APIs/V1/GetUserTest.php
+++ tests/Http/APIs/V1/GetUserTest.php
@@ -1,13 +1,12 @@
-<?php 
+<?php
+
 namespace Ts\Test\Http\APIs\V1;
 
 use App\Models\User;
-use App\Models\UserProfileSetting;
-use App\Models\UserProfileSettingLink;
 
 class GetUserTest extends TestCase
 {
-	// test API.
+    // test API.
     protected $uri = '/api/v1/users/1';
 
     protected $url_404 = '/api/v1/users/5';
@@ -24,23 +23,23 @@ class GetUserTest extends TestCase
         foreach ($profiles as $value) {
             $datas[$value->profile] = $value->pivot->user_profile_setting_data;
         }
-        
+
         $json = $this->createMessageResponseBody([
-            'code' => 0,
+            'code'   => 0,
             'status' => true,
-            'data' => $datas
+            'data'   => $datas,
         ]);
         $this->seeJsonEquals($json);
     }
 
     public function testUserNotExistedStep()
-    {	
-    	$this->getJson($this->url_404, ['ACCESS-TOKEN' => 'dc22aae7774d9387ba766a1adce3165b']);
+    {
+        $this->getJson($this->url_404, ['ACCESS-TOKEN' => 'dc22aae7774d9387ba766a1adce3165b']);
         $this->seeStatusCode(404);
-    	$json = $this->createMessageResponseBody([
-    		'code' => 1005,
-    		'status' => false
-    	]);
-    	$this->seeJsonEquals($json);
+        $json = $this->createMessageResponseBody([
+            'code'   => 1005,
+            'status' => false,
+        ]);
+        $this->seeJsonEquals($json);
     }
 }
