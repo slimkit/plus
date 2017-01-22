7f4110323e0ddb55c90b8ff9e1cadedaffe4f883
Seven <shiweidu@outlook.com>
2017-01-22 18:44:02 +0800

    完成后台登录基本表单～

--- resources/assets/admin/component/Login.vue
+++ resources/assets/admin/component/Login.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="test" id="app">
-    <h1>hahaTest</h1>
+  <div class="container">
+    <div class="center-block login">
+      <form role="form">
+        <div class="form-group has-error">
+          <label for="access">账号</label>
+          <input type="tel" class="form-control" id="access" placeholder="输入账号">
+        </div>
+        <div class="form-group">
+          <label for="password">密码</label>
+          <input type="password" class="form-control" id="password" placeholder="输入密码">
+        </div>
+        <button type="submit" class="btn btn-lg btn-primary btn-block">登录</button>
+      </form>
+    </div>
   </div>
 </template>
+
+<style lang="scss">
+.login {
+  width: 100%;
+  min-width: 290px;
+  max-width: 320px;
+  margin-top: 40px;
+  padding: 15px;
+}
+</style>
