4be76e6f2dff8f2590a49850f5eee86b396471d0
Seven <shiweidu@outlook.com>
2017-01-25 13:17:57 +0800

    修复升级后的加载问题～

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,28 @@ const { mix } = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+// mix.js('resources/assets/js/app.js', 'public/js')
+//    .sass('resources/assets/sass/app.scss', 'public/css');
+
+
+
+// const elixir = require('laravel-elixir');
+
+
+//  |--------------------------------------------------------------------------
+//  | Elixir Asset Management
+//  |--------------------------------------------------------------------------
+//  |
+//  | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+//  | for your Laravel application. By default, we are compiling the Sass
+//  | file for your application as well as publishing vendor resources.
+//  |
+ 
+
+// elixir(function (mix) {
+//   mix.version([
+//     'js/*.js',
+//     'css/*.css'
+//   ]);
+// });
+
