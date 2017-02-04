99cc94df23fcc153c9d7bb0d0d46debf92f33a3f
Seven <shiweidu@outlook.com>
2017-02-04 14:01:15 +0800

    修正单元测试～

--- tests/Feature/APIs/V1/TestCase.php
+++ tests/Feature/APIs/V1/TestCase.php
@@ -3,21 +3,9 @@
 namespace Tests\Feature\APIs\V1;
 
 use Tests\TestCase as BaseTestCase;
+use Zhiyi\Plus\Traits\CreateJsonResponseData;
 
 class TestCase extends BaseTestCase
 {
-    /**
-     * 创建MessageResponseBody对象数据体.
-     *
-     * @param array $body 数据
-     *
-     * @return array 创建的数据
-     *
-     * @author Seven Du <shiweidu@outlook.com>
-     * @homepage http://medz.cn
-     */
-    protected function createMessageResponseBody(array $body): array
-    {
-        return app(MessageResponseBody::class, $body)->getBody();
-    }
+    use CreateJsonResponseData;
 }
