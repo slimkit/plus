59c30c8d5fbc16ba59d58e0bfe167ec1b7b4076e
Seven <shiweidu@outlook.com>
2017-02-04 10:38:26 +0800

    fix

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-use Zhiyi\Plus\Plus\Traits\CreateJsonResponseData;
+use Zhiyi\Plus\Traits\CreateJsonResponseData;
 
 class Controller extends BaseController
 {
