15d3d52fdaf7fe9be48e391e618ca4f8deba4d2e
Seven <shiweidu@outlook.com>
2017-02-04 14:01:06 +0800

    修改路由文件定义，更少的字符串输出。

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,17 +1,19 @@
 <?php
 
+use Zhiyi\Plus\Http\Middleware;
+
 // 获取手机验证码
 Route::post('/auth/phone/send-code', 'AuthController@sendPhoneCode')
-    ->middleware(Zhiyi\Plus\Http\Middleware\VerifyPhoneNumber::class) // 验证手机号格式是否正确
-    ->middleware(Zhiyi\Plus\Http\Middleware\VerifySendPhoneCodeType::class) // 验证发送验证码类型等集合
+    ->middleware(Middleware\VerifyPhoneNumber::class) // 验证手机号格式是否正确
+    ->middleware(Middleware\VerifySendPhoneCodeType::class) // 验证发送验证码类型等集合
 ;
 
 // 用户登录
 Route::post('/auth', 'AuthController@login')
-    ->middleware(Zhiyi\Plus\Http\Middleware\CheckDeviceCodeExisted::class) // 验证设备号是否存在
-    ->middleware(Zhiyi\Plus\Http\Middleware\VerifyPhoneNumber::class) // 验证手机号码是否正确
-    ->middleware(Zhiyi\Plus\Http\Middleware\CheckUserByPhoneExisted::class) // 验证手机号码用户是否存在
-    ->middleware(Zhiyi\Plus\Http\Middleware\VerifyPassword::class) // 验证密码是否正确
+    ->middleware(Middleware\CheckDeviceCodeExisted::class) // 验证设备号是否存在
+    ->middleware(Middleware\VerifyPhoneNumber::class) // 验证手机号码是否正确
+    ->middleware(Middleware\CheckUserByPhoneExisted::class) // 验证手机号码用户是否存在
+    ->middleware(Middleware\VerifyPassword::class) // 验证密码是否正确
 ;
 
 // 重置token接口
@@ -19,35 +21,35 @@ Route::patch('/auth', 'AuthController@resetToken');
 
 // 用户注册
 Route::post('/auth/register', 'AuthController@register')
-    ->middleware(Zhiyi\Plus\Http\Middleware\CheckDeviceCodeExisted::class) // 验证设备号是否存在
-    ->middleware(Zhiyi\Plus\Http\Middleware\VerifyPhoneNumber::class) // 验证手机号码是否正确
-    ->middleware(Zhiyi\Plus\Http\Middleware\VerifyUserNameRole::class) // 验证用户名规则是否正确
-    ->middleware(Zhiyi\Plus\Http\Middleware\CheckUserByNameNotExisted::class) // 验证用户名是否被占用
-    ->middleware(Zhiyi\Plus\Http\Middleware\CheckUserByPhoneNotExisted::class) // 验证手机号码是否被占用
-    ->middleware(Zhiyi\Plus\Http\Middleware\VerifyPhoneCode::class) // 验证验证码释放正确
+    ->middleware(Middleware\CheckDeviceCodeExisted::class) // 验证设备号是否存在
+    ->middleware(Middleware\VerifyPhoneNumber::class) // 验证手机号码是否正确
+    ->middleware(Middleware\VerifyUserNameRole::class) // 验证用户名规则是否正确
+    ->middleware(Middleware\CheckUserByNameNotExisted::class) // 验证用户名是否被占用
+    ->middleware(Middleware\CheckUserByPhoneNotExisted::class) // 验证手机号码是否被占用
+    ->middleware(Middleware\VerifyPhoneCode::class) // 验证验证码释放正确
 ;
 
 // 找回密码
 Route::patch('/auth/forgot', 'AuthController@forgotPassword')
-    ->middleware(Zhiyi\Plus\Http\Middleware\VerifyPhoneNumber::class) // 验证手机号格式
-    ->middleware(Zhiyi\Plus\Http\Middleware\CheckUserByPhoneExisted::class) // 验证手机号码用户是否存在
-    ->middleware(Zhiyi\Plus\Http\Middleware\VerifyPhoneCode::class) // 验证手机号码验证码是否正确
+    ->middleware(Middleware\VerifyPhoneNumber::class) // 验证手机号格式
+    ->middleware(Middleware\CheckUserByPhoneExisted::class) // 验证手机号码用户是否存在
+    ->middleware(Middleware\VerifyPhoneCode::class) // 验证手机号码验证码是否正确
 ;
 
 // 用户相关组
 Route::group([
     'middleware' => [
-        Zhiyi\Plus\Http\Middleware\AuthUserToken::class,
+        Middleware\AuthUserToken::class,
     ],
     'prefix' => 'users',
 ], function ($routes) {
     // 修改用户资料
     Route::patch('/', 'UserController@profile')
-        ->middleware(Zhiyi\Plus\Http\Middleware\ChangeUserAvatar::class)
-        ->middleware(Zhiyi\Plus\Http\Middleware\ChangeUsername::class);
+        ->middleware(Middleware\ChangeUserAvatar::class)
+        ->middleware(Middleware\ChangeUsername::class);
     // 修改用户密码
     Route::patch('/password', 'UserController@resetPassword') // 设置控制器
-        ->middleware(Zhiyi\Plus\Http\Middleware\VerifyPassword::class); // 验证用户密码是否正确
+        ->middleware(Middleware\VerifyPassword::class); // 验证用户密码是否正确
     // 获取用户信息
     Route::get('/{user}', 'UserController@get');
 });
@@ -57,7 +59,7 @@ Route::get('/storages/{storage}/{process?}', 'StorageController@get');
 // 附件储存相关
 Route::group([
     'middleware' => [
-        Zhiyi\Plus\Http\Middleware\AuthUserToken::class,
+        Middleware\AuthUserToken::class,
     ],
     'prefix' => 'storages',
 ], function () {
@@ -76,7 +78,7 @@ Route::group([
 Route::group([
     'prefix'     => 'im',
     'middleware' => [
-        Zhiyi\Plus\Http\Middleware\AuthUserToken::class,
+        Middleware\AuthUserToken::class,
     ],
 ], function () {
     Route::get('/users', 'ImController@getImAccount'); //获取聊天授权账号
