b44c0e90ac96767ce20a21cfddc372420fc4a3e4
zhiyicx <lovevipdsw@vip.qq.com>
2017-02-07 08:45:40 +0000

    Apply fixes from StyleCI

--- config/component.php
+++ config/component.php
@@ -1,24 +1,24 @@
 <?php
 
 return [
-    
-    /**
+
+    /*
      * The TS+ example.
      */
-    'zhiyicx/plus-component-example' => new class {
-
+    'zhiyicx/plus-component-example' => new class() {
         public function install()
         {
             var_dump('example-install');
         }
 
         public function router()
-        {}
+        {
+        }
 
         public function resource()
         {
             return [];
         }
     },
-    
+
 ];
