a3ffbd52d09e6e2eca27e106721458d5e6e3abd2
Seven <shiweidu@outlook.com>
2017-02-09 11:15:41 +0800

    delete example.

--- config/component.php
+++ config/component.php
@@ -1,24 +1,3 @@
 <?php
 
-return [
-
-    /*
-     * The TS+ example.
-     */
-    'zhiyicx/plus-component-example' => new class() {
-        public function install()
-        {
-        }
-
-        public function router()
-        {
-            return storage_path('example/router.php');
-        }
-
-        public function resource()
-        {
-            return storage_path('example/res');
-        }
-    },
-
-];
+return [];
--- storage/example/res/example.js
+++
@@ -1,1 +0,0 @@
-alert('This is TS+ component example.');
