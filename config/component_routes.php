f47d40350327fc6e3390c71291aa4789515d752d
martinsun <80171636@qq.com>
2017-02-13 17:20:49 +0800

    抽离IM模块

--- config/component_routes.php
+++ config/component_routes.php
@@ -1,3 +1,5 @@
-<?php
-
-return [];
+<?php 
+return 
+array (
+  'zhiyicx/plus-component-im' => 'D:\\wamp_new\\www\\thinksns-plus\\vendor\\zhiyicx\\plus-component-im/routes/api.php',
+);
--- routes/api_v1.php
+++ routes/api_v1.php
@@ -73,22 +73,3 @@ Route::group([
     Route::post('/task/{storage_task_id}', 'StorageController@upload')
         ->name('storage/upload');
 });
-
-// IM相关接口
-Route::group([
-    'prefix'     => 'im',
-    'middleware' => [
-        Middleware\AuthUserToken::class,
-    ],
-], function () {
-    Route::get('/users', 'ImController@getImAccount'); //获取聊天授权账号
-    Route::post('/conversations', 'ImController@createConversations'); //创建聊天
-    Route::get('/conversations/{cid}', 'ImController@getConversation'); //获取单个聊天信息
-    Route::get('/conversations/list/all', 'ImController@getConversationList'); //获取某个用户聊天列表
-    Route::patch('/users', 'ImController@refresh'); //刷新授权
-    Route::delete('/conversations/{cid}', 'ImController@deleteConversation'); //删除对话
-    Route::post('/conversations/members/limited/{cid}', 'ImController@disableLimited'); //对话成员限制
-    Route::delete('/conversations/members/limited/{cid}/{uid}', 'ImController@enabledLimited'); //移除对话中限制的成员
-    Route::delete('/conversations/members/{cid}', 'ImController@exitConversations'); //退出对话
-    Route::delete('/conversations/members/{cid}/{uid}', 'ImController@deleteMembers'); //剔除指定成员
-});
