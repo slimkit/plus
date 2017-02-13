8dd4ea72a0bc4daee3ab3caa9d35a99d0f42d953
martinsun <80171636@qq.com>
2017-02-13 17:21:29 +0800

    添加IM拓展应用

--- config/component.php
+++ config/component.php
@@ -1,3 +1,5 @@
-<?php
-
-return [];
+<?php 
+return array (
+  'zhiyicx/plus-component-im' => 'Zhiyi\\Component\\ZhiyiPlus\\PlusComponentIm\\Installer\\Installer',
+  'medz/plus-component-example' => 'Medz\\Component\\ZhiyiPlus\\PlusComponentExample\\Installer\\Installer',
+);
