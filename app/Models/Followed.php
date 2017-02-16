14197fbf45393a96f9dc69419838bda319c78cb9
boxshadow <idafoo@sina.com>
2017-02-16 15:33:14 +0800

    update models

--- app/Models/Followed.php
+++ app/Models/Followed.php
@@ -8,9 +8,14 @@ use Illuminate\Database\Eloquent\Builder;
 class Followed extends Model
 {
 	protected $fillable = ['user_id', 'followed_user_id'];
-	
+
     public function user()
     {
     	return $this->belongsTo(User::class, 'id', 'user_id');
+    }
+
+    public function scopeByUserId(Builder $query, int $user_id): Builder
+    {
+    	return $query->where('user_id', $user_id);
     }
 }
--- app/Models/Following.php
+++ app/Models/Following.php
@@ -7,9 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Following extends Model
 {
 	protected $fillable = ['user_id', 'following_user_id'];
-	
+
     public function user()
     {
-    	return $this->belongsTo(User::class, 'id', 'user_id');
+    	return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function scopeByUserId(Builder $query, int $user_id): Builder
+    {
+    	return $query->where('user_id', $user_id);
     }
 }
