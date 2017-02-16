b979933a50a35688905e2421f2bdba79fafa2956
boxshadow <idafoo@sina.com>
2017-02-16 14:21:35 +0800

    add middleware for follow operations

---
+++ app/Http/Middleware/CheckISFollowing.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Zhiyi\Plus\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Zhiyi\Plus\Traits\CreateJsonResponseData;
+use Zhiyi\Plus\Models\User;
+use Zhiyi\Plus\Models\Following;
+
+class CheckIsFollowing
+{
+    use CreateJsonResponseData;
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $user_id = $request->user()->id;
+        $following_user_id = $request->user_id;
+        if(!Following::where([
+                ['user_id', $user_id],
+                ['following_user_id', $following_user_id]
+            ])
+            ->count()
+        ) {
+            return response()->json(static::createJsonData([
+                'code' => 1021,
+                'message' => '您并没有关注此用户',
+                'status' => false
+            ]))->setStatusCode(400);
+        }
+
+        return $next($request);
+    }
+}
