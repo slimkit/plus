17e02726fb186f3c0dcb920200766aecdd461c01
Seven <shiweidu@outlook.com>
2017-02-21 14:43:57 +0800

    change admin route.

--- routes/admin.php
+++ routes/admin.php
@@ -6,5 +6,11 @@
 
 Route::get('/', 'HomeController@index')
 ->name('admin');
+
 Route::post('/login', 'HomeController@login');
 Route::any('/logout', 'HomeController@logout');
+
+// Add the route, SPA used mode "history"
+// But, RESTful the route?
+// Route::get('/{route?}', 'HomeController@index')
+// ->where('route', '.*');
