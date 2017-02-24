db03f64ecf6f1576b637632dae9373f15a913cb7
zhiyicx <lovevipdsw@vip.qq.com>
2017-02-24 10:05:48 +0000

    Apply fixes from StyleCI

--- app/Models/UserDatas.php
+++ app/Models/UserDatas.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class UserDatas extends Model
-{   
+{
     protected $table = 'user_datas';
 
     protected $fillable = [
@@ -16,12 +16,14 @@ class UserDatas extends Model
     ];
 
     /**
-     * 通过用户id查看统计数据
-     * 
+     * 通过用户id查看统计数据.
+     *
      * @author bs<414606094@qq.com>
-     * @param  Builder $query   [description]
-     * @param  int     $user_id [description]
-     * @return [type]           [description]
+     *
+     * @param Builder $query   [description]
+     * @param int     $user_id [description]
+     *
+     * @return [type] [description]
      */
     public function scopeByUserId(Builder $query, int $user_id): Builder
     {
