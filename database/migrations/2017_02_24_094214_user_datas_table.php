dbb74917b7023b764acf3c5f5f82ab6cca0916ed
白石 <414606094@qq.com>
2017-02-24 17:49:16 +0800

    添加  用户统计信息表  迁移

---
+++ database/migrations/2017_02_24_094214_user_datas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UserDatasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_datas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('uid')->comment('用户uid');
+            $table->string('key')->comment('键名');
+            $table->string('value')->comment('对应key的值');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_datas');
+    }
+}
