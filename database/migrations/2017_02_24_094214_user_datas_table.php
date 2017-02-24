db31b88f567ffe5692b853305cf04c66426a4230
白石 <414606094@qq.com>
2017-02-24 17:58:55 +0800

    统一为语义化命名

--- database/migrations/2017_02_24_094214_user_datas_table.php
+++ database/migrations/2017_02_24_094214_user_datas_table.php
@@ -15,7 +15,7 @@ class UserDatasTable extends Migration
     {
         Schema::create('user_datas', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('uid')->comment('用户uid');
+            $table->integer('user_id')->comment('用户id');
             $table->string('key')->comment('键名');
             $table->string('value')->comment('对应key的值');
             $table->timestamps();
