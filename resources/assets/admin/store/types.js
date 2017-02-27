240a04bf3d689b27f16042161b50949ee58e8b34
Seven <shiweidu@outlook.com>
2017-02-27 16:45:41 +0800

    增加更新状态树type。

--- resources/assets/admin/store/types.js
+++ resources/assets/admin/store/types.js
@@ -9,3 +9,6 @@ export const USER_UPDATE = 'user/update';
 
 // The const is defined delete user data type.
 export const USER_DELETE = 'user/delete';
+
+// Defined [settings/site].
+export const SETTINGS_SITE_UPDATE = 'settings/site/UPDATE';
