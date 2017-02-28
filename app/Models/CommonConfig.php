83d5d245cbee6652641d899863379050578ac5ef
Seven <shiweidu@outlook.com>
2017-02-28 11:46:10 +0800

    add CommonConfig model.

---
+++ app/Models/CommonConfig.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Zhiyi\Plus\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+
+class CommonConfig extends Model
+{
+    /**
+     * Scope func to namespace.
+     *
+     * @param Illuminate\Database\Eloquent\Builder $query
+     * @param string $namespace
+     * @return Illuminate\Database\Eloquent\Builder
+     * @author Seven Du <shiweidu@outlook.com>
+     * @homepage http://medz.cn
+     */
+    public function scopeByNamespace(Builder $query, string $namespace): Builder
+    {
+        return $query->where('namespace', $namespace);
+    }
+
+    /**
+     * Scope func to name.
+     *
+     * @param Illuminate\Database\Eloquent\Builder $query
+     * @param string $name
+     * @return Illuminate\Database\Eloquent\Builder
+     * @author Seven Du <shiweidu@outlook.com>
+     * @homepage http://medz.cn
+     */
+    public function scopeByName(Builder $query, string $name): Builder
+    {
+        return $query->where('name', $name);
+    }
+}
