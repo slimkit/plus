41643397e6bd8aec1eee10e60a6424b48b8a2ee7
Seven <shiweidu@outlook.com>
2017-02-28 11:45:59 +0800

    add common_configs migration.

---
+++ database/migrations/2017_02_28_031017_common_configs.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CommonConfigs extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('common_confgs', function (Blueprint $table) {
+            $table->string('name', 30)->comment('配置名称');
+            $table->string('namespace', 15)->comment('配置命名空间');
+            $table->text('value')->nullable()->comment('缓存值');
+            $table->primary(['name', 'namespace']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('common_confgs');
+    }
+}
