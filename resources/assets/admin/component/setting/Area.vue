1aa6d148b9558f2edcff71d2dc41860513489d36
Seven <shiweidu@outlook.com>
2017-03-02 17:14:39 +0800

    修改地区管理模板～

--- resources/assets/admin/component/setting/Area.vue
+++ resources/assets/admin/component/setting/Area.vue
@@ -1,3 +1,8 @@
 <template>
-  <div>area.</div>
+  <div class="container-fluid">
+    <ol class="breadcrumb">
+      <li><a href="#">国家</a></li>
+      <li class="active">中国</li>
+    </ol>
+  </div>
 </template>
