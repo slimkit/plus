61d07ef3ec613047bca8cc6c9f863f1c5882e444
Seven <shiweidu@outlook.com>
2017-03-03 15:04:41 +0800

    修改地区管理页面逻辑

--- resources/assets/admin/component/setting/Area.vue
+++ resources/assets/admin/component/setting/Area.vue
@@ -1,8 +1,39 @@
+<style lang="css" module>
+.container {
+  padding-top: 15px;
+}
+</style>
+
 <template>
-  <div class="container-fluid">
+  <div class="container-fluid" :class="$style.container">
     <ol class="breadcrumb">
       <li><a href="#">国家</a></li>
       <li class="active">中国</li>
     </ol>
   </div>
 </template>
+
+<script>
+import { mapGetters } from 'vuex';
+import { SETTINGS_AREA } from '../../store/getter-types';
+
+const AreaComponent = {
+  data: () => ({
+    // list: []
+  }),
+  computed: {
+    ...mapGetters({
+      areas: SETTINGS_AREA
+    }),
+    list () {
+      return [];
+    }
+  },
+  created () {
+    const { id = 0 } = this.$route.params;
+    console.log(id);
+  }
+};
+
+export default AreaComponent;
+</script>
