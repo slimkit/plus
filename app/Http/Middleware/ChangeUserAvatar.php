d6c3b09ec68b3e72ba6fb8b9e441ee1cd71c53a2
zhiyicx <lovevipdsw@vip.qq.com>
2017-03-07 06:51:09 +0000

    Apply fixes from StyleCI

--- app/Http/Middleware/ChangeUserAvatar.php
+++ app/Http/Middleware/ChangeUserAvatar.php
@@ -3,8 +3,8 @@
 namespace Zhiyi\Plus\Http\Middleware;
 
 use Closure;
-use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Zhiyi\Plus\Models\StorageTask;
 use Zhiyi\Plus\Models\User;
