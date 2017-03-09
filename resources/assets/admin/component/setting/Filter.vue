7126887c5995dc950f9329ffa248d231a7417596
Seven <shiweidu@outlook.com>
2017-03-09 15:50:05 +0800

    添加过滤词库缺省～

--- resources/assets/admin/component/setting/Filter.vue
+++ resources/assets/admin/component/setting/Filter.vue
@@ -1,3 +1,13 @@
+<style lang="css" module>
+.container {
+  padding-top: 15px;
+}
+</style>
+
 <template>
-  <div>fiter.</div>
+  <div class="container-fluid" :class="$style.container">
+    <div class="alert alert-success" role="alert">
+      暂无设置
+    </div>
+  </div>
 </template>
