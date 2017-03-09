b9c018523bdc507d928d66bdac3df7f1f1e1db62
Seven <shiweidu@outlook.com>
2017-03-09 16:45:00 +0800

    增加title

--- resources/assets/admin/component/user/Manage.vue
+++ resources/assets/admin/component/user/Manage.vue
@@ -1,3 +1,17 @@
+<style lang="css" module>
+.container {
+  padding-top: 15px;
+}
+</style>
+
 <template>
-  <div>manage.</div>
+  <div class="container-fluid" :class="$style.container">
+    <div class="well well-sm">
+      检索用户
+      <a class="btn btn-link pull-right btn-xs" href="#" role="button">
+        <span class="glyphicon glyphicon-plus"></span>
+        添加用户
+      </a>
+    </div>
+  </div>
 </template>
