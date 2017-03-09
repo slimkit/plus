8f5b7234c4d33ecc73b99ffdd2c20f9eba0ee408
Seven <shiweidu@outlook.com>
2017-03-09 16:44:43 +0800

    增加favicon.ico

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <link rel="icon" type="image/x-icon" href="{{ url('/favicon.ico') }}">
 
     <title>后台管理</title>
 
