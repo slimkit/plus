9bfbe65bcced94eed72b386d5d76df5f193a323d
Seven <shiweidu@outlook.com>
2017-03-13 16:31:54 +0800

    增加权限节点UI提示～

--- resources/assets/admin/component/user/Permission.vue
+++ resources/assets/admin/component/user/Permission.vue
@@ -1,3 +1,25 @@
+<style lang="css" module>
+.container {
+  padding-top: 15px;
+}
+.loadding {
+  text-align: center;
+  font-size: 42px;
+}
+.loaddingIcon {
+  animation-name: "TurnAround";
+  animation-duration: 1.4s;
+  animation-timing-function: linear;
+  animation-iteration-count: infinite;
+}
+</style>
+
 <template>
-  <div>permission.</div>
+  <div class="container-fluid" :class="$style.container">
+    <!-- 提示 -->
+    <div class="alert alert-success" role="alert">
+      权限节点，用于各个位置标示用户权限的配置～配置需要配合程序。尽量不要删除权限节点～以为节点name是在程序中赢编码的～
+      这里提供管理，只是方便技术人员对节点进行管理。
+    </div>
+  </div>
 </template>
