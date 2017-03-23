786c386f62d41338818ffeade004ab8beaef897f
Seven <shiweidu@outlook.com>
2017-03-23 14:26:46 +0800

    增加基础样式～

--- resources/assets/admin/component/user/RoleManage.vue
+++ resources/assets/admin/component/user/RoleManage.vue
@@ -1,3 +1,19 @@
+<style lang="css" module>
+.container {
+  padding-top: 15px;
+}
+.loadding {
+  text-align: center;
+  font-size: 42px;
+}
+.loaddingIcon {
+  animation-name: "TurnAround";
+  animation-duration: 1.4s;
+  animation-timing-function: linear;
+  animation-iteration-count: infinite;
+}
+</style>
+
 <template>
   <div>123</div>
 </template>
