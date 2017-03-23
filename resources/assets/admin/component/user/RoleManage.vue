8e9dc29619812684faa3c008662962db8f419572
Seven <shiweidu@outlook.com>
2017-03-23 14:30:27 +0800

    抽离够用component样式～减少编译后的样式大小～

--- resources/assets/admin/component/user/RoleManage.vue
+++ resources/assets/admin/component/user/RoleManage.vue
@@ -1,19 +1,3 @@
-<style lang="css" module>
-.container {
-  padding-top: 15px;
-}
-.loadding {
-  text-align: center;
-  font-size: 42px;
-}
-.loaddingIcon {
-  animation-name: "TurnAround";
-  animation-duration: 1.4s;
-  animation-timing-function: linear;
-  animation-iteration-count: infinite;
-}
-</style>
-
 <template>
-  <div>123</div>
+  <div class="component-container container-fluid">123</div>
 </template>
