7a5050e8bb26a7e2f8efb9260f1b4f7cec1be626
白石 <414606094@qq.com>
2017-03-23 11:17:52 +0800

    更新文档

--- database/migrations/2017_03_22_035404_create_comments_table.php
+++ database/migrations/2017_03_22_035404_create_comments_table.php
@@ -17,8 +17,8 @@ class CreateCommentsTable extends Migration
             $table->increments('id');
             $table->string('component')->comment('扩展包名');
             $table->integer('comment_id')->comment('关联评论id');
-            $table->integer('to_user_id')->default(0)->comment('资源作者id');
             $table->integer('user_id')->default(0)->comment('评论者id');
+            $table->integer('to_user_id')->default(0)->comment('资源作者id');
             $table->integer('reply_to_user_id')->default(0)->comment('被回复的评论者id');
             $table->timestamps();
         });
