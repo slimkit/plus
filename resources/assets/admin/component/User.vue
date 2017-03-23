fbc5009deb772d62b12dcc7aaa37a992455e8fe1
Seven <shiweidu@outlook.com>
2017-03-23 14:19:48 +0800

    修改正确的路由导航～

--- resources/assets/admin/component/User.vue
+++ resources/assets/admin/component/User.vue
@@ -7,13 +7,13 @@
 <template>
   <div>
     <ul class="nav nav-tabs" :class="$style.nav">
-      <router-link to="/users/manage" tag="li" active-class="active">
+      <router-link to="/users" tag="li" active-class="active" exact>
         <a href="#">用户管理</a>
       </router-link>
-      <router-link to="/users/role" tag="li" active-class="active">
+      <router-link to="/users/roles" tag="li" active-class="active">
         <a href="#">角色管理</a>
       </router-link>
-      <router-link to="/users/permission" tag="li" active-class="active">
+      <router-link to="/users/permissions" tag="li" active-class="active">
         <a href="#">权限管理</a>
       </router-link>
     </ul>
