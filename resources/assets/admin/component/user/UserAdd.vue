44b0cd8468f07a3f7f8f2a25b8ff842aff408683
Seven <shiweidu@outlook.com>
2017-03-24 12:08:44 +0800

    更优雅的配置～

--- resources/assets/admin/component/user/UserAdd.vue
+++ resources/assets/admin/component/user/UserAdd.vue
@@ -57,6 +57,7 @@
 
 <script>
 import request, { createRequestURI } from '../../util/request';
+import { code2message } from '../../util/codes';
 import lodash from 'lodash';
 
 const UserAddComponent = {
@@ -78,14 +79,7 @@ const UserAddComponent = {
         this.$router.replace({ path: '/users', query: { userId } });
       }).catch(({ response: { data = {} } = {} }) => {
         const { errors = ['添加失败'], code } = data;
-        const { [code]: errorMessage = lodash.values(errors).pop() } = {
-          1000: '手机号码格式不正确',
-          1002: '用户名长度不正确，长度必须大于1个长度且小于12个长度',
-          1003: '用户名格式不正确，不能特殊字符或者数字开头',
-          1004: '用户名已经被使用',
-          1010: '手机号码已经被使用'
-        };
-        this.errorMessage = errorMessage;
+        this.errorMessage = code2message(code, lodash.values(errors).pop());
         this.adding = false;
       });
     },
