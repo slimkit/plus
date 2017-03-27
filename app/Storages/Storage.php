9ca26c28281e8c61963b480513593971da19643a
Seven <shiweidu@outlook.com>
2017-03-27 17:28:25 +0800

    删除不需要的部分～

--- app/Storages/Storage.php
+++ app/Storages/Storage.php
@@ -22,24 +22,6 @@ class Storage
     protected static $storages = [];
 
     /**
-     * 初始化.
-     *
-     * @author Seven Du <shiweidu@outlook.com>
-     * @homepage http://medz.cn
-     */
-    public function __construct($storages = [])
-    {
-        foreach ($storages as $engine => $storage) {
-            $this->setStorageEngine($engine, $storage);
-        }
-
-        if (! isset(static::$storages['local'])) {
-            $storage = new Engine\LocalStorage();
-            $this->setStorageEngine('local', $storage);
-        }
-    }
-
-    /**
      * 设置储存引擎.
      *
      * @param string                 $engine  储存引擎名称
