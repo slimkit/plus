ad594b6a2d188c92cc16ebe81f25cfc7553ca9bd
Seven <shiweidu@outlook.com>
2017-03-31 15:55:48 +0800

    完善储存服务类功能～

--- app/Services/Storage.php
+++ app/Services/Storage.php
@@ -51,6 +51,19 @@ class Storage
     }
 
     /**
+     * 设置选择的储存引擎.
+     *
+     * @param string $engine
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function setEngineSelect(string $engine): bool
+    {
+        return (bool) CommonConfig::byNamespace('storage')->byName('select')->update([
+            'value' => $engine,
+        ]);
+    }
+
+    /**
      * 获取储存过程.
      *
      * @return \Zhiyi\Plus\Storages\Storage
@@ -124,17 +137,17 @@ class Storage
      */
     public function getEngineOptionModel(string $engine): CommonConfig
     {
-        $engineOptiomModel = CommonConfig::byNamespace('storage')->byName($engine)->first();
+        $engineOptionModel = CommonConfig::byNamespace('storage')->byName($engine)->first();
 
-        if (! $engineOptiomModel) {
-            $engineOptiomModel = new CommonConfig();
-            $engineOptiomModel->namespace = 'storage';
-            $engineOptiomModel->name = $engine;
-            $engineOptiomModel->value = json_encode([]);
-            $engineOptiomModel->save();
+        if (! $engineOptionModel) {
+            $engineOptionModel = new CommonConfig();
+            $engineOptionModel->namespace = 'storage';
+            $engineOptionModel->name = $engine;
+            $engineOptionModel->value = json_encode([]);
+            $engineOptionModel->save();
         }
 
-        return $engineOptiomModel;
+        return $engineOptionModel;
     }
 
     /**
@@ -158,20 +171,70 @@ class Storage
     /**
      * 设置储存引擎配置.
      *
-     * @param string $engins
+     * @param string $engine
      * @param array $option
-     * @param array $baseOption
      * @return bool
      * @author Seven Du <shiweidu@outlook.com>
      */
-    public function setEngineOption(string $engins, array $option, array $baseOption = []): bool
+    public function setEngineOption(string $engine, array $option): bool
     {
-        $option = array_merge($baseOption, $option);
+        $option = $this->testOptionInout(array_get($this->getEngines(), $engine.'.option'), $option);
         $value = json_encode($option);
 
         return (bool) CommonConfig::byNamespace('storage')->byName($engine)->update([
             'value' => $value,
         ]);
+    }
+
+    /**
+     * 验证 option 表单字段.
+     *
+     * @param CommonConfig $engineOptionModel option 模型
+     * @param array $option 表单数据
+     * @return array
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function testOptionInout(array $optionRoles, array $option): array
+    {
+        $newOption = [];
+        $roles = collect($optionRoles)->keyBy('name');
+        $roles->each(function ($role, $key) use ($option, &$newOption) {
+            $optionValue = array_get($option, $key);
+            $type = array_get($role, 'type');
+
+            // 文本输入框
+            if ($type === 'text') {
+               $newOption[$key] = $optionValue ?: array_get($role, 'value');
+
+            // 多选
+            } elseif (
+                ($type === 'checkbox' && is_array($optionValue)) ||
+                ($type === 'select' && array_get($role, 'multiple'))
+            ) {
+                $values = array_keys(array_get($role, 'items', []));
+                collect($optionValue)->each(function ($value) use ($values, $key) {
+                    if (! in_array($value, $values)) {
+                        throw new \Exception(sprintf('验证的储存引擎表单%s储存的值不存在于规定表单中', $key), 422);
+                    }
+                });
+                $newOption[$key] = $optionValue;
+
+            // 单选
+            } elseif (
+                $type === 'radio' ||
+                ($type === 'select' && ! array_get($role, 'multiple'))
+            ) {
+                $values = array_keys(array_get($role, 'items', []));
+                if (! in_array($optionValue, $values) && $optionValue) {
+                    throw new \Exception(sprintf('验证的储存引擎表单%s储存的值不存在于规定表单中', $key), 422);
+                }
+                $newOption[$key] = $optionValue ?: array_get($role, 'value');
+            } else {
+                throw new \Exception(sprintf('表单%s不允许不传递'), 422);
+            }
+        });
+
+        return $newOption;
     }
 
     /**
