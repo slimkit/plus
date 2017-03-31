d9ac2d3147fbe5e84b5bc838735310c75fc201b7
Seven <shiweidu@outlook.com>
2017-03-31 13:59:51 +0800

    增加修改储存引擎配置控制器。

--- app/Http/Controllers/Admin/StorageController.php
+++ app/Http/Controllers/Admin/StorageController.php
@@ -2,6 +2,7 @@
 
 namespace Zhiyi\Plus\Http\Controllers\Admin;
 
+use Illuminate\Http\Request;
 use Zhiyi\Plus\Http\Controllers\Controller;
 use Zhiyi\Plus\Services\Storage as StorageService;
 
@@ -24,5 +25,18 @@ class StorageController extends Controller
             'selected' => $selected,
             'optionsValues' => $optionsValues,
         ])->setStatusCode(200);
+    }
+
+    /**
+     * 修改用户选择引擎的表单配置.
+     *
+     * @param Request $request
+     * @param string $engine
+     * @return mixed
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function updateEngineOption(Request $request, string $engine)
+    {
+        var_dump($engine);
     }
 }
