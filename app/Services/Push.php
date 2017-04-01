ebf01e85c371de75967c1369c6c5ceee76cc8193
白石 <414606094@qq.com>
2017-04-01 15:15:59 +0800

    依赖极光推送的 简单推送队列

---
+++ app/Services/Push.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Zhiyi\Plus\Services;
+use JPush\Client;
+
+class Push 
+{
+	public function __construct()
+	{
+		$appkey = env('JPUSH_APP_KEY');
+		$secret = env('JPUSH_MASTER_SECRET');
+		if (!$appkey || !$secret) {
+			return false;
+		}
+
+		$this->client = new Client($appkey, $secret);
+	}
+
+	public function push($alert, $audience, $extras = array())
+	{
+        $notification = array(
+            'extras' => $extras, 
+        );
+
+        try {
+
+            $result = $this->client->push()
+                ->setOptions(1, null, null, false, null)
+                ->setPlatform('all') //全部平台
+                ->addAlias($audience) // 指定用户
+                ->iosNotification($alert, $notification)
+                ->androidNotification($alert, $notification)
+                ->send();
+            return [
+                'code'  => 1,
+                'data' => [],
+                'message' => '推送成功',
+            ]; 
+        } catch (\Exception $e) {
+            return [
+                'code'  => 0,
+                'message' => $e->getMessage(),
+            ]; 
+        }
+	}
+}
