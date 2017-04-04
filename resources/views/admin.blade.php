d05b91aa593c0cbd3f49a0cf047b269a5a8a8ce3
Seven <shiweidu@outlook.com>
2017-04-04 14:12:37 +0800

    修改后台模板UI缩放模式。

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -4,7 +4,7 @@
 
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <link rel="icon" type="image/x-icon" href="{{ url('/favicon.ico') }}">
 
     <title>后台管理</title>
