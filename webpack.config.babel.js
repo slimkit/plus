f42da05adf744ceb0aa16a71ce7b8485a3ddf71a
Seven <shiweidu@outlook.com>
2017-04-06 14:31:55 +0800

    postcss-load-config

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -153,21 +153,7 @@ const webpackConfig = {
         options: {
           loaders: cssLoaders({
             sourceMap: !isProd
-          }),
-          postcss: [
-            autoprefixer({
-              browsers: [
-                'Android 2.3',
-                'Android >= 4',
-                'Chrome >= 20',
-                'Firefox >= 24',
-                'Explorer >= 8',
-                'iOS >= 6',
-                'Opera >= 12',
-                'Safari >= 6'
-              ]
-            })
-          ]
+          })
         }
       },
       {
