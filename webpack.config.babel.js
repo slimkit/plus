5c7353631de3523a104a491d506286cc890032f5
Seven <shiweidu@outlook.com>
2017-04-06 14:00:56 +0800

    修改 cssLoader.

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -23,42 +23,48 @@ const entry = {
   admin: path.join(assetsRoot, 'admin', 'index.js')
 };
 
-const cssLoaders = (options = {}) => {
+function cssLoaders (options = {}) {
+  let cssLoader = {
+    loader: 'css-loader',
+    options: {
+      minimize: isProd,
+      sourceMap: options.sourceMap
+    }
+  };
+
   // generate loader string to be used with extract text plugin
-  function generateLoaders (loaders) {
-    let sourceLoader = loaders.map(function (loader) {
-      let extraParamChar;
-      if (/\?/.test(loader)) {
-        loader = loader.replace(/\?/, '-loader?');
-        extraParamChar = '&';
-      } else {
-        loader = loader + '-loader';
-        extraParamChar = '?';
-      }
-      return loader + (options.sourceMap ? extraParamChar + 'sourceMap' : '');
-    }).join('!');
+  function generateLoaders (loader, loaderOptions) {
+    let loaders = [cssLoader];
+    if (loader) {
+      loaders.push({
+        loader: loader + '-loader',
+        options: Object.assign({}, loaderOptions, {
+          sourceMap: options.sourceMap
+        })
+      });
+    }
 
     // Extract CSS when that option is specified
     // (which is the case during production build)
     if (options.extract) {
       return ExtractTextPlugin.extract({
-        use: sourceLoader,
+        use: loaders,
         fallback: 'vue-style-loader'
       });
-    } else {
-      return ['vue-style-loader', sourceLoader].join('!');
     }
+
+    return ['vue-style-loader'].concat(loaders);
   }
 
-  // http://vuejs.github.io/vue-loader/en/configurations/extract-css.html
+  // https://vue-loader.vuejs.org/en/configurations/extract-css.html
   return {
-    css: generateLoaders(['css']),
-    postcss: generateLoaders(['css']),
-    // less: generateLoaders(['css', 'less']),
-    sass: generateLoaders(['css', 'sass?indentedSyntax']),
-    scss: generateLoaders(['css', 'sass']),
-    // stylus: generateLoaders(['css', 'stylus']),
-    // styl: generateLoaders(['css', 'stylus'])
+    css: generateLoaders(),
+    postcss: generateLoaders(),
+    // less: generateLoaders('less'),
+    sass: generateLoaders('sass', { indentedSyntax: true }),
+    scss: generateLoaders('sass'),
+    // stylus: generateLoaders('stylus'),
+    // styl: generateLoaders('stylus')
   };
 };
 
@@ -75,7 +81,7 @@ function MixManifest(stats) {
 
   // return stats;
   return JSON.stringify(manifest, null, 2);
-}
+};
 
 // 环境插件～不同环境启用的不同插件.
 const plugins = isProd ?
