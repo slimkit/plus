57bd93e07c685db9e6af45b745e1144dafbfe2c0
白石 <414606094@qq.com>
2017-04-11 15:15:21 +0800

    修改点赞记录表字段

--- database/migrations/2017_04_10_094448_create_diggs.php
+++ database/migrations/2017_04_10_094448_create_diggs.php
@@ -17,7 +17,10 @@ class CreateDiggs extends Migration
             $table->increments('id');
             $table->engine = 'InnoDB';
             $table->string('component')->comment('扩展包名');
+            $table->string('digg_table')->comment('点赞表名');
             $table->integer('digg_id')->comment('关联点赞记录id');
+            $table->string('source_table')->comment('资源表名');
+            $table->integer('source_id')->comment('关联资源id');
             $table->integer('user_id')->default(0)->comment('点赞者id');
             $table->integer('to_user_id')->default(0)->comment('资源作者id');
             $table->timestamps();
