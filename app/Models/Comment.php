51cbc636c777bee4ec04aa149c590868fa3425ae
白石 <414606094@qq.com>
2017-04-12 15:14:24 +0800

    修改对应模型

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,5 +8,5 @@ class Comment extends Model
 {
     protected $table = 'comments';
 
-    protected $fillable = ['component', 'comment_id', 'user_id', 'to_user_id', 'reply_to_user_id', 'comment_table', 'source_table', 'source_id'];
+    protected $fillable = ['component', 'comment_id', 'comment_content', 'user_id', 'to_user_id', 'reply_to_user_id', 'comment_table', 'source_table', 'source_id', 'source_content', 'source_cover'];
 }
--- app/Models/Digg.php
+++ app/Models/Digg.php
@@ -8,5 +8,5 @@ class Digg extends Model
 {
     protected $table = 'diggs';
 
-    protected $fillable = ['component', 'digg_table', 'digg_id', 'user_id', 'to_user_id', 'reply_to_user_id', 'source_table', 'source_id'];
+    protected $fillable = ['component', 'digg_table', 'digg_id', 'user_id', 'to_user_id', 'reply_to_user_id', 'source_table', 'source_id', 'source_content', 'source_cover'];
 }
