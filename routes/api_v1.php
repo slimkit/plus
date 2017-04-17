47ff43af96cae96b5d2d1bc03acba96a4dd2b29c
Seven <shiweidu@outlook.com>
2017-04-17 11:42:12 +0800

    修复会重新回滚action的bug。

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -82,7 +82,7 @@ Route::get('/follows/follows/{user_id}/{max_id?}', 'FollowController@follows');
 Route::get('/follows/followeds/{user_id}/{max_id?}', 'FollowController@followeds');
 
 // 获取一个附件资源
-tap(Route::get('/storages/{storage}/{process?}', 'StorageController@get'), function (\Illuminate\Routing\Route $route) {
+$a = tap(Route::get('/storages/{storage}/{process?}', 'StorageController@get'), function (\Illuminate\Routing\Route &$route) {
     $route->setAction(array_merge($route->getAction(), [
         'middleware' => 'bindings',
     ]));
