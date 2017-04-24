f6e83a68a038e96d08d17b5dad7737ae2e36cb99
Seven <shiweidu@outlook.com>
2017-04-24 17:08:29 +0800

    删除不存在的组件路由

--- resources/assets/admin/router/setting.js
+++ resources/assets/admin/router/setting.js
@@ -7,8 +7,6 @@
 import Setting from '../component/Setting';
 import Base from '../component/setting/Base';
 import Area from '../component/setting/Area';
-import Filter from '../component/setting/Filter';
-import AppTheme from '../component/setting/AppTheme';
 import StoreageManage from '../component/setting/StoreageManage';
 
 const settingRouter = {
@@ -23,14 +21,6 @@ const settingRouter = {
     {
       path: 'area',
       component: Area
-    },
-    {
-      path: 'filter',
-      component: Filter
-    },
-    {
-      path: 'app/theme',
-      component: AppTheme
     },
     {
       path: 'storeages',
