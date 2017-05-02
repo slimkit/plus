4156d94a055d2d2faf998bf6573ad2786a4169aa
Seven <shiweidu@outlook.com>
2017-05-02 16:31:38 +0800

    增加找回密码单元测试。

--- tests/Feature/APIs/V1/AuthTest.php
+++ tests/Feature/APIs/V1/AuthTest.php
@@ -2,6 +2,7 @@
 
 namespace Zhiyi\Plus\Tests\Feature\APIs\V1;
 
+use Zhiyi\Plus\Models\User;
 use Illuminate\Http\Request;
 use Zhiyi\Plus\Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
@@ -92,5 +93,26 @@ class AuthTest extends TestCase
         $response = $this->json('PATCH', '/api/v1/auth');
 
         $response->assertExactJson(['status' => true]);
+    }
+
+    /**
+     * 测试找回密码.
+     *
+     * @return void
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function testForgotPassword()
+    {
+        $user = factory(User::class)->make();
+
+        $this->app->make(AuthController::class)->method('forgotPassword')
+            ->will($this->returnCallback(function () use ($user) {
+                return response()->json($user)->setStatusCode(201);
+            }));
+
+        $response = $this->json('patch', '/api/v1/auth/forgot');
+
+        $response->assertStatus(201)
+            ->assertJson($user->toArray());
     }
 }
