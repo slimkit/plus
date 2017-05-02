54719f6ee1a082765b1657912658949a46d44fda
Seven <shiweidu@outlook.com>
2017-05-02 15:16:29 +0800

    update user model Factories.

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -16,9 +16,13 @@ $factory->define(Zhiyi\Plus\Models\User::class, function (Faker\Generator $faker
     static $password;
 
     return [
-        'name'           => $faker->name,
-        'email'          => $faker->unique()->safeEmail,
-        'password'       => $password ?: $password = bcrypt('secret'),
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'phone' => $faker->unique()->phoneNumber,
+        'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+        'created_at' => $faker->dateTime,
+        'updated_at' => $faker->dateTime,
+        'deleted_at' => null,
     ];
 });
