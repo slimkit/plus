86431eef8c5de56953cc26d7136f3f7481e0b6ca
Seven <shiweidu@outlook.com>
2017-05-02 14:11:01 +0800

    增加 auth 单元测试。

---
+++ tests/Feature/APIs/V1/AuthTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Zhiyi\Plus\Tests\Feature\APIs\V1;
+
+use Zhiyi\Plus\Tests\TestCase;
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Zhiyi\Plus\Http\Controllers\APIs\V1\AuthController;
+
+class AuthTest extends TestCase
+{
+    use WithoutMiddleware;
+
+    // protected $controller;
+
+    // protected $json = ['status' => true];
+
+    /**
+     * 前置操作
+     *
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        // mock controller.
+        $controller = $this->getMockBuilder(AuthController::class)
+            ->setMethods(['login', 'resetToken', 'register', 'forgotPassword'])
+            ->getMock();
+
+        $this->instance(AuthController::class, $controller);
+    }
+
+    /**
+     * Test login api.
+     *
+     * @return void
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function testLogin()
+    {
+        // create login method.
+        $this->app->make(AuthController::class)->method('login')
+            ->will($this->returnCallback(function (Request $request) {
+                return response()->json([
+                    'phone' => $request->input('phone'),
+                    'password' => $request->input('password'),
+                ])->setStatusCode(201);
+            }));
+
+        // create data.
+        $data = [
+            'phone' => '17000000000',
+            'password' => '123456',
+        ];
+
+        // request
+        $response = $this->json('POST', 'api/v1/auth', $data);
+
+        // Assert
+        $response->assertStatus(201)
+            ->assertExactJson($data);
+    }
+}
