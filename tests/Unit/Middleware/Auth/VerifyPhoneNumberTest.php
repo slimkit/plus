72a9658288e09e5a2141938a6488f6c51e808023
zhiyicx <lovevipdsw@vip.qq.com>
2017-05-10 01:26:58 +0000

    Apply fixes from StyleCI

--- tests/Unit/Middleware/Auth/VerifyPhoneNumberTest.php
+++ tests/Unit/Middleware/Auth/VerifyPhoneNumberTest.php
@@ -2,35 +2,35 @@
 
 namespace Zhiyi\Plus\Unit\Middleware\Auth;
 
-use Zhiyi\Plus\Tests\TestCase;
 use Illuminate\Http\Request;
+use Zhiyi\Plus\Tests\TestCase;
 use Illuminate\Foundation\Testing\TestResponse;
 use Zhiyi\Plus\Http\Middleware\VerifyPhoneNumber;
 
 class VerifyPhoneNumberTest extends TestCase
 {
-	/**
-	 * test verify phone number
-	 *
-	 * @author bs<414606094@qq.com>
-	 */
-	public function testHandle()
-	{
-		$request = $this->getMockBuilder(Request::class)
-			->setMethods(['all'])
-			->getMock();
+    /**
+     * test verify phone number.
+     *
+     * @author bs<414606094@qq.com>
+     */
+    public function testHandle()
+    {
+        $request = $this->getMockBuilder(Request::class)
+            ->setMethods(['all'])
+            ->getMock();
 
-		$request->expects($this->any())
-			->method('all')
-			->willReturn([
-				'phone' => '',
-			]);
+        $request->expects($this->any())
+            ->method('all')
+            ->willReturn([
+                'phone' => '',
+            ]);
 
-		$response = TestResponse::fromBaseResponse(
-            with(new VerifyPhoneNumber())->handle($request, function () {})
+        $response = TestResponse::fromBaseResponse(
+            with(new VerifyPhoneNumber())->handle($request, function () {
+            })
         );
 
         $response->assertStatus(403);
-
-	}
+    }
 }
--- tests/Unit/Middleware/Auth/VerifySendPhoneCodeTypeTest.php
+++ tests/Unit/Middleware/Auth/VerifySendPhoneCodeTypeTest.php
@@ -2,34 +2,34 @@
 
 namespace Zhiyi\Plus\Unit\Middleware\Auth;
 
-use Zhiyi\Plus\Tests\TestCase;
 use Illuminate\Http\Request;
+use Zhiyi\Plus\Tests\TestCase;
 use Illuminate\Foundation\Testing\TestResponse;
 use Zhiyi\Plus\Http\Middleware\VerifyPhoneNumber;
 
 class VerifySendPhoneCodeTypeTest extends TestCase
 {
-	/**
-	 * test verify phone number
-	 *
-	 * @author bs<414606094@qq.com>
-	 */
-	public function testHandle()
-	{
-		$request = $this->getMockBuilder(Request::class)
-			->setMethods(['post'])
-			->getMock();
+    /**
+     * test verify phone number.
+     *
+     * @author bs<414606094@qq.com>
+     */
+    public function testHandle()
+    {
+        $request = $this->getMockBuilder(Request::class)
+            ->setMethods(['post'])
+            ->getMock();
 
-		$request->expects($this->any())
-			->method('post')
-			->willReturn([
-				'type' => 'test',
-			]);
+        $request->expects($this->any())
+            ->method('post')
+            ->willReturn([
+                'type' => 'test',
+            ]);
 
-		$response = TestResponse::fromBaseResponse(
-            with(new VerifyPhoneNumber())->handle($request, function () {})
+        $response = TestResponse::fromBaseResponse(
+            with(new VerifyPhoneNumber())->handle($request, function () {
+            })
         );
         $response->assertStatus(403);
-
-	}
+    }
 }
