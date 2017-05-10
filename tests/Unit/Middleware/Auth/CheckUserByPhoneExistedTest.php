abc4170c43f86f8e7bc9fc6b939e7a5fb950fc51
白石 <414606094@qq.com>
2017-05-10 15:03:12 +0800

    测试检测用户是否存在中间件

---
+++ tests/Unit/Middleware/Auth/CheckUserByPhoneExistedTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Zhiyi\Plus\Unit\Middleware\Auth;
+
+use Illuminate\Http\Request;
+use Zhiyi\Plus\Tests\TestCase;
+use Zhiyi\Plus\Models\User;
+use Illuminate\Foundation\Testing\TestResponse;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Zhiyi\Plus\Http\Middleware\CheckUserByPhoneExisted;
+
+class CheckUserByPhoneExistedTest extends TestCase
+{
+    use DatabaseTransactions;
+    /**
+     * test check user with phone exited.
+     *
+     * @author bs<414606094@qq.com>
+     */
+    public function testHandle()
+    {
+        $user = factory(User::class)->make();
+
+        $phone = $user->phone;
+        $user->delete();
+
+        $request = $this->getMockBuilder(Request::class)
+            ->setMethods(['input'])
+            ->getMock();
+
+        $request->expects($this->any())
+            ->method('input')
+            ->with('phone')
+            ->willReturn($phone);
+        $response = TestResponse::fromBaseResponse(
+            with(new CheckUserByPhoneExisted())->handle($request, function () {
+            })
+        );
+        $response->assertStatus(404);
+    }
+}
