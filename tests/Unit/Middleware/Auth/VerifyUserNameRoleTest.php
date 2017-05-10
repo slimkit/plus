ac2c2a0406b9c20a6ab59a93d226ffa6c1640031
白石 <414606094@qq.com>
2017-05-10 15:20:36 +0800

    测试检测用户名中间件

---
+++ tests/Unit/Middleware/Auth/VerifyUserNameRoleTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Zhiyi\Plus\Unit\Middleware\Auth;
+
+use Illuminate\Http\Request;
+use Zhiyi\Plus\Tests\TestCase;
+use Zhiyi\Plus\Models\User;
+use Illuminate\Foundation\Testing\TestResponse;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Zhiyi\Plus\Http\Middleware\VerifyUserNameRole;
+
+class VerifyUserNameRoleTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /**
+     * test min length.
+     *
+     * @author bs<414606094@qq.com>
+     */
+    public function testMinLengthName()
+    {
+        $request = $this->mockRequestName('123');
+
+        $response = TestResponse::fromBaseResponse(
+            with(new VerifyUserNameRole())->handle($request, function () {
+            })
+        );
+        $response->assertStatus(403);
+    }
+
+    /**
+     * test max length.
+     *
+     * @author bs<414606094@qq.com>
+     */
+    public function testMaxLengthName()
+    {
+        $request = $this->mockRequestName('123123123123123123123123123123123123123123123123123123123123123123123123123123123');
+
+        $response = TestResponse::fromBaseResponse(
+            with(new VerifyUserNameRole())->handle($request, function () {
+            })
+        );
+        $response->assertStatus(403);
+    }
+
+    protected function mockRequestName($name):Request
+    {
+        $request = $this->getMockBuilder(Request::class)
+            ->setMethods(['input'])
+            ->getMock();
+
+        $request->expects($this->any())
+            ->method('input')
+            ->with('name')
+            ->willReturn($name);
+
+        return $request;
+    }
+}
