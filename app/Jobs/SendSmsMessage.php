118fa252ac75c72e7c6bc611b8e5b1b39b5521df
Seven <shiweidu@outlook.com>
2017-05-11 17:56:21 +0800

    增加发送短信的job

---
+++ app/Jobs/SendSmsMessage.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Zhiyi\Plus\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class SendSmsMessage implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        //
+    }
+}
