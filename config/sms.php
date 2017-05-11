9499c9c224dd0f3450d7e9ea8b8af19fab6a66c6
Seven <shiweidu@outlook.com>
2017-05-11 17:36:59 +0800

    增加短信配置文件

---
+++ config/sms.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | SMS Driver
+    |--------------------------------------------------------------------------
+    |
+    | ThinkSNS + through the PHP design SMS send function as a driver to send 
+    | text messages. Your program uses "testing" to send text messages by default.
+    |
+    | Supported: "testing",
+    |
+    */
+
+    'default' => 'testing',
+
+    'aliases' => [
+        'testing' => '开发测试（不发送）',
+        'alidayu' => '阿里大鱼',
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | SMS Connections
+    |--------------------------------------------------------------------------
+    |
+    | Here you can define the connection to send text messages, each available
+    | transmission methods are defined here.
+    |
+    */
+
+    'connections' => [
+        'alidayu' => [
+            'app_key' => null,
+            'app_secret' => null,
+            'sandbox' => false,
+            'sign_name' => null,
+            'verify_template_id' => null,
+        ],
+    ]
+];
