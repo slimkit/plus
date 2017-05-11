b5414154eb12496c939395bae556fa83153c0262
Seven <shiweidu@outlook.com>
2017-05-11 14:32:39 +0800

    增加配置加载启动器注释

--- app/Bootstrap/LoadConfiguration.php
+++ app/Bootstrap/LoadConfiguration.php
@@ -10,12 +10,23 @@ class LoadConfiguration
     protected $app;
     protected $configuration;
 
+    /**
+     * 加载配置构造方法.
+     *
+     * @author Seven Du <shiweidu@outlook.com>
+     */
     public function __construct(Application $app, Configuration $configuration)
     {
         $this->app = $app;
         $this->configuration = $configuration;
     }
 
+    /**
+     *  Run handler.
+     *
+     * @return void
+     * @author Seven Du <shiweidu@outlook.com>
+     */
     public function handle()
     {
         static $loaded = false;
