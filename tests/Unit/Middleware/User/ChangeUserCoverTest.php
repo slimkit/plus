f676f48ccb5dcac48aa0f947641f0457901d9c5d
Seven <shiweidu@outlook.com>
2017-05-12 16:52:38 +0800

    更新单元测试。

--- tests/Unit/Middleware/User/ChangeUserCoverTest.php
+++ tests/Unit/Middleware/User/ChangeUserCoverTest.php
@@ -2,6 +2,7 @@
 
 namespace Zhiyi\Plus\Unit\Middleware\User;
 
+use Closure;
 use Zhiyi\Plus\Models\User;
 use Illuminate\Http\Request;
 use Zhiyi\Plus\Tests\TestCase;
@@ -16,94 +17,44 @@ class ChangeUserCoverTest extends TestCase
     use DatabaseTransactions;
 
     /**
-     * test middleware with empty task.
+     * Test handle method.
      *
-     * @author bs<414606094@qq.com>
+     * @return void
+     * @author Seven Du <shiweidu@outlook.com>
      */
-    public function testEmptyTask()
+    public function testHanle()
     {
-        $storageTask = factory(StorageTask::class)->create();
-        $request = $this->getMockBuilder(Request::class)
-            ->setMethods(['input'])
+        // Create ChangeUserCover::class mock.
+        $changeUserCover = $this->getMockBuilder(ChangeUserCoverMiddlewareMock::class)
+            ->setMethods(['storageTaskExiste'])
             ->getMock();
+        $changeUserCover->method('storageTaskExiste')
+            ->will($this->returnValue(true));
 
-        $map = [
-            ['cover_storage_task_id', null, $storageTask->id],
-        ];
+        // Create Request::class mock.
+        $request = $this->createMock(Request::class);
+
+        // test next.
+        $changeUserCover->handle($request, Closure::bind(function () {
+            $this->assertTrue(true);
+        }, $this));
+
+        // Test storageTaskExiste next.
         $request->expects($this->any())
             ->method('input')
-            ->will($this->returnValueMap($map));
-        $storageTask->delete();
+            ->will($this->returnValue(true));
 
-        $response = TestResponse::fromBaseResponse(
-            with(new ChangeUserCover())->handle($request, function () {
-            })
+        $this->assertTrue(
+            $changeUserCover->handle($request, function () {})
         );
-
-        $response->assertStatus(403);
     }
+}
 
-    /**
-     * test change cover without avatar profile.
-     *
-     * @author bs<414606094@qq.com>
-     */
-    public function testEmptyProfile()
+// mock class.
+class ChangeUserCoverMiddlewareMock extends ChangeUserCover
+{
+    public function storageTaskExiste($storage_task_id, Request $request, Closure $next)
     {
-        $storageTask = factory(StorageTask::class)->create();
-        $user = factory(User::class)->create();
-        $request = $this->getMockBuilder(Request::class)
-            ->setMethods(['input', 'user'])
-            ->getMock();
-
-        $map = [
-            ['cover_storage_task_id', null, $storageTask->id],
-        ];
-        $request->expects($this->any())
-            ->method('input')
-            ->will($this->returnValueMap($map));
-        $request->expects($this->any())
-            ->method('user')
-            ->willReturn($user);
-
-        UserProfileSetting::where('profile', 'cover')->delete();
-
-        $response = TestResponse::fromBaseResponse(
-            with(new ChangeUserCover())->handle($request, function () {
-            })
-        );
-
-        $response->assertStatus(500);
-    }
-
-    /**
-     * test change cover without storage.
-     *
-     * @author bs<414606094@qq.com>
-     */
-    public function testEmptyStorage()
-    {
-        $storageTask = factory(StorageTask::class)->create();
-        $user = factory(User::class)->create();
-        $request = $this->getMockBuilder(Request::class)
-            ->setMethods(['input', 'user'])
-            ->getMock();
-
-        $map = [
-            ['cover_storage_task_id', null, $storageTask->id],
-        ];
-        $request->expects($this->any())
-            ->method('input')
-            ->will($this->returnValueMap($map));
-        $request->expects($this->any())
-            ->method('user')
-            ->willReturn($user);
-
-        $response = TestResponse::fromBaseResponse(
-            with(new ChangeUserCover())->handle($request, function () {
-            })
-        );
-
-        $response->assertStatus(404);
+        return parent::storageTaskExiste($storage_task_id, $request, $next);
     }
 }
