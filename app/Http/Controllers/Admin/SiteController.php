631f93f9bf0aa24a49641fb9754b62791a410821
Seven <shiweidu@outlook.com>
2017-05-12 14:52:36 +0800

    删除冗余的后台控制器代码。

--- app/Http/Controllers/Admin/SiteController.php
+++ app/Http/Controllers/Admin/SiteController.php
@@ -64,17 +64,6 @@ class SiteController extends Controller
             'description' => $description,
             'icp' => $icp,
         ])->setStatusCode(200);
-
-        // $sites = $this->newCommonConfigModel()
-        //     ->byNamespace('site')
-        //     ->whereIn('name', ['title', 'keywords', 'description', 'icp'])
-        //     ->get();
-
-        // $sites = $sites->mapWithKeys(function ($item) {
-        //     return [$item['name'] => $item['value']];
-        // });
-
-        // return response()->json($sites)->setStatusCode(200);
     }
 
     /**
@@ -107,40 +96,6 @@ class SiteController extends Controller
         return $response->json([
             'message' => '更新成功',
         ])->setStatusCode(201);
-
-        // $sites = $this->newCommonConfigModel()
-        //     ->byNamespace('site')
-        //     ->whereIn('name', $keys)
-        //     ->get()
-        //     ->keyBy('name');
-
-        // $callback = function () use ($sites, $requestSites) {
-        //     foreach ($requestSites as $name => $value) {
-        //         $model = $sites[$name] ?? false;
-        //         if (! $model) {
-        //             $model = new CommonConfig();
-        //             $model->namespace = 'site';
-        //             $model->name = $name;
-        //             $model->value = $value;
-        //             $model->save();
-        //             continue;
-        //         }
-
-        //         $this->newCommonConfigModel()
-        //             ->byNamespace('site')
-        //             ->byName($name)
-        //             ->update([
-        //                 'value' => $value,
-        //             ]);
-        //     }
-
-        //     return response()->json([
-        //         'message' => '更新成功',
-        //     ])->setStatusCode(201);
-        // };
-        // $callback->bindTo($this);
-
-        // return $this->dbTransaction($callback);
     }
 
     /**
@@ -289,38 +244,5 @@ class SiteController extends Controller
         return response()->json([
             'message' => [$key => '更新成功'],
         ])->setStatusCode(201);
-    }
-
-    /**
-     * instance a new model.
-     *
-     * @return Zhiyi\Plus\Models\CommonConfig::newQuery
-     *
-     * @author Seven Du <shiweidu@outlook.com>
-     * @homepage http://medz.cn
-     */
-    protected function newCommonConfigModel()
-    {
-        if (! $this->commonCinfigModel instanceof CommonConfig) {
-            $this->commonCinfigModel = new CommonConfig();
-        }
-
-        return $this->commonCinfigModel->newQuery();
-    }
-
-    /**
-     * Static bind DB::transaction .
-     *
-     * @param Closure $callback
-     * @param int     $attempts
-     *
-     * @return mixed
-     *
-     * @author Seven Du <shiweidu@outlook.com>
-     * @homepage http://medz.cn
-     */
-    protected function dbTransaction(Closure $callback, $attempts = 1)
-    {
-        return DB::transaction($callback, $attempts);
     }
 }
