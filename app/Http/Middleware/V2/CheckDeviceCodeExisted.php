b6a0c75b357f5ce41d207400c611e93c3afbf71e
白石 <414606094@qq.com>
2017-05-15 11:56:48 +0800

    v2版本验证设备号中间件

---
+++ app/Http/Middleware/V2/CheckDeviceCodeExisted.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Zhiyi\Plus\Http\Middleware\V2;
+
+use Closure;
+use Validator;
+
+class CheckDeviceCodeExisted
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure                 $next
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $validator = Validator::make($request->all(), [
+            'device_code' => 'required',
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => '设备号不能为空',
+            ])->setStatusCode(422);
+        }
+
+        return $next($request);
+    }
+}
