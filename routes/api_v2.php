33690129fbeb058da6b0731d5d6e62aca6da9191
白石 <414606094@qq.com>
2017-05-15 09:44:35 +0800

    增加v2接口文档 示例

--- routes/api_v2.php
+++ routes/api_v2.php
@@ -1,2 +1,16 @@
 <?php
 
+use Zhiyi\Plus\Http\Middleware;
+
+// 获取手机验证码
+Route::post('/auth/phone/code', 'AuthController@sendPhoneCode')
+    ->middleware(Middleware\VerifyPhoneNumber::class) // 验证手机号格式是否正确
+    ->middleware(Middleware\VerifySendPhoneCodeType::class) // 验证发送验证码类型等集合
+;
+
+// 用户登录
+Route::post('/auth', 'AuthController@login')
+    ->middleware(Middleware\CheckDeviceCodeExisted::class) // 验证设备号是否存在
+    ->middleware(Middleware\VerifyPhoneNumber::class) // 验证手机号码是否正确
+    ->middleware(Middleware\CheckUserByPhoneExisted::class) // 验证手机号码用户是否存在
+;
