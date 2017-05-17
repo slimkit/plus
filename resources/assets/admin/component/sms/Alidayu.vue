1962043548ac1078e393a68c1df818240bdbed02
Seven <shiweidu@outlook.com>
2017-05-17 11:32:24 +0800

    完成数据加载和提交效果设计。

--- resources/assets/admin/component/sms/Alidayu.vue
+++ resources/assets/admin/component/sms/Alidayu.vue
@@ -14,7 +14,7 @@
         <div class="form-group">
           <label for="app-key" class="col-sm-2 control-label">App Key</label>
           <div class="col-sm-4">
-            <input type="text" name="app_key" class="form-control" id="app-key" placeholder="请输入应用 AppKey" aria-describedby="app-key-help">
+            <input type="text" name="app_key" class="form-control" id="app-key" placeholder="请输入应用 AppKey" aria-describedby="app-key-help" v-model="options.app_key">
           </div>
           <div class="col-sm-6">
             <span id="app-key-help" class="help-block">输入应用 App Key 信息</span>
@@ -24,7 +24,7 @@
         <div class="form-group">
           <label for="app-secret" class="col-sm-2 control-label">App Secret</label>
           <div class="col-sm-4">
-            <input type="text" name="app_secret" class="form-control" id="app-secret" placeholder="请输入应用 App Secret" aria-describedby="app-secret-help">
+            <input type="text" name="app_secret" class="form-control" id="app-secret" placeholder="请输入应用 App Secret" aria-describedby="app-secret-help" v-model="options.app_secret">
           </div>
           <div class="col-sm-6">
             <span id="app-secret-help" class="help-block">输入应用 App Secret 信息</span>
@@ -34,7 +34,7 @@
         <div class="form-group">
           <label class="col-sm-2 control-label" for="sign-name">短信签名</label>
           <div class="col-sm-4">
-            <input type="text" name="sign_name" class="form-control" id="sign-name" placeholder="请输入短信签名名称" aria-describedby="sign-name-help">
+            <input type="text" name="sign_name" class="form-control" id="sign-name" placeholder="请输入短信签名名称" aria-describedby="sign-name-help" v-model="options.sign_name">
           </div>
           <div class="col-sm-6">
             <span class="help-block" id="sign-name-help">请输入短信签名的名称</span>
@@ -44,7 +44,7 @@
         <div class="form-group">
           <label class="col-sm-2 control-label" for="template-id">模板ID</label>
           <div class="col-sm-4">
-            <input type="text" name="template_id" class="form-control" id="template-id" placeholder="请输入短信模板id" aria-describedby="template-id-help">
+            <input type="text" name="template_id" class="form-control" id="template-id" placeholder="请输入短信模板id" aria-describedby="template-id-help" v-model="options.verify_template_id">
           </div>
           <div class="col-sm-6">
             <span class="help-block" id="template-id-help">请输入短信模板id</span>
@@ -53,30 +53,64 @@
         <!-- button -->
         <div class="form-group">
           <div class="col-sm-offset-2 col-sm-4">
-            <button type="button" class="btn btn-primary">提交</button>
+            <button v-if="submit.state === true" class="btn btn-primary" type="submit" disabled="disabled">
+              <span class="glyphicon glyphicon-refresh component-loadding-icon"></span>
+              提交...
+            </button>
+            <button v-else type="button" class="btn btn-primary" @click.stop.prevent="submitHandle">提交</button>
           </div>
           <div class="col-sm-6 help-block">
-            <span class="text-">哈哈</span>
+            <span class="text-">{{ submit.message }}</span>
           </div>
         </div>
       </div>
       <!-- Loading Error -->
       <div v-else class="panel-body">
         <div class="alert alert-danger" role="alert">{{ loadding.message }}</div>
-        <button type="button" class="btn btn-primary">刷新</button>
+        <button type="button" class="btn btn-primary" @click.stop.prevent="request">刷新</button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import request, { createRequestURI } from '../../util/request';
+
 const AlidayuComponent = {
   data: () => ({
     loadding: {
-      state: 2,
+      state: 0,
       message: '',
     },
-  })
+    submit: {
+      state: false,
+      type: 'muted',
+      message: '',
+    },
+    options: {},
+  }),
+  methods: {
+    request() {
+      this.loadding.state = 0;
+      request.get(
+        createRequestURI('sms/driver/alidayu'),
+        { validateStatus: status => status === 200 }
+      ).then(({ data = {} }) => {
+        this.loadding.state = 1;
+        this.options = data;
+      }).catch(({ response: { data: { message: [ message = '加载失败' ] = [] } = {} } = {} }) => {
+        this.loadding.state = 2;
+        this.loadding.message = message;
+      });
+    },
+    submitHandle() {
+      const { app_key = null, app_secret = null, sign_name = null, verify_template_id = null } = this.options;
+      this.submit.state = true;
+    }
+  },
+  created() {
+    window.setTimeout(() => this.request(), 500);
+  }
 };
 
 export default AlidayuComponent;
