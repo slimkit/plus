6883da22bed2e078322f0b46c8bbc79e179f230b
Seven <shiweidu@outlook.com>
2017-05-22 18:51:56 +0800

    完成批量用户获取接口.

--- app/Http/Controllers/APIs/V2/UserController.php
+++ app/Http/Controllers/APIs/V2/UserController.php
@@ -4,6 +4,7 @@ namespace Zhiyi\Plus\Http\Controllers\APIs\V2;
 
 use Zhiyi\Plus\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
 use Zhiyi\Plus\Http\Controllers\Controller;
 
 class UserController extends Controller
@@ -28,9 +29,47 @@ class UserController extends Controller
         return response()->json($user, 200);
     }
 
+    /**
+     * 获取用户列表.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return mixed
+     * @author Seven Du <shiweidu@outlook.com>
+     */
     public function show(Request $request)
     {
-        //
+        $ids = array_filter(explode(',', $request->query('user')));
+        $currentUser = $request->user('api') ? $request->user('api')->id : 0;
+
+        if (empty($ids)) {
+            return response()->json([], 200);
+        }
+
+        $users = User::whereIn('id', $ids)
+            ->with([
+                'datas',
+                'followings' => function ($query) use ($currentUser) {
+                    $query->where('id', $currentUser);
+                },
+                'followers' => function ($query) use ($currentUser) {
+                    $query->where('id', $currentUser);
+                },
+            ])
+            ->get();
+
+        $users = $users->reduce(function (Collection $users, $user) {
+            $temp = new Collection($user);
+
+            $temp->pull('followings');
+            $temp->pull('followers');
+            $temp->offsetSet('following', $user->followings->isNotEmpty());
+            $temp->offsetSet('follower', $user->followers->isNotEmpty());
+
+            $users->push($temp);
+            return $users;
+        }, new Collection());
+
+        return response()->json($users, 200);
     }
 
     /**
@@ -43,7 +82,8 @@ class UserController extends Controller
      */
     protected function hasFollowing(Request $request, User &$user)
     {
-        $hasUser = (int) $request->query('following', $request->user() ?: 0);
+        $currentUser = $request->user('api');
+        $hasUser = (int) $request->query('following', $currentUser ? $currentUser->id : 0);
         $user['following'] = $user->hasFollwing($hasUser);
     }
 
@@ -57,7 +97,8 @@ class UserController extends Controller
      */
     protected function hasFollower(Request $request, User &$user)
     {
-        $hasUser = (int) $request->query('follower', $request->user() ?: 0);
+        $currentUser = $request->user('api');
+        $hasUser = (int) $request->query('follower', $currentUser ? $currentUser->id : 0);
         $user['follower'] = $user->hasFollower($hasUser);
     }
 }
