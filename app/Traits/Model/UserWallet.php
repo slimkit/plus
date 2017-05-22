93c9e9eb881c836afc0c41039274064cbacc9287
Seven <shiweidu@outlook.com>
2017-05-22 13:21:42 +0800

    增加钱包关系。

--- app/Traits/Model/UserWallet.php
+++ app/Traits/Model/UserWallet.php
@@ -2,6 +2,19 @@
 
 namespace Zhiyi\Plus\Traits\Model;
 
+use Zhiyi\Plus\Models\Wallet;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
 trait UserWallet
 {
+    /**
+     * 用户钱包.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function wallet(): HasOne
+    {
+        $this->hasOne(Wallet::class, 'user_id', 'id');
+    }
 }
