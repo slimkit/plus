5ad6433d1295fd8f27e4bba33ef3fb7f25e4fb12
Seven <shiweidu@outlook.com>
2017-05-22 17:16:39 +0800

    完成关注性状开发。

--- app/Traits/Model/UserFollow.php
+++ app/Traits/Model/UserFollow.php
@@ -2,6 +2,34 @@
 
 namespace Zhiyi\Plus\Traits\Model;
 
+use Zhiyi\Plus\Models\User;
+use \Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
 trait UserFollw
 {
+    /**
+     * 正在关注 - 我关注的
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function followings(): BelongsToMany
+    {
+        return $this
+            ->belongsToMany(User::class, 'user_follow', 'user_id', 'target')
+            ->withTimestamps();
+    }
+
+    /**
+     * 关注着 - 关注我的.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function followers(): BelongsToMany
+    {
+        return $this
+            ->belongsToMany(User::class, 'user_follow', 'target', 'user_id')
+            ->withTimestamps();
+    }
 }
