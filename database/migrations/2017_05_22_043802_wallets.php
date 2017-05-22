71beca3c11f26a8abf3fb513323e4f51cb595fbe
Seven <shiweidu@outlook.com>
2017-05-22 13:03:37 +0800

    完成钱包迁移创建。

---
+++ database/migrations/2017_05_22_043802_wallets.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Wallets extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wallets', function (Blueprint $table) {
+            // 主键
+            $table
+                ->bigIncrements('id')
+                ->comment('钱包ID');
+
+            // 关联用户
+            $table
+                ->integer('user_id')
+                ->unsigned()
+                ->comment('钱包所属用户');
+
+            // 余额，单位 分 (避免小数计算float偏移)
+            $table
+                ->integer('balance', 9)
+                ->default(0)
+                ->comment('钱包余额');
+            $table->timestamps(); // 自动维护时间
+            $table->softDeletes(); // 软删除
+
+            // 外健约束
+            $table
+                ->foreign('user_id')
+                ->references('id')
+                ->on('users');
+
+            // 唯一健设置
+            $table->primary('user_id');
+
+            // 加入余额到普通索引，加速查找和sql命中.
+            $table->index('balance');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wallets');
+    }
+}
