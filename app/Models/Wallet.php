5e1a4943c529e0421a327fe004db85b699336de2
Seven <shiweidu@outlook.com>
2017-05-22 13:15:51 +0800

    增加钱包用户关系。

--- app/Models/Wallet.php
+++ app/Models/Wallet.php
@@ -3,8 +3,18 @@
 namespace Zhiyi\Plus\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Wallet extends Model
 {
-    //
+    /**
+     * 获取钱包所属用户.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function user(): HasOne
+    {
+        $this->hasOne(User::class, 'id', 'user_id');
+    }
 }
