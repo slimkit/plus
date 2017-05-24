0a0c953830e2be8d97e6965038133ca5e807215e
Seven <shiweidu@outlook.com>
2017-05-24 18:04:06 +0800

    完成钱包充值选项获取接口。

--- app/Http/Controllers/Admin/WalletSettingController.php
+++ app/Http/Controllers/Admin/WalletSettingController.php
@@ -2,6 +2,32 @@
 
 namespace Zhiyi\Plus\Http\Controllers\Admin;
 
-class WalletSettingController
+// use Illuminate\Http\Request;
+use Zhiyi\Plus\Models\CommonConfig;
+use Zhiyi\Plus\Http\Controllers\Controller;
+
+class WalletSettingController extends Controller
 {
+    /**
+     * Get wallet labels.
+     *
+     * @return mixed
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function labels()
+    {
+        $labels = CommonConfig::byNamespace('wallet')
+            ->byName('labels')
+            ->first();
+
+        if (! $labels) {
+            return response()
+                ->json([], 200);
+        }
+
+        return response()
+            ->json()
+            ->setJson($labels->value);
+    }
 }
+
