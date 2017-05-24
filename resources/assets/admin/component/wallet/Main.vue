6b27926a2dccc2bec7a8508698e6f9701e6a859c
Seven <shiweidu@outlook.com>
2017-05-24 11:11:54 +0800

    增加导航布局选项。

--- resources/assets/admin/component/wallet/Main.vue
+++ resources/assets/admin/component/wallet/Main.vue
@@ -2,14 +2,27 @@
   <div>
     <ul class="nav nav-tabs component-controller-nav">
       <router-link to="/wallet" tag="li" active-class="active" exact>
-        <a href="#">统计信息</a>
+        <a href="#">统计</a>
       </router-link>
       <router-link to="/wallet/accounts" tag="li" active-class="active" exact>
-        <a href="#">流水记录</a>
+        <a href="#">流水</a>
       </router-link>
-      <router-link to="/wallet/setting" tag="li" active-class="active" exact>
-        <a href="#">基础设置</a>
+      <router-link to="/wallet/cash" tag="li" active-class="active" exact>
+        <a href="#">提现审批</a>
       </router-link>
+      <li role="presentation" class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+          基础设置 <span class="caret"></span>
+        </a>
+        <ul class="dropdown-menu">
+          <li>
+            <a href="#">充值选项</a>
+          </li>
+          <li>
+            <a href="#">规则设置</a>
+          </li>
+        </ul>
+      </li>
       <li role="presentation" class="dropdown">
         <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
           支付设置 <span class="caret"></span>
