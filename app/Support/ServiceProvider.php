d51bafc79096faa6f9629676176a74809b2e8464
Seven <shiweidu@outlook.com>
2017-05-27 13:11:37 +0800

    增加服务提供者默认服务处理。

--- app/Support/ServiceProvider.php
+++ app/Support/ServiceProvider.php
@@ -21,4 +21,42 @@ abstract class ServiceProvider extends BaseServiceProvider
 
         $this->app->make(ProviderRepository::class)->load($providers);
     }
+
+    /**
+     * Service provider list handle.
+     *
+     * @param \Illuminate\Console\Command $command
+     * @return mixed
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function listHandle($command)
+    {
+        $command->comment('Usage:');
+        $command->line(sprintf('  package:run %s [<handle>]', $command->argument('package')));
+        $command->line('');
+        $command->comment('Available Handles:');
+        foreach (get_class_methods($this) as $method) {
+            if (strtolower(substr($method, -6)) === 'handle') {
+                $command->info(sprintf('  - %s', substr($method, 0, -6)));
+            }
+        }
+    }
+
+    /**
+     * Service provider default handle.
+     *
+     * @param \Illuminate\Console\Command $command
+     * @return mixed
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function defaultHandle($command)
+    {
+        $command->call(
+            'package:run',
+            [
+                'package' => $command->argument('package'),
+                'handle' => 'list'
+            ]
+        );
+    }
 }
