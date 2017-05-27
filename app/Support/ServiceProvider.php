4b50f06a65605522d4a5915a2e43f06f7be196f4
Seven <shiweidu@outlook.com>
2017-05-27 17:33:37 +0800

    对后台导航的注入。

--- app/Support/ServiceProvider.php
+++ app/Support/ServiceProvider.php
@@ -6,6 +6,55 @@ use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 
 abstract class ServiceProvider extends BaseServiceProvider
 {
+    protected static $manages = [];
+
+    /**
+     * Get the manages for the provider.
+     *
+     * @return array
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public static function getManages(): array
+    {
+        $manages = [];
+        foreach (static::$manages as $item) {
+            $name = $item['name'];
+            $uri = $item['uri'];
+            $option = $item['option'];
+
+            $isRoute = $option['route'] ?? false;
+            $parameters = (array) ($option['parameters'] ?? []);
+            $absolute = $option['absolute'] ?? true;
+            $icon = $option['icon'] ?? null;
+
+            $manages[] = [
+                'name' => $name,
+                'icon' => $icon,
+                'uri' => ! $isRoute ? $uri : route($uri, $parameters, $absolute),
+            ];
+        }
+
+        return $manages;
+    }
+
+    /**
+     * Push manage url.
+     *
+     * @param string $name
+     * @param string $uri
+     * @param array $option
+     * @return void
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function loadManageFrom(string $name, string $uri, array $option = [])
+    {
+        static::$manages[] = [
+            'name' => $name,
+            'uri' => $uri,
+            'option' => $option
+        ];
+    }
+
     /**
      * Load service provider.
      *
