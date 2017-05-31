aae9673d637b8ae16d28393c7bb455b207a46ad5
Seven <shiweidu@outlook.com>
2017-05-31 17:30:52 +0800

    完成后台提现设置。

--- resources/assets/admin/component/wallet/Cash.vue
+++ resources/assets/admin/component/wallet/Cash.vue
@@ -4,7 +4,7 @@
       <!-- title -->
       <div class="panel-heading">
         提现审批
-        <router-link to="setting" class="pull-right" append>
+        <router-link to="setting" class="pull-right" append replace>
           <span class="glyphicon glyphicon-cog"></span>
           提现设置
         </router-link>
--- resources/assets/admin/component/wallet/CashSetting.vue
+++ resources/assets/admin/component/wallet/CashSetting.vue
@@ -1,3 +1,161 @@
+<style lang="scss" module>
+.alert {
+  margin: 22px 0 0;
+}
+</style>
+
 <template>
-  <div>提现设置</div>
+  <div class="component-container container-fluid">
+    <div class="panel panel-default">
+      <!-- title -->
+      <div class="panel-heading">
+        <router-link to="/wallet/cash" replace>
+          <span class="glyphicon glyphicon-menu-left"></span>
+          返回
+        </router-link>
+        <span class="pull-right" >提现审批 - 提现设置</span>
+      </div>
+
+      <!-- Loading -->
+      <div v-if="load.status === 0" class="panel-body text-center">
+        <span class="glyphicon glyphicon-refresh component-loadding-icon"></span>
+        加载中...
+      </div>
+
+      <!-- Body -->
+      <div v-else-if="load.status === 1" class="panel-body form-horizontal">
+        <!-- 提现方式 -->
+        <div class="form-group">
+          <label class="col-sm-2 control-label">提现方式</label>
+          <div class="col-sm-4">
+            <div class="checkbox">
+              <label>
+                <input type="checkbox" value="alipay" v-model="cashType"> 支付宝
+              </label>
+            </div>
+            <div class="checkbox">
+              <label>
+                <input type="checkbox" value="wechat" v-model="cashType"> 微信
+              </label>
+            </div>
+          </div>
+          <div class="col-sm-6">
+            <span class="help-block">选择用户提现支持的提现方式，如果都不勾选，则表示关闭提现功能。</span>
+          </div>
+        </div>
+
+        <!-- 提交按钮 -->
+        <div class="form-group">
+          <div class="col-sm-offset-2 col-sm-4">
+            <button v-if="update === true" class="btn btn-primary" type="submit" disabled="disabled">
+              <span class="glyphicon glyphicon-refresh component-loadding-icon"></span>
+              提交...
+            </button>
+            <button v-else type="button" class="btn btn-primary" @click="updateHandle">提交</button>
+          </div>
+        </div>
+
+        <!-- 警告框 -->
+        <div v-show="alert.open" :class="['alert', `alert-${alert.type}`, $style.alert]" role="alert">
+          {{ alert.message }}
+        </div>
+
+      </div>
+
+      <!-- Loading Error. -->
+      <div v-else class="panel-body">
+        <div class="alert alert-danger" role="alert">{{ load.message }}</div>
+        <button type="button" class="btn btn-primary" @click="requestCashType">刷新</button>
+      </div>
+
+    </div>
+  </div>
 </template>
+
+<script>
+import request, { createRequestURI } from '../../util/request';
+export default {
+  data: () => ({
+    alert: {
+      open: false,
+      interval: null,
+      type: 'info',
+      message: null,
+    },
+    cashType: [],
+    load: {
+      status: 0,
+      message: '',
+    },
+    update: false,
+  }),
+  methods: {
+    /**
+     * 发送提示.
+     *
+     * @param {string} type
+     * @param {string} message
+     * @return {void}
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    sendAlert(type, message) {
+      window.clearInterval(this.alert.interval);
+      this.alert = {
+        ...this.alert,
+        type,
+        message,
+        open: true,
+        interval: window.setInterval(() => {
+          window.clearInterval(this.alert.interval);
+          this.alert.open = false;
+        }, 2000)
+      };
+    },
+
+    /**
+     * 请求提现方式数据.
+     *
+     * @return {void}
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    requestCashType() {
+      request.get(
+        createRequestURI('wallet/cash/type'),
+        { validateStatus: status => status === 200 }
+      ).then(({ data = [] }) => {
+        this.cashType = data;
+        this.load.status = 1;
+      }).catch(({ response: { data: { message: [ message = '加载失败' ] = [] } = {} } = {} }) => {
+        this.load = {
+          message,
+          status: 2
+        };
+      });
+    },
+
+    /**
+     * 更新设置.
+     *
+     * @return {void}
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    updateHandle() {
+      this.update = true;
+      request.patch(
+        createRequestURI('wallet/cash/type'),
+        { types: this.cashType },
+        { validateStatus: status => status === 201 }
+      ).then(({ data: { message: [ message = '更新成功' ] = [] } = {} }) => {
+        this.update = false;
+        this.sendAlert('success', message);
+      }).catch(({ response: { data: { message: [ message = '更新失败，请刷新重试' ] = [] } = {} } = {} }) => {
+        this.update = false;
+        this.sendAlert('danger', message);
+      });
+    },
+  },
+  created() {
+    this.requestCashType();
+  }
+}
+</script>
