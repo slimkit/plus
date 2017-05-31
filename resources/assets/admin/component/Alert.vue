bb7f8f1ff6ebc2b37d3269207ce2e10181ea652b
Seven <shiweidu@outlook.com>
2017-05-31 18:30:34 +0800

    增加公用弹出框组件。

---
+++ resources/assets/admin/component/Alert.vue
@@ -0,0 +1,43 @@
+<style lang="scss" module>
+.alert {
+  margin: 22px 0 0;
+}
+</style>
+
+<template>
+  <div v-show="show" :class="['alert', `alert-${type}`, $style.alert]" role="alert">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    open: {
+      type: Boolean,
+      default: false
+    },
+    type: {
+      type: String,
+      validator: value => ['success', 'info', 'warning', 'danger'].indexOf(value),
+      default: 'info',
+    },
+    class: [String, Array]
+  },
+  data: () => ({
+    show: false
+    interval: null
+  }),
+  watch: {
+    open: function (open) {
+      if (open === true) {
+        window.clearInterval(this.interval);
+        this.interval = window.setInterval(() => {
+          this.open = false;
+          window.clearInterval(this.interval);
+        }, 1500);
+      }
+    }
+  }
+};
+</script>
