6061fcf398ae766b3b2c37eb09b7f70fed1529c8
Seven <shiweidu@outlook.com>
2017-05-31 16:00:00 +0800

    增加提现审批基础UI。

--- resources/assets/admin/component/wallet/Cash.vue
+++ resources/assets/admin/component/wallet/Cash.vue
@@ -1,3 +1,14 @@
 <template>
-  <div>提现审批</div>
+  <div class="component-container container-fluid">
+    <div class="panel panel-default">
+      <!-- title -->
+      <div class="panel-heading">
+        提现审批
+        <router-link to="setting" class="pull-right" append>
+          <span class="glyphicon glyphicon-cog"></span>
+          提现设置
+        </router-link>
+      </div>
+    </div>
+  </div>
 </template>
