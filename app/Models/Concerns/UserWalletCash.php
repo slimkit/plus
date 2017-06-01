de1a792d06aa24980a569e9af8e5a1b3ccfc95dd
Seven <shiweidu@outlook.com>
2017-06-01 17:32:50 +0800

    增加用户数据模型的提现记录关系。

---
+++ app/Models/Concerns/UserWalletCash.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Zhiyi\Plus\Models\Concerns;
+
+use Zhiyi\Plus\Models\WalletCash;
+
+trait UserWalletCash
+{
+    /**
+     * Wallet cshs.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function walletCashs()
+    {
+        return $this->hasMany(WalletCash::class, 'user_id', 'id');
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -15,6 +15,7 @@ class User extends Authenticatable
     use Notifiable,
         SoftDeletes,
         UserWallet,
+        Concerns\UserWalletCash,
         UserFollw;
     use UserRolePerms {
         SoftDeletes::restore insteadof UserRolePerms;
