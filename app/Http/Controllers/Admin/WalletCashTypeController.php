17cfebe9b46b34a6fe62783ef44e55f8137b38f1
Seven <shiweidu@outlook.com>
2017-06-01 17:31:19 +0800

    优化后台提现类型接口

--- app/Http/Controllers/Admin/WalletCashTypeController.php
+++ app/Http/Controllers/Admin/WalletCashTypeController.php
@@ -5,6 +5,7 @@ namespace Zhiyi\Plus\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use Zhiyi\Plus\Models\CommonConfig;
 use Zhiyi\Plus\Http\Controllers\Controller;
+use Zhiyi\Plus\Repository\UserWalletCashType;
 
 class WalletCashTypeController extends Controller
 {
@@ -14,15 +15,10 @@ class WalletCashTypeController extends Controller
      * @return mixed
      * @author Seven Du <shiweidu@outlook.com>
      */
-    public function show()
+    public function show(UserWalletCashType $repository)
     {
-        $types = CommonConfig::byNamespace('wallet')
-            ->byName('cash')
-            ->value('value') ?: '[]';
-
         return response()
-            ->json()
-            ->setJson($types)
+            ->json($repository->get())
             ->setStatusCode(200);
     }
 
@@ -33,32 +29,23 @@ class WalletCashTypeController extends Controller
      * @return mexed
      * @author Seven Du <shiweidu@outlook.com>
      */
-    public function update(Request $request)
+    public function update(Request $request, UserWalletCashType $repository)
     {
-        $model = CommonConfig::firstOrNew(
-            ['name' => 'cash', 'namespace' => 'wallet'],
-            ['value' => '[]']
-        );
-
         $rules = [
-            'types' => 'array|in_array:alipay,wechat',
+            'types' => 'array|in:alipay,wechat',
         ];
         $messages = [
             'types.array' => '提交的数据有误，请刷新重试',
             'types.in_array' => '提交的数据不合法，请刷新重试',
         ];
 
-        $types = $request->input('types', []);
-        $model->value = json_encode($types);
-
-        if ($model->save()) {
-            return response()
-                ->json(['messages' => ['更新成功']])
-                ->setStatusCode(201);
-        }
+        $this->validate($request, $rules, $messages);
+        $repository->store(
+            $request->input('types', [])
+        );
 
         return response()
-            ->json(['messages' => ['更新失败']])
-            ->setStatusCode(500);
+            ->json(['messages' => ['更新成功']])
+            ->setStatusCode(201);
     }
 }
