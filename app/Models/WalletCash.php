29a86fb32063ed0c0796c87d21c66555d008e421
Seven <shiweidu@outlook.com>
2017-06-01 11:00:23 +0800

    添加提现模型。

---
+++ app/Models/WalletCash.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Zhiyi\Plus\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+// use Illuminate\Database\Eloquent\Builder;
+
+class WalletCash extends Model
+{
+    /**
+     * User.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+}
