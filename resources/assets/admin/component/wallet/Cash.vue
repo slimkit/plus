2eb0a366152e6e61a3421e96ace81347c004f4b1
Seven <shiweidu@outlook.com>
2017-06-01 10:18:59 +0800

    完成用户提现审批页面基础布局～

--- resources/assets/admin/component/wallet/Cash.vue
+++ resources/assets/admin/component/wallet/Cash.vue
@@ -9,6 +9,84 @@
           提现设置
         </router-link>
       </div>
+
+      <!-- Body -->
+      <div>
+        <div class="panel-body">
+          <!-- 搜索 -->
+          <div class="form-inline">
+            <!-- 提现用户 -->
+            <div class="form-group">
+              <label>用户：</label>
+              <input type="number" class="form-control" placeholder="User ID" min="1">
+            </div>
+
+            <!-- 状态 -->
+            <div class="form-group">
+              <label>状态</label>
+              <select class="form-control">
+                <option>全部</option>
+                <option>待审批</option>
+                <option>已审批</option>
+                <option>被拒绝</option>
+              </select>
+            </div>
+
+            <!-- 排序 -->
+            <div class="form-group">
+              <label>排序</label>
+              <select class="form-control">
+                <option>最新申请</option>
+                <option>时间排序</option>
+              </select>
+            </div>
+
+            <!-- 搜索按钮 -->
+            <button type="submit" class="btn btn-default">搜索</button>
+
+          </div>
+        </div>
+
+        <!-- 列表 -->
+        <table class="table table-striped table-hover">
+          <thead>
+            <tr>
+              <th>用户</th>
+              <th>金额</th>
+              <th>提现账户</th>
+              <th>操作</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Seven(id:1)</td>
+              <td>1500</td>
+              <td>支付宝：lovevipdsw@outlook.com</td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+
+        <!-- 分页 -->
+        <ul class="pagination">
+          <li>
+            <a href="#" aria-label="Previous">
+              <span aria-hidden="true">&laquo;</span>
+            </a>
+          </li>
+          <li><a href="#">1</a></li>
+          <li><a href="#">2</a></li>
+          <li><a href="#">3</a></li>
+          <li><a href="#">4</a></li>
+          <li><a href="#">5</a></li>
+          <li>
+            <a href="#" aria-label="Next">
+              <span aria-hidden="true">&raquo;</span>
+            </a>
+          </li>
+        </ul>
+
+      </div>
     </div>
   </div>
 </template>
