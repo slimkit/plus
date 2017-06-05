bdfbe83a9046aa352b6b1a2c4cdaf024312dc296
Seven <shiweidu@outlook.com>
2017-06-05 09:25:28 +0800

    增加管理仓库类

---
+++ app/ManageRepository.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Zhiyi\Plus\Support;
+
+class ManageRepository
+{
+    protected static $manages = [];
+
+    /**
+     * Push manage url.
+     *
+     * @param string $name
+     * @param string $uri
+     * @param array $option
+     * @return void
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public static function loadManageFrom(string $name, string $uri, array $option = [])
+    {
+        static::$manages[] = [
+            'name' => $name,
+            'uri' => $uri,
+            'option' => $option,
+        ];
+    }
+
+    /**
+     * Get the manages for the provider.
+     *
+     * @return array
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public static function getManages(): array
+    {
+        $manages = [];
+        foreach (static::$manages as $item) {
+            $name = $item['name'];
+            $uri = $item['uri'];
+            $option = $item['option'];
+
+            $isRoute = $option['route'] ?? false;
+            $parameters = (array) ($option['parameters'] ?? []);
+            $absolute = $option['absolute'] ?? true;
+            $icon = $option['icon'] ?? null;
+
+            $manages[] = [
+                'name' => $name,
+                'icon' => $icon,
+                'uri' => ! $isRoute ? $uri : route($uri, $parameters, $absolute),
+            ];
+        }
+
+        return $manages;
+    }
+}
