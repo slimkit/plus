b4dcee7237dd1b3a4ee6fb2672bffb6889f64abc
Seven <shiweidu@outlook.com>
2017-06-07 11:04:13 +0800

    增加凭据模型

---
+++ app/Models/WalletCharge.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Zhiyi\Plus\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class WalletCharge extends Model
+{
+    use SoftDeletes;
+}
