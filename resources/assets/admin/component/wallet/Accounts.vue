03e4200b702bb7f5a25e733ffe23cd6037748235
Seven <shiweidu@outlook.com>
2017-06-12 13:47:01 +0800

    完成后台记录流水开发。

--- resources/assets/admin/component/wallet/Accounts.vue
+++ resources/assets/admin/component/wallet/Accounts.vue
@@ -13,7 +13,7 @@
           <div class="col-sm-4">
             <input type="number" class="form-control" v-model="search.user">
           </div>
-          <span class="help-block col-sm-6">输入交易账户的用户 ID,如果要检索不属于用户的订单，请输入 null</span>
+          <span class="help-block col-sm-6">输入交易账户的用户 ID</span>
         </div>
 
         <!-- 交易账户 -->
@@ -22,7 +22,7 @@
           <div class="col-sm-4">
             <input type="text" class="form-control" v-model="search.account">
           </div>
-          <span class="col-sm-6 help-block">输入交易账户，应用内交易则为用户ID，例如支付宝则输入支付宝，无法获取的交易账户输入 null 查询</span>
+          <span class="col-sm-6 help-block">输入交易账户，应用内交易则为用户ID</span>
         </div>
 
         <!-- Ping++ 凭据 -->
@@ -79,12 +79,133 @@
 
       </div>
 
+      <!-- Loading -->
+      <div v-if="load.status === 0" class="panel-body text-center">
+        <span class="glyphicon glyphicon-refresh component-loadding-icon"></span>
+        加载中...
+      </div>
+
+      <!-- List table -->
+      <table v-else-if="load.status === 1" class="table table-striped table-hover">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>用户（ID）</th>
+            <th>支付频道</th>
+            <th>交易账户</th>
+            <th>Ping++</th>
+            <th>订单ID</th>
+            <th>金额</th>
+            <th>交易信息</th>
+            <th>状态</th>
+            <th>时间</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="charge in charges" :key="charge.id" :class="charge.status === 2 ? 'danger' : charge.status === 1 ? 'success' : ''">
+            <td>{{ charge.id }}</td>
+            <td>{{ resolveChargeUserDisplay(charge.user) }}</td>
+            <td>{{ resolvePayChannel(charge.channel) }}</td>
+            <td>{{ charge.account }}</td>
+            <td>{{ charge.charge_id }}</td>
+            <td>{{ charge.transaction_no }}</td>
+            <td>{{ charge.action == 1 ? '+' : '-' }}{{ charge.amount / 100 }}</td>
+            <td>
+              {{ charge.subject }}<br>
+              <small>{{ charge.body }}</small>
+            </td>
+            
+            <!-- 状态 -->
+            <td v-if="charge.status === 0">等待</td>
+            <td v-else-if="charge.status === 1">成功</td>
+            <td v-else-if="charge.status === 2">失败</td>
+            <td v-else>未知</td>
+
+            <td>{{ charge.created_at }}</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <!-- Load error -->
+      <div v-else class="panel-body">
+        <div class="alert alert-danger" role="alert">{{ load.message }}</div>
+        <button type="button" class="btn btn-primary" @click="requestRefresh">重试</button>
+      </div>
+
     </div>
+
+    <!-- 分页 -->
+    <div class="text-center" v-show="pagination.show">
+      <ul class="pagination">
+        <!-- 上一页按钮 -->
+        <router-link
+          v-show="!!pagination.isPrevPage"
+          tag="li"
+          :to="resolvePaginationRoute(pagination.isPrevPage)"
+        >
+          <a aria-label="Previous">
+            <span aria-hidden="true">&laquo;</span>
+          </a>
+        </router-link>
+
+        <!-- 前页码 -->
+        <router-link
+          v-for="item in pagination.prevPages"
+          tag="li"
+          :key="item.page"
+          :to="resolvePaginationRoute(item.page)"
+        >
+          <a>{{ item.name }}</a>
+        </router-link>
+
+        <!-- 当前页码 -->
+        <router-link class="active" tag="li" :to="resolvePaginationRoute(pagination.current)">
+          <a>
+            {{ pagination.current }}
+          </a>
+        </router-link>
+
+        <!-- 后页码 -->
+        <router-link
+          v-for="item in pagination.nextPages"
+          tag="li"
+          :key="item.page"
+          :to="resolvePaginationRoute(item.page)"
+        >
+          <a>{{ item.name }}</a>
+        </router-link>
+
+        <!-- 下一页按钮 -->
+        <router-link
+          v-show="!!pagination.isNextPage"
+          tag="li"
+          :to="resolvePaginationRoute(pagination.isNextPage)"
+        >
+          <a aria-label="Next">
+            <span aria-hidden="true">&raquo;</span>
+          </a>
+        </router-link>
+        
+      </ul>
+    </div>
+
   </div>
 </template>
 
 <script>
 import lodash from 'lodash';
+import request, { createRequestURI } from '../../util/request';
+
+const channelTypes = {
+  applepay_upacp: 'Apple Pay',
+  alipay: '支付宝 APP',
+  alipay_pc_direct: '支付宝电脑网站',
+  alipay_qr: '支付宝扫码',
+  alipay_wap: '支付宝手机网页',
+  wx: '微信 APP',
+  wx_wap: '微信 WAP'
+};
+
 export default {
   data: () => ({
     // Search state.
@@ -101,7 +222,16 @@ export default {
     page: {
       current: 1,
       total: 1,
-    }
+    },
+
+    // 凭据
+    charges: [],
+
+    // Load
+    load: {
+      status: 0,
+      message: ''
+    },
   }),
   computed: {
     /**
@@ -117,6 +247,95 @@ export default {
         }
         return search;
       }, {});
+    },
+    /**
+     * Pagination.
+     *
+     * @return {Object}
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    pagination() {
+      // 当前页
+      let current = 1;
+      current = isNaN(current = parseInt(this.page.current)) ? 1 : current;
+
+      // 最后页码
+      let last = 1;
+      last = isNaN(last = parseInt(this.page.total)) ? 1 : last;
+
+      // 是否显示
+      const show = last > 1;
+
+      // 前三页
+      let minPage = current - 3;
+      minPage = minPage <= 1 ? 1 : minPage;
+
+      // 后三页
+      let maxPage = current + 3;
+      maxPage = maxPage >= last ? last : maxPage;
+
+      // 是否有上一页
+      let isPrevPage = false;
+      // 前页页码
+      let prevPages = [];
+
+      // 前页计算
+      if (current > minPage) {
+        // 有上一页按钮
+        isPrevPage = current - 1; // 如果有，传入上一页页码.
+
+        // 回归第一页
+        if (minPage > 1) {
+          prevPages.push({
+            name: current < 6 ? 1 : '1...',
+            page: 1,
+          });
+        }
+
+        // 前页码
+        for (let i = minPage; i < current; i++) {
+          prevPages.push({
+            name: i,
+            page: i
+          });
+        }
+      }
+
+      // 是否有下一页
+      let isNextPage = false;
+      // 后页页码
+      let nextPages = [];
+
+      // 后页计算
+      if (current < maxPage) {
+        // 后页码
+        for (let i = current + 1; i <= maxPage; i++) {
+          nextPages.push({
+            name: i,
+            page: i,
+          });
+        }
+
+        // 进入最后页
+        if (maxPage < last) {
+          nextPages.push({
+            name: current + 4 === last ? last : '...'+last,
+            page: last,
+          })
+        }
+
+        // 是否有下一页按钮
+        isNextPage = current + 1;
+      }
+
+      return {
+        isPrevPage,
+        isNextPage,
+        current,
+        show,
+        prevPages,
+        nextPages,
+      }
     }
   },
   watch: {
@@ -134,6 +353,59 @@ export default {
     }, 
   },
   methods: {
+    /**
+     * Builder page route object.
+     *
+     * @param {Number} page
+     * @return {Object}
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    resolvePaginationRoute(page) {
+      return {
+        path: '/wallet/accounts',
+        query: { ...this.resolveQuery(), page }
+      };
+    },
+
+    /**
+     * 返回显示的频道类型.
+     *
+     * @param {String} channel
+     * @return {String}
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    resolvePayChannel(channel) {
+      const { [channel]: displayChannel = '余额' } = channelTypes;
+
+      return displayChannel;
+    },
+    /**
+     * 解决用户信息显示.
+     *
+     * @param {Object} user
+     * @return {String|null}
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    resolveChargeUserDisplay(user = {}) {
+      const { id, name } = user;
+      if (!! id) {
+        return `${name} (${id})`;
+      }
+
+      return null;
+    },
+    /**
+     * Request refresh.
+     *
+     * @return {void}
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    requestRefresh() {
+      this.requestCharge({
+        page: this.page.current,
+        ...this.resolveQuery(),
+      });
+    },
     /**
      * Resolve route query.
      *
@@ -174,7 +446,17 @@ export default {
      * @author Seven Du <shiweidu@outlook.com>
      */
     requestCharge(params = {}) {
-      console.log(params);
+      this.load.status = 0;
+      request.get(
+        createRequestURI('wallet/charges'),
+        { params, validateStatus: status => status === 200 }
+      ).then(({ data: { total = 1, current = 1, items = [] } }) => {
+        this.load.status = 1;
+        this.page = { total, current };
+        this.charges = items;
+      }).catch(({ response: { data: { message: [ anyMessage = '加载失败，请刷新重试' ] = [] } = {} } = {} } = {}) => {
+        this.load = { message: anyMessage, status: 2 };
+      });
     }
   },
   /**
