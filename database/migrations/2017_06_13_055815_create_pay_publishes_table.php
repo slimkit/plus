e4b5d6ca3de138e09ec54a6a20100cd08ec0b2b0
Seven <shiweidu@outlook.com>
2017-06-13 14:13:32 +0800

    增加发布付费节点记录表迁移～

---
+++ database/migrations/2017_06_13_055815_create_pay_publishes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePayPublishesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pay_publishes', function (Blueprint $table) {
+            $table->increments('id')->comment('付费记录ID');
+            $table->string('index')->comment('付费索引');
+            $table->string('subject')->comment('付费主题');
+            $table->integer('amount')->unsigned()->comment('付费金额');
+            $table->timestamps();
+
+            $table->unique('index');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pay_publishes');
+    }
+}
