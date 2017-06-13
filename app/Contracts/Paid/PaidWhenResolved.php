ab9d950896b13390b14e23ec371bc5883b9244cd
Seven <shiweidu@outlook.com>
2017-06-13 13:49:32 +0800

    完成发布处理器设置～

--- app/Contracts/Paid/PaidWhenResolved.php
+++ app/Contracts/Paid/PaidWhenResolved.php
@@ -5,9 +5,43 @@ namespace Zhiyi\Plus\Contracts\Paid;
 interface PaidWhenResolved
 {
     /**
-     * Validate the given class instance.
+     * Get the node key.
      *
-     * @return void
+     * @return string
+     * @author Seven Du <shiweidu@outlook.com>
      */
-    public function validate();
+    public static function node(): string;
+
+    /**
+     * Get the node desc.
+     *
+     * @return string
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public static function desc(): string;
+
+    /**
+     * Get the node forms.
+     *
+     * @return array
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public static function form(): array;
+
+    /**
+     * Handle.
+     *
+     * @param callable $callable
+     * @return mixed
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function handle(callable $callable);
+
+    /**
+     * 生成发布信息 key.
+     *
+     * @return string
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function makePayKey(): string;
 }
--- app/Support/PaidHandler.php
+++ app/Support/PaidHandler.php
@@ -6,5 +6,80 @@ use Zhiyi\Plus\Contracts\Paid\PaidWhenResolved;
 
 abstract class PaidHandler implements PaidWhenResolved
 {
-    //
+    /**
+     * The paid nodes.
+     *
+     * @var array
+     */
+    private static $nodes = [];
+
+    protected $node;
+
+    protected $formValues = null;
+
+    /**
+     * Publish Paid handler.
+     *
+     * @param string|array $node
+     * @param \Zhiyi\Plus\Contracts\PaidWhenResolved $handler
+     * @return void
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public static function publish($nodes, $handler = null)
+    {
+        if (! is_array($nodes)) {
+            $nodes = [$nodes => $handler];
+        }
+
+        array_merge(static::$nodes, $nodes);
+    }
+
+    public function handle(callable $callable)
+    {
+        $callable($this, );
+    }
+
+    /**
+     * Set node key.
+     *
+     * @param string $node
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function setNode(string $node)
+    {
+        $this->node = $node;
+    }
+
+    /**
+     * Get node key.
+     *
+     * @return string
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function getNode(): string
+    {
+        return $this->node;
+    }
+
+    /**
+     * Set node form values.
+     *
+     * @param mixed $values
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function setFormValues($values)
+    {
+        $this->formValues = $values;
+    }
+
+    /**
+     * Get node form values.
+     *
+     * @return mixed
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function getFormValues()
+    {
+        return $this->formValues;
+    }
 }
