9d16ef2e94605f4f77c40e669fd4675757f909ce
Seven <shiweidu@outlook.com>
2017-06-14 09:46:14 +0800

    规范辅助函数名～

--- app/helpers.php
+++ app/helpers.php
@@ -2,18 +2,15 @@
 
 namespace Zhiyi\Plus;
 
-if (! function_exists(__NAMESPACE__.'\is_cn_phone')) {
-
-    /**
-     * 检查一个字符串是否一个中国大陆手机号格式.
-     *
-     * @param string $value
-     * @return bool
-     */
-    function is_cn_phone(string $value): bool
-    {
-        return preg_match('/^(\+?0?86\-?)?((13\d|14[57]|15[^4,\D]|17[3678]|18\d)\d{8}|170[059]\d{7})$/', $value);
-    }
+/**
+ * 验证
+ *
+ * @param string $value
+ * @return bool
+ */
+function validateChinaPhoneNumber(string $value): bool
+{
+    return preg_match('/^(\+?0?86\-?)?((13\d|14[57]|15[^4,\D]|17[3678]|18\d)\d{8}|170[059]\d{7})$/', $value);
 }
 
 if (! function_exists(__NAMESPACE__.'\is_username')) {
