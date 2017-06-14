44ccc106f540a62b28ce5cc6d291e06823784bfd
Seven <shiweidu@outlook.com>
2017-06-14 09:49:24 +0800

    规范用户名验证函数.

--- app/helpers.php
+++ app/helpers.php
@@ -3,26 +3,23 @@
 namespace Zhiyi\Plus;
 
 /**
- * 验证
+ * 验证是否是中国验证码.
  *
- * @param string $value
+ * @param string $number
  * @return bool
  */
-function validateChinaPhoneNumber(string $value): bool
+function validateChinaPhoneNumber(string $number): bool
 {
-    return preg_match('/^(\+?0?86\-?)?((13\d|14[57]|15[^4,\D]|17[3678]|18\d)\d{8}|170[059]\d{7})$/', $value);
+    return preg_match('/^(\+?0?86\-?)?((13\d|14[57]|15[^4,\D]|17[3678]|18\d)\d{8}|170[059]\d{7})$/', $number);
 }
 
-if (! function_exists(__NAMESPACE__.'\is_username')) {
-
-    /**
-     * 检查一个字符串是否一个用户名格式.
-     *
-     * @param string $value
-     * @return bool
-     */
-    function is_username(string $value): bool
-    {
-        return preg_match('/^[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*$/', $value);
-    }
+/**
+ * 验证用户名是否合法.
+ *
+ * @param string $username
+ * @return bool
+ */
+function validateUsername(string $username): bool
+{
+    return preg_match('/^[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*$/', $username);
 }
