a4b715508a4cdb1863bba0c4fbfeb92c51b92394
Seven <shiweidu@outlook.com>
2017-06-15 16:04:54 +0800

    增加文件表迁移

---
+++ database/migrations/2017_06_15_070414_create_files_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('hash')->comment('文件 hash');
+            $table->string('origin_filename')->nullbale()->default(null)->comment('原始文件名');
+            $table->string('filename')->comment('文件名');
+            $table->string('mime', 100)->comment('mime type');
+            $table->float('width')->nullbale()->default(null)->comment('图片宽度');
+            $table->float('height')->nullbale()->default(null)->comment('图片高度');
+
+            $table->unique('hash');
+            $table->index('mime');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('files');
+    }
+}
