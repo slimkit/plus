f263768420a939240ace4a65b5bb1116d1e77434
Seven <shiweidu@outlook.com>
2017-06-16 18:04:36 +0800

    完成控制器

--- app/Http/Controllers/APIs/V2/FilesController.php
+++ app/Http/Controllers/APIs/V2/FilesController.php
@@ -6,13 +6,52 @@ use Illuminate\Http\Request;
 use Zhiyi\Plus\Models\File as FileModel;
 use Zhiyi\Plus\Http\Controllers\Controller;
 use Zhiyi\Plus\Models\FileWith as FileWithModel;
+use Zhiyi\Plus\Models\PayPublish as PayPublishModel;
 use Illuminate\Contracts\Routing\ResponseFactory as ResponseContract;
 
 class FilesController extends Controller
 {
-    public function show(Request $request)
+    /**
+     * Get file.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Zhiyi\Plus\Models\FileWith $fileWith
+     * @return mixed
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function show(Request $request, FileWithModel $fileWith)
     {
-        dd($request);
+        $fileWith->load(['file', 'pay']);
+
+        if ($fileWith->pay instanceof PayPublishModel) {
+            $this->resolveUserPaid($request->user('api'), $fileWith->pay);   
+        }
+
+        dd($fileWith->pay);
+    }
+
+    /**
+     * 解决用户是否购买过处理.
+     *
+     * @param \Zhiyi\Plus\Models\User|null $user
+     * @param \Zhiyi\Plus\Models\PayPublish $pay
+     * @return void
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    protected function resolveUserPaid($user, PayPublishModel $pay)
+    {
+        // 如果用户位空，则抛出认证错误.
+        if ($user === null) {
+            abort(401);
+        } else
+
+        // 检查用户是否购买过，购买过则跳过.
+        if ($user->payPublishes()->newPivotStatementForId($pay->id)->exists() === true) {
+            return;
+        }
+
+        // 检查失败后抛出禁止访问异常.
+        abort(403, '没有购买当前文件');
     }
 
     public function store()
