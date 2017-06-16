5d9618384215f2e133b38c3393f12ffbb0ba7205
Seven <shiweidu@outlook.com>
2017-06-16 12:07:43 +0800

    Add File uploaded check or create API.

--- app/Http/Controllers/APIs/V2/FilesController.php
+++ app/Http/Controllers/APIs/V2/FilesController.php
@@ -19,15 +19,40 @@ class FilesController extends Controller
     {
     }
 
-    public function uploaded(Request $request, ResponseContract $response, FileModel $file, string $hash)
+    /**
+     * Get or create a uploaded file with id.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Illuminate\Contracts\Routing\ResponseFactory $response
+     * @param \Zhiyi\Plus\Models\File $file
+     * @param \Zhiyi\Plus\Models\FileWith $fileWith
+     * @param string $hash
+     * @return mixed
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function uploaded(Request $request, ResponseContract $response, FileModel $file, FileWithModel $fileWith, string $hash)
     {
         $file = $file->where('hash', $hash)->firstOr(['id'], function () {
             abort(404);
         });
 
-        $fileWith = new FileWithModel();
-        $fileWith->file_id = $file->id;
+        // 复用空类型数据～减少资源浪费.
+        $fileWith = $fileWith->where('file_id', $file->id)
+            ->where('user_id', $request->user()->id)
+            ->where('channel', null)
+            ->where('raw', null)
+            ->firstOr(['id'], function () use ($file, $request, $fileWith) {
+                $fileWith->file_id = $file->id;
+                $fileWith->channel = null;
+                $fileWith->raw = null;
+                $request->user()->files()->save($fileWith);
 
-        $request->user()->files()->save($fileWith);
+                return $fileWith;
+            });
+
+        return $response->json([
+            'message' => ['success'],
+            'id' => $fileWith->id,
+        ])->setStatusCode(200);
     }
 }
