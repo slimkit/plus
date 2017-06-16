11fd086af31553ee963c922767925bd5abcdeff6
Seven <shiweidu@outlook.com>
2017-06-16 18:04:17 +0800

    增加用户购买关系～

--- app/Models/FileWith.php
+++ app/Models/FileWith.php
@@ -2,10 +2,17 @@
 
 namespace Zhiyi\Plus\Models;
 
+use Zhiyi\Plus\Models\File;
+use Zhiyi\Plus\Models\PayPublish;
 use Illuminate\Database\Eloquent\Model;
 
 class FileWith extends Model
 {
+    public function getPayIndexAttribute(): string
+    {
+        return sprintf('file:%d', $this->id);
+    }
+
     /**
      * has file.
      *
@@ -14,6 +21,17 @@ class FileWith extends Model
      */
     public function file()
     {
-        return $this->hasOne(File::class, 'file_id', 'id');
+        return $this->hasOne(File::class, 'id', 'file_id');
+    }
+
+    /**
+     * has pay publish.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function pay()
+    {
+        return $this->hasOne(PayPublish::class, 'index', 'pay_index');
     }
 }
