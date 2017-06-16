9722eda5e8fca419460e03bef6e1102d6b8b0353
Seven <shiweidu@outlook.com>
2017-06-16 12:40:02 +0800

    添加 File With 于 File 一对一关系。

--- app/Models/FileWith.php
+++ app/Models/FileWith.php
@@ -2,9 +2,19 @@
 
 namespace Zhiyi\Plus\Models;
 
+use Zhiyi\Plus\Models\File;
 use Illuminate\Database\Eloquent\Model;
 
 class FileWith extends Model
 {
-    //
+    /**
+     * has file.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function file()
+    {
+        return $this->hasOne(File::class, 'file_id', 'id');
+    }
 }
