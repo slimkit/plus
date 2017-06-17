fd937a6c6793374b3658ea51378aa4849cc4fa04
白石 <414606094@qq.com>
2017-06-17 11:34:26 +0800

    移除冗余模型，使用已有关联进行附加与卸除

--- app/Http/Controllers/APIs/V1/FollowController.php
+++ app/Http/Controllers/APIs/V1/FollowController.php
@@ -5,7 +5,6 @@ namespace Zhiyi\Plus\Http\Controllers\APIs\V1;
 use DB;
 use Zhiyi\Plus\Models\User;
 use Illuminate\Http\Request;
-use Zhiyi\Plus\Models\Follow;
 use Zhiyi\Plus\Models\Followed;
 use Zhiyi\Plus\Jobs\PushMessage;
 use Zhiyi\Plus\Models\Following;
@@ -33,13 +32,10 @@ class FollowController extends Controller
         $follow->user_id = $user_id;
         $follow->following_user_id = $follow_user_id;
 
-        $newfollow = new follow();
-        $newfollow->user_id = $user_id;
-        $newfollow->target = $follow_user_id;  // TODO 暂定兼容方案 之后删除旧版关注数据操作
-
         DB::beginTransaction();
 
-        $newfollow->save();
+        $request->user()->followings()->attach($follow_user_id);
+
         $add_following = $follow->save();
         $add_followed = $follow->syncFollowed();
         $following_count = UserDatas::byKey('following_count')->byUserId($user_id)->increment('value');
@@ -86,7 +82,7 @@ class FollowController extends Controller
 
         $delete_followed = Followed::where(['user_id' => $follow_user_id, 'followed_user_id' => $user_id])->delete();
         $delete_following = Following::where(['user_id' => $user_id, 'following_user_id' => $follow_user_id])->delete();
-        $delete_follow = Follow::where(['user_id' => $user_id, 'target' => $follow_user_id])->delete();
+        $delete_follow = $request->user()->followings()->detach($follow_user_id);
         $following_count = UserDatas::byKey('following_count')->byUserId($user_id)->decrement('value');
         $followed_count = UserDatas::byKey('followed_count')->byUserId($follow_user_id)->decrement('value');
 
