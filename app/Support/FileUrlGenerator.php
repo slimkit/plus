a6423a1be6350462b860123bdc6f3bd075b00129
Seven <shiweidu@outlook.com>
2017-06-20 10:52:40 +0800

    删除过度设计

--- app/Support/FileUrlGenerator.php
+++ app/Support/FileUrlGenerator.php
@@ -8,51 +8,11 @@ use Zhiyi\Plus\Contracts\File\UrlGenerator as FileUrlGeneratorContract;
 abstract class FileUrlGenerator implements FileUrlGeneratorContract
 {
     /**
-     * The app of generator abstracts.
-     *
-     * @var array
-     */
-    private static $generators = [];
-
-    /**
      * File data model.
      *
      * @var \Zhiyi\Plus\Models\File
      */
     protected $file;
-
-    /**
-     * Register a file URL generator abstract.
-     *
-     * @param string $name
-     * @param string $abstract
-     * @return void
-     * @author Seven Du <shiweidu@outlook.com>
-     */
-    public static function registerGenerator(string $name, string $abstract)
-    {
-        static::$generators[$name] = $abstract;
-    }
-
-    /**
-     * Get a generator abstract.
-     *
-     * @param string|null $name
-     * @return array|string
-     * @author Seven Du <shiweidu@outlook.com>
-     */
-    public static function getGeneratorAbstract(string $name = '')
-    {
-        if (! $name) {
-            return static::$generator;
-        }
-
-        if (($abstract = static::$generators[$name] ?? false) !== false) {
-            return $abstract;
-        }
-
-        return $name;
-    }
 
     /**
      * Get file data model.
