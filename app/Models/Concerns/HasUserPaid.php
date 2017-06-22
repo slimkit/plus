c5726a75634709687baede59bf658feeae88be42
Seven <shiweidu@outlook.com>
2017-06-22 11:40:24 +0800

    使用新的付费节点字段表～

---
+++ app/Models/Concerns/HasUserPaid.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Zhiyi\Plus\Models\Concerns;
+
+use Zhiyi\Plus\Models\User;
+use Illuminate\Support\Facades\Cache;
+
+trait HasUserPaid
+{
+    /**
+     * Paid node users.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'paid_node_users', 'node_id', 'user_id');
+    }
+
+
+    /**
+     * 判断是非对节点进行付费，支持过滤发布者
+     *
+     * @param int $user User ID
+     * @return bool
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function paid(int $user, bool $filter = true): bool
+    {
+        if ($filter === true && $this->user_id === $user) {
+            return true;
+        }
+
+        $cacheKey = sprintf('paid:%s,%s', $this->id, $user);
+        if (Cache::has($cacheKey)) {
+            return Cache::get($cacheKey);
+        }
+
+        $status = $this->users()->newPivotStatementForId($user)->first() !== null;
+        Cache::forever($cacheKey, $status);
+
+        return $status;
+    }
+}
--- app/Models/PaidNode.php
+++ app/Models/PaidNode.php
@@ -6,14 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class PaidNode extends Model
 {
-    /**
-     * Get pays.
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\HasMany
-     * @author Seven Du <shiweidu@outlook.com>
-     */
-    public function pays()
-    {
-        return $this->hasMany(PaidNodeUser::class, 'node_id', 'id');
-    }
+    use Concerns\HasUserPaid;
 }
