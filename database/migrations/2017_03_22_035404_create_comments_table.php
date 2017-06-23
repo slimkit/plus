9684514a29a346c601728a6416fa51e96775c7c5
Seven <shiweidu@outlook.com>
2017-06-23 10:44:52 +0800

    修改评论表结构。

--- database/migrations/2017_03_22_035404_create_comments_table.php
+++ database/migrations/2017_03_22_035404_create_comments_table.php
@@ -14,19 +14,16 @@ class CreateCommentsTable extends Migration
     public function up()
     {
         Schema::create('comments', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('component')->comment('扩展包名');
-            $table->string('comment_table')->comment('评论表名');
-            $table->integer('comment_id')->comment('关联评论id');
-            $table->text('comment_content')->comment('关联评论内容');
-            $table->string('source_table')->comment('资源表名');
-            $table->integer('source_id')->comment('关联资源id');
-            $table->integer('source_cover')->default(0)->comment('资源封面');
-            $table->longtext('source_content')->comment('资源内容');
-            $table->integer('user_id')->default(0)->comment('评论者id');
-            $table->integer('to_user_id')->default(0)->comment('资源作者id');
-            $table->integer('reply_to_user_id')->default(0)->comment('被回复的评论者id');
+            $table->increments('id')->comment('评论ID');
+            $table->integer('user_id')->unsigned()->comment('评论用户');
+            $table->integer('target_user')->unsigned()->comment('目标用户');
+            $table->integer('parent_id')->unsigned()->nullable()->default(0)->comment('父评论ID');
+            $table->string('channel', 100)->comment('来源频道');
+            $table->string('target', 100)->comment('来源目标');
             $table->timestamps();
+
+            $table->index('user_id');
+            $table->index('target_user');
         });
     }
 
