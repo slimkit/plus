fd71744f6e690de23f5778d46e96053ea3227317
Seven <shiweidu@outlook.com>
2017-06-30 18:10:57 +0800

    (fix #57) 减少账户名称所占长度。

--- database/migrations/2016_12_21_081206_create_verify_codes.php
+++ database/migrations/2016_12_21_081206_create_verify_codes.php
@@ -15,7 +15,7 @@ class CreateVerifyCodes extends Migration
     {
         Schema::create('verify_codes', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('account')->comment('账户');
+            $table->string('account', 100)->comment('账户');
             $table->integer('code')->comment('验证码');
             $table->tinyInteger('state')->nullable()->default(0)->comment('状态');
             $table->timestamps();
