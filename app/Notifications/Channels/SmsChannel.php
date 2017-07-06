29835f324342e27d135e8ac02f1bcf765c0a4180
Seven <shiweidu@outlook.com>
2017-07-06 23:18:45 +0800

    增加短信频道类

---
+++ app/Notifications/Channels/SmsChannel.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Zhiyi\Plus\Notifications\Channels;
+
+use Overtrue\EasySms\EasySms;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Foundation\Application as ApplicationContract;
+
+class SmsChannel
+{
+    /**
+     * The SMS notification driver.
+     *
+     * @var \Overtrue\EasySms\EasySms
+     */
+    protected $sms;
+
+    /**
+     * The app.
+     *
+     * @var \Illuminate\Contracts\Foundation\Application
+     */
+    protected $app;
+
+    /**
+     * Create the SMS notification channel instance.
+     *
+     * @param \Illuminate\Contracts\Foundation\Application $app
+     * @param \Overtrue\EasySms\EasySms $sms
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function __construct(ApplicationContract $app, EasySms $sms)
+    {
+        $this->app = $app;
+        $this->sms = $sms;
+    }
+
+    /**
+     * Send the given notification.
+     *
+     * @param  mixed  $notifiable
+     * @param  \Illuminate\Notifications\Notification  $notification
+     * @return \Nexmo\Message\Message
+     */
+    public function send($notifiable, Notification $notification)
+    {
+        if (! $to = $notifiable->routeNotificationFor('sms')) {
+            return;
+        }
+
+        $message = $notification->toSms($notifiable, $this->sms->getConfig());
+
+        dd($this->sms->send($to, $message));
+        return $this->sms->send($to, $message);
+    }
+}
