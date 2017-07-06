5d9d734c4aa44c01e46a7c0137f646c1dff52134
Seven <shiweidu@outlook.com>
2017-07-06 17:30:29 +0800

    修改邮件验证码模板。

--- resources/views/mail/varification_code.blade.php
+++ resources/views/mail/varification_code.blade.php
@@ -1,11 +1,13 @@
 @component('mail::message')
-# Introduction
 
-The body of your message.
+@if ($user)
+**{{ $user->name }}, 你好，**
+@else
+**你好，**
+@endif
 
-@component('mail::button', ['url' => ''])
-Button Text
-@endcomponent
+您此次申请的的验证码为 `{{ $model->code }}` ，请在 30 分钟内输入验证码进行下一步操作。<br>
+如非你本人操作，请忽略此邮件。
 
 Thanks,<br>
 {{ config('app.name') }}
