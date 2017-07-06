266c35c6e6a8a87ff968177a5c2243d00eba3dd7
Seven <shiweidu@outlook.com>
2017-07-07 01:18:36 +0800

    增加复用查找对象。

--- app/Models/VerificationCode.php
+++ app/Models/VerificationCode.php
@@ -4,6 +4,7 @@ namespace Zhiyi\Plus\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class VerificationCode extends Model
@@ -29,5 +30,25 @@ class VerificationCode extends Model
     public function user()
     {
         return $this->hasOne(User::class, 'id', 'user_id');
+    }
+
+    /**
+     * 设置复用的创建时间范围查询，单位秒.
+     *
+     * @param Builder $query  查询对象
+     * @param int     $second 范围时间，单位秒
+     *
+     * @return Builder 查询对象
+     *
+     * @author Seven Du <shiweidu@outlook.com>
+     * @homepage http://medz.cn
+     */
+    public function scopeByValid(Builder $query, int $second = 300): Builder
+    {
+        $now = $this->freshTimestamp();
+        $sub = clone $now;
+        $sub->subSeconds($second);
+
+        return $query->whereBetween('created_at', [$sub, $now]);
     }
 }
