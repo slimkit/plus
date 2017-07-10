fe183fc6f8ae1a7de829c740cf3674548e7632b2
Seven <shiweidu@outlook.com>
2017-07-10 10:36:00 +0800

    完成 toArray 方法开发。

--- app/Notifications/UserNotification.php
+++ app/Notifications/UserNotification.php
@@ -59,13 +59,10 @@ class UserNotification extends Notification implements ShouldQueue
     /**
      * Get the array representation of the notification.
      *
-     * @param  mixed  $notifiable
      * @return array
      */
-    public function toArray($notifiable)
+    public function toArray(): array
     {
-        return [
-            //
-        ];
+        return $this->message->toArray();
     }
 }
