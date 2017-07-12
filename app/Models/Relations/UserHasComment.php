9c25eb09ed6c37f7aeab95e7bb642cdb6e28c05f
Seven <shiweidu@outlook.com>
2017-07-12 11:57:47 +0800

    增加用户评论关系注释。

--- app/Models/Relations/UserHasComment.php
+++ app/Models/Relations/UserHasComment.php
@@ -6,6 +6,12 @@ use Zhiyi\Plus\Models\Comment;
 
 trait UserHasComment
 {
+    /**
+     * Has comments.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     * @author Seven Du <shiweidu@outlook.com>
+     */
     public function comments()
     {
         return $this->hasMany(Comment::class, 'user_id', 'id');
