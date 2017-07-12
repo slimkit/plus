c8d5aacbc9bad27a5eb9d3e9498171105bb01395
Seven <shiweidu@outlook.com>
2017-07-12 16:04:51 +0800

    增加赞数据模型关系。

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Like extends Model
 {
-    //
+    /**
+     * 可以被批量赋值的属性。
+     *
+     * @var array
+     */
+    protected $fillable = ['user_id', 'target_user'];
+
+    /**
+     * Has likeable.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\MorphTo
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function likeable()
+    {
+        return $this->morphTo();
+    }
 }
