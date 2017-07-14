fbe1ae2aff119cb2c58024c279df1e780257bd36
Seven <shiweidu@outlook.com>
2017-07-14 10:09:53 +0800

    增加用户头像基础控制器。

---
+++ app/Http/Controllers/APIs/V2/UserAvatarController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Zhiyi\Plus\Http\Controllers\APIs\V2;
+
+class UserAvatarController extends Controller
+{
+    public function show()
+    {
+        // tod.
+    }
+
+    public function update()
+    {
+        // todo
+    }
+}
