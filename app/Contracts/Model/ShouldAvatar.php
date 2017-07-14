f0c7123b20d2985fb1f557cc6bb6a707fbe67227
Seven <shiweidu@outlook.com>
2017-07-14 17:57:12 +0800

    增加应该使用头像注入接口

---
+++ app/Contracts/Model/ShouldAvatar.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Zhiyi\Plus\Contracts\Model;
+
+interface ShouldAvatar
+{
+    /**
+     * Get avatar trait.
+     *
+     * @return string|int
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function getAvatarKey();
+}
