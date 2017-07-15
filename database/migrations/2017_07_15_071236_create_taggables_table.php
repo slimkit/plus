c3ea499969c144c195b03ce67878046c7c65861b
Seven <shiweidu@outlook.com>
2017-07-15 17:20:46 +0800

    增加用户标识标签。

---
+++ database/migrations/2017_07_15_071236_create_taggables_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTaggablesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('taggables', function (Blueprint $table) {
+            $table->integer('tag_id')->unsigned()->comment('标签ID');
+            $table->morphs('taggable');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('taggables');
+    }
+}
