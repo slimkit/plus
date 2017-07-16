9f48d656b0ddb8c98392f6d470c57e92a3224992
Seven <shiweidu@outlook.com>
2017-07-16 16:58:46 +0800

    增加文件上传消息.

--- app/Http/Requests/API2/StoreUploadFile.php
+++ app/Http/Requests/API2/StoreUploadFile.php
@@ -27,4 +27,20 @@ class StoreUploadFile extends FormRequest
             'file' => 'required|max:2048|file|mimes:jpeg,bmp,png,gif,txt',
         ];
     }
+
+    /**
+     * Get the validation message that apply to the request.
+     *
+     * @return array
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function messages(): array
+    {
+        return [
+            'file.required' => '没有上传文件或者上传错误',
+            'file.max' => '文件上传超出服务器限制',
+            'file.file' => '文件上传失败',
+            'file.mimes' => '文件上传格式错误',
+        ];
+    }
 }
