e6f66adb125ff8e31a167b5d975b15cb0342c71c
Seven <shiweidu@outlook.com>
2017-07-16 16:01:08 +0800

    开发完成我的粉丝接口控制器。

--- app/Http/Controllers/APIs/V2/CurrentUserController.php
+++ app/Http/Controllers/APIs/V2/CurrentUserController.php
@@ -4,6 +4,7 @@ namespace Zhiyi\Plus\Http\Controllers\APIs\V2;
 
 use Illuminate\Http\Request;
 use Zhiyi\Plus\Http\Controllers\Controller;
+use Illuminate\Contracts\Routing\ResponseFactory as ResponseFactoryContract;
 
 class CurrentUserController extends Controller
 {
@@ -26,9 +27,29 @@ class CurrentUserController extends Controller
         return response()->json($user, 200);
     }
 
-    public function followers()
+    /**
+     * Show user followers.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Illuminate\Contracts\Routing\ResponseFactory $response
+     * @return mixed
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function followers(Request $request, ResponseFactoryContract $response)
     {
-        // todo.
+        $user = $request->user();
+        $limit = $request->query('limit', 20);
+        $after = $request->query('after', false);
+
+        $followers = $user->followers()
+            ->when($after, function ($query) use ($after, $user) {
+                return $query->where($user->getQualifiedKeyName(), '<', $after);
+            })
+            ->limit($limit)
+            ->orderBy('id', 'desc')
+            ->get();
+
+        return $response->json($followers)->setStatusCode(200);
     }
 
     public function followings()
