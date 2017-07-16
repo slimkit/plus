3a289a81446fc6231aaf864ab95905cef327a83b
Seven <shiweidu@outlook.com>
2017-07-16 17:22:15 +0800

    List users followed by another user.

--- app/Http/Controllers/APIs/V2/UserFollowController.php
+++ app/Http/Controllers/APIs/V2/UserFollowController.php
@@ -41,8 +41,36 @@ class UserFollowController extends Controller
         });
     }
 
-    public function followings()
+    /**
+     * List users followed by another user.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Illuminate\Contracts\Routing\ResponseFactory $response
+     * @param \Zhiyi\Plus\Models\User $user
+     * @return mixed
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function followings(Request $request, ResponseFactoryContract $response, UserModel $user)
     {
-        // todo.
+        $target = $request->user('api')->id ?? 0;
+        $limit = $request->query('limit', 20);
+        $after = $request->query('after', false);
+
+        $followings = $user->followings()
+            ->when($after, function ($query) use ($after, $user) {
+                return $query->where($user->getQualifiedKeyName(), '<', $after);
+            })
+            ->limit($limit)
+            ->orderBy('id', 'desc')
+            ->get();
+
+        return $user->getConnection()->transaction(function () use ($followings, $target, $response) {
+            return $response->json($followings->map(function (UserModel $user) use ($target) {
+                $user->following = $user->hasFollwing($target);
+                $user->follower = $user->hasFollower($target);
+
+                return $user;
+            }))->setStatusCode(200);
+        });
     }
 }
