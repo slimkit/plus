2cd3c7eee9c958ce6c7929bc77dda8b66da15c42
白石 <414606094@qq.com>
2017-07-27 14:50:52 +0800

    修复数据迁移操作

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call(UserProfileSettingLinksTableSeeder::class);
         $this->call(SensitivesTableSeeder::class); //敏感词
         $this->call(CertificationCategoryTableSeeder::class); // 用户认证类型
-        $this->call(CertificationCategoryTableSeeder::class); // 广告位类型
+        $this->call(AdvertisingSpaceTableSeeder::class); // 广告位类型
 
         // 把地区放在最后，因为耗时较长.
         $this->call(AreasTableSeeder::class);
