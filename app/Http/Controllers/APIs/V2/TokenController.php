0075316fd7ff6dc92fc96a2e4387a75e531050ba
白石 <414606094@qq.com>
2017-07-28 14:16:50 +0800

    #111 登录信息增加钱包余额

--- app/Http/Controllers/APIs/V2/TokenController.php
+++ app/Http/Controllers/APIs/V2/TokenController.php
@@ -23,7 +23,7 @@ class TokenController extends Controller
     {
         $login = $request->input('login');
         $password = $request->input('password');
-        $user = $model->where(username($login), $login)->first();
+        $user = $model->where(username($login), $login)->with('wallet')->first();
 
         if (! $user) {
             return $response->json(['login' => ['用户不存在']], 404);
@@ -35,8 +35,9 @@ class TokenController extends Controller
             ? $response->json([
                 'token' => $token,
                 'user' => array_merge($user->toArray(), [
-                    'phone' => $user->phone,
-                    'email' => $user->email,
+                    'phone'  => $user->phone,
+                    'email'  => $user->email,
+                    'wallet' => $user->wallet ? $user->wallet->balance : null,
                 ]),
                 'ttl' => config('jwt.ttl'),
                 'refresh_ttl' => config('jwt.refresh_ttl'),
