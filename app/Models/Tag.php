420ed1186a16eadf575cc9433ddd0f296c6d5cbd
Seven Du <shiweidu@outlook.com>
2017-07-28 17:40:09 +0800

    优化标签关系模型。

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array
+     */
+    protected $hidden = ['created_at', 'updated_at'];
+
+    /**
      * Has the category of tag.
      *
      * @return \Illuminate\Database\Eloquent\Relations\HasOne
--- app/Models/TagCategory.php
+++ app/Models/TagCategory.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class TagCategory extends Model
 {
-    //
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array
+     */
+    protected $hidden = ['created_at', 'updated_at'];
+
+    /**
+     * Has tags of the category.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function tags()
+    {
+        return $this->hasMany(Tag::class, 'tag_category_id', 'id');
+    }
 }
