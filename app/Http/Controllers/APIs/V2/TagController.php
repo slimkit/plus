997348476fe4e79a4cd46748affd41a354ec4723
Seven Du <shiweidu@outlook.com>
2017-07-28 17:41:18 +0800

    增加接口控制器注释。

--- app/Http/Controllers/APIs/V2/TagController.php
+++ app/Http/Controllers/APIs/V2/TagController.php
@@ -7,6 +7,14 @@ use Illuminate\Contracts\Routing\ResponseFactory as ResponseFactoryContract;
 
 class TagController extends Controller
 {
+    /**
+     * Get all tags.
+     *
+     * @param \Illuminate\Contracts\Routing\ResponseFactory $response
+     * @param \Zhiyi\Plus\Models\TagCategory $categoryModel
+     * @return mixed
+     * @author Seven Du <shiweidu@outlook.com>
+     */
     public function index(ResponseFactoryContract $response, TagCategoryModel $categoryModel)
     {
         return $response->json(
