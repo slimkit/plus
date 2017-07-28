c3b45acfdc2bee05a4b47cf337d4b2354b97dd7a
Seven Du <shiweidu@outlook.com>
2017-07-28 17:34:23 +0800

    增加标签分类关系。

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+    /**
+     * Has the category of tag.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function category()
+    {
+        return $this->hasOne(TagCategory::class, 'id', 'tag_category_id');
+    }
 }
