9bea6a540aec4770586b5f5160c9ab982e6935c8
Seven Du <shiweidu@outlook.com>
2017-07-28 15:41:48 +0800

    优化通用 markdown 私有资源查找性状。

--- app/Contracts/FindMarkdownFileTrait.php
+++ app/Contracts/FindMarkdownFileTrait.php
@@ -2,7 +2,10 @@
 
 namespace Zhiyi\Plus\Contracts;
 
+use Illuminate\Support\Collection;
+use Illuminate\Database\Eloquent\Builder;
 use function Zhiyi\Plus\findMarkdownImageIDs;
+use Zhiyi\Plus\Models\FileWith as FileWithModel;
 
 trait FindMarkdownFileTrait
 {
@@ -13,8 +16,46 @@ trait FindMarkdownFileTrait
      * @return array
      * @author Seven Du <shiweidu@outlook.com>
      */
-    public function findImageIDs(string $markdown): array
+    public function findMarkdownImageIDs(string $markdown): array
     {
         return findMarkdownImageIDs($markdown);
+    }
+
+    /**
+     * Find markdown images of collection.
+     *
+     * @param string $markdown
+     * @param callable $call
+     * @return \Illuminate\Support\Collection
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function findMarkdownImageModels(string $markdown, $call = null): Collection
+    {
+        $IDs = $this->findMarkdownImageIDs($markdown);
+        if (empty($IDs)) {
+            return new Collection();
+        }
+
+        $query = FileWithModel::whereIn('id', $IDs);
+        if ($call && is_callable($call)) {
+            call_user_func($call, $query);
+        }
+
+        return $query->get();
+    }
+
+    /**
+     * Find markdown images of collection for not with.
+     *
+     * @param string $markdown
+     * @return \Illuminate\Support\Collection
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function findMarkdownImageNotWithModels(string $markdown): Collection
+    {
+        return $this->findMarkdownImageModels($markdown, function (Builder $query) {
+            $query->where('channel', null)
+                ->where('raw', null);
+        });
     }
 }
