c669bda5a895274a15ea1f27e928795fe8cc5208
Seven Du <shiweidu@outlook.com>
2017-08-04 11:02:13 +0800

    增加用户喜欢关系。

--- app/Models/Relations/UserHasLike.php
+++ app/Models/Relations/UserHasLike.php
@@ -2,10 +2,18 @@
 
 namespace Zhiyi\Plus\Models\Relations;
 
+use Zhiyi\Plus\Models\Like;
+
 trait UserHasLike
 {
+    /**
+     * Has likes for user.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\MorphMany
+     * @author Seven Du <shiweidu@outlook.com>
+     */
     public function likes()
     {
-        // todo.
+        return $this->morphMany(Like::class, 'likeable');
     }
 }
