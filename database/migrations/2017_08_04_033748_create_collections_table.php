d63f89a9f509a4c3dad97b4d1f21c96cb561eb8b
Seven Du <shiweidu@outlook.com>
2017-08-04 11:38:32 +0800

    增加多对多关系手册数据模型和迁移。

---
+++ database/migrations/2017_08_04_033748_create_collections_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCollectionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('collections', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->comment('用户ID');
+            $table->morphs('collectible');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('collections');
+    }
+}
