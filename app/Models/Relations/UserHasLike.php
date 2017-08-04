9784d80e6d4e79af98613dd2a5f5f519f72ffe25
Seven Du <shiweidu@outlook.com>
2017-08-04 11:08:27 +0800

    增加被喜欢的用户关系。

--- app/Models/Relations/UserHasLike.php
+++ app/Models/Relations/UserHasLike.php
@@ -16,4 +16,15 @@ trait UserHasLike
     {
         return $this->hasMany(Like::class, 'user_id', 'id');
     }
+
+    /**
+     * Has be likeds for user.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function belikeds()
+    {
+        return $this->hasMany(Like::class. 'target_user', 'id');
+    }
 }
