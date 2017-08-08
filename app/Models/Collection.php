480a4cbf7e8e7aba1d6a355a23334c9176ff4075
白石 <414606094@qq.com>
2017-08-08 16:00:23 +0800

    增加关联以便查询收藏数据

--- app/Models/Collection.php
+++ app/Models/Collection.php
@@ -12,4 +12,15 @@ class Collection extends Model
      * @var array
      */
     protected $guarded = ['created_at', 'updated_at'];
+
+    /**
+     * Has collectible.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\MorphTo
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function collectible()
+    {
+        return $this->morphTo();
+    }
 }
