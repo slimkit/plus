b0078a466679b6e6134bf346642058aeae0149cc
Seven <shiweidu@outlook.com>
2017-08-09 10:13:52 +0000

    Apply fixes from StyleCI

--- app/Models/UserRecommended.php
+++ app/Models/UserRecommended.php
@@ -2,15 +2,14 @@
 
 namespace Zhiyi\Plus\Models;
 
-use Zhiyi\Plus\Models\User;
 use Illuminate\Database\Eloquent\Model;
 
 class UserRecommended extends Model
 {
-	protected $table = 'users_recommended';
+    protected $table = 'users_recommended';
 
-	public function user()
-	{
-		return $this->belongsTo(User::class, 'user_id', 'id');
-	}
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
 }
