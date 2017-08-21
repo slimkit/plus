3424d100f2d6aca86e4700e1d22d7e4eb682f211
Seven Du <shiweidu@outlook.com>
2017-08-21 17:49:59 +0800

    首页增加登陆按钮。

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@
 Route::get('/', 'HomeController@welcome');
 Route::get('/auth/login', 'Auth\\LoginController@showLoginForm')->name('login');
 Route::post('/auth/login', 'Auth\\LoginController@login');
+Route::any('auth/logout', 'Auth\\LoginController@logout')->name('logout');
 
 Route::prefix('admin')
     ->namespace('Admin')
