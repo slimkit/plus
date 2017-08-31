02ff3e3bcf99c61daaa918b33f075f8456e4cdc6
Seven Du <shiweidu@outlook.com>
2017-08-31 11:34:45 +0800

    移除预先的 laravel 5.5 包管理机制。

--- app/Application.php
+++ app/Application.php
@@ -62,18 +62,6 @@ class Application extends LaravelApplication
     }
 
     /**
-     * Register all of the configured providers.
-     *
-     * @return void
-     * @author Seven Du <shiweidu@outlook.com>
-     */
-    public function registerConfiguredProviders()
-    {
-        (new ProviderRepository($this, new Filesystem, $this->getCachedServicesPath()))
-                    ->load($this->getConfiguredProviders());
-    }
-
-    /**
      * Register the core class aliases in the container.
      *
      * @return void
@@ -96,43 +84,5 @@ class Application extends LaravelApplication
                 $this->alias($key, $alias);
             }
         }
-    }
-
-    /**
-     * Get the path to the cached packages.php file.
-     *
-     * @return string
-     */
-    public function getCachedPackagesPath()
-    {
-        return $this->bootstrapPath('cache/packages.php');
-    }
-
-    /**
-     * Get all of the configured providers.
-     *
-     * @return array
-     * @author Seven Du <shiweidu@outlook.com>
-     */
-    protected function getConfiguredProviders(): array
-    {
-        return array_merge(
-            $this->config['app.providers'],
-            $this->make(PackageManifest::class)->providers()
-        );
-    }
-
-    /**
-     * Register the basic bindings into the container.
-     *
-     * @return void
-     * @author Seven Du <shiweidu@outlook.com>
-     */
-    protected function registerBaseBindings()
-    {
-        parent::registerBaseBindings();
-        $this->instance(PackageManifest::class, new PackageManifest(
-            new Filesystem, $this->basePath(), $this->getCachedPackagesPath()
-        ));
     }
 }
