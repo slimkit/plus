a1e852d8cca468b3934f09be7fa7e915e5f48a36
boxshadow <idafoo@sina.com>
2017-09-08 11:33:05 +0800

    增加未提交文件

---
+++ resources/assets/admin/component/user/Register.vue
@@ -0,0 +1,32 @@
+<style lang="css" module>
+.container {
+  padding-top: 15px;
+}
+.loadding {
+  text-align: center;
+  font-size: 42px;
+}
+.loaddingIcon {
+  animation-name: "TurnAround";
+  animation-duration: 1.4s;
+  animation-timing-function: linear;
+  animation-iteration-count: infinite;
+}
+</style>
+
+<template>
+  <div class="container-fluid" :class="$style.container">
+		<div class="well well-sm">
+      用户注册设置
+    </div>
+	</div>
+</template>
+
+<script>
+	
+	const RegisterSetting = {
+
+	};
+
+	export default RegisterSetting;
+</script>
