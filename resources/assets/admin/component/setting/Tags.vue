18e97718c0c2bc86469ef5ce088cee62d39fe834
huhao <915664508@qq.com>
2017-09-27 11:16:34 +0800

    标签管理
    1、标签首页样式修改

--- resources/assets/admin/component/setting/Tags.vue
+++ resources/assets/admin/component/setting/Tags.vue
@@ -1,94 +1,106 @@
-<template>
-  <div class="container-fluid">
-    <div class="page-header">
-      <h4>标签列表<small :class="$style.link"><router-link to="/setting/addtag">添加标签</router-link></small></h4>
-    </div>
-    <div v-show="error" class="alert alert-danger alert-dismissible" role="alert">
-      <button type="button" class="close" @click.prevent="dismisAddAreaError">
-        <span aria-hidden="true">&times;</span>
-      </button>
-      <strong>Error:</strong>
-      <p>{{ message }}</p>
-    </div>
-    <!-- 标签列表 -->
-      <table class="table table-striped" v-if="!empty">
-        
-        <thead>
-          <tr>
-            <th>标签ID</th>
-            <th>标签</th>
-            <th>所属分类</th>
-            <th>热度</th>
-            <th>权重(越大越靠前)</th>
-            <th>操作</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-show="loadding">
-            <!-- 加载动画 -->
-            <td :class="$style.loadding" colspan="6">
-              <span class="glyphicon glyphicon-refresh" :class="$style.loaddingIcon"></span>
-            </td>
-          </tr>
-          <tr v-for="tag in tags" :key="tag.id">
-            <td>{{ tag.id }}</td>
-            <td>{{ tag.name }}</td>
-            <td>
-              <router-link :to="`/setting/tags?cate=${ tag.category.id }`">
-                {{ tag.category.name }}
-              </router-link>
-            </td>
-            <td>{{ tag.taggable_count }}</td>
-            <td>{{ tag.weight }}</td>
-            <td>
-              <!-- 编辑 -->
-              <router-link type="button" class="btn btn-primary btn-sm" :to="`/setting/updatetag/${tag.id}`" >编辑</router-link>
-              <!-- 删除 -->
-              <button type="button" class="btn btn-danger btn-sm" @click="deleteTag(tag.id)">删除</button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      <p v-else style="text-align: center; padding: 8px;">还没有添加标签</p>
-      <ul class="pager" v-show="page >= 1 && last_page > 1">
-        <li class="previous" :class="page <= 1 ? 'disabled' : ''">
-          <router-link :to="{ path: '/setting/tags', query: prevQuery }">
-            <span aria-hidden="true">&larr;</span>
-            上一页
-          </router-link>
-        </li>
-        <li>
-          共 {{ total }}个标签，总共{{ last_page }}页，当前为第{{ page }}页
-        </li>
-        <li class="next" :class="page >= last_page ? 'disabled': ''">
-          <router-link :to="{ path: '/setting/tags', query: nextQuery }">
-            下一页
-            <span aria-hidden="true">&rarr;</span>
-          </router-link>
-        </li>
-      </ul>
-    </div>
-</template>
-
-<style lang="scss" module>
-  .loadding {
-    text-align: center;
-    font-size: 42px;
-  }
-  .loaddingIcon {
-    animation-name: "TurnAround";
-    animation-duration: 1.4s;
-    animation-timing-function: linear;
-    animation-iteration-count: infinite;
-  }
-  .link { margin-left: 16px; }
+<style lang="css" module>
+    .container {
+        padding: 15px;
+    }
+    .loadding {
+        text-align: center;
+        font-size: 42px;
+    }
+    .loaddingIcon {
+        animation-name: "TurnAround";
+        animation-duration: 1.4s;
+        animation-timing-function: linear;
+        animation-iteration-count: infinite;
+    }
 </style>
+<template>
+  <div class="container-fluid" style="padding:15px;">
+    <div v-show="message.success" class="alert alert-success alert-dismissible" role="alert">
+        <button type="button" class="close" @click.prevent="offAlert">
+            <span aria-hidden="true">&times;</span>
+        </button>
+        {{ message.success }}
+    </div>
+    <div v-show="message.error" class="alert alert-danger alert-dismissible" role="alert">
+        <button type="button" class="close" @click.prevent="offAlert">
+            <span aria-hidden="true">&times;</span>
+        </button>
+        {{ message.error }}
+    </div>
+    <div class="panel panel-default">
+      <div class="panel panel-heading">
+        <router-link to="/setting/addtag" class="btn btn-primary btn-sm">添加标签</router-link>
+      </div>
+      <div class="panel-body">
+        <table class="table table-striped">
+          <thead>
+            <tr>
+              <th>标签ID</th>
+              <th>标签</th>
+              <th>所属分类</th>
+              <th>热度</th>
+              <th>权重(越大越靠前)</th>
+              <th>操作</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-show="loadding">
+                <!-- 加载动画 -->
+                <td :class="$style.loadding" colspan="6">
+                    <span class="glyphicon glyphicon-refresh" :class="$style.loaddingIcon"></span>
+                </td>
+            </tr>
+            <template v-if="!empty">
+              <tr v-for="tag in tags" :key="tag.id">
+                <td>{{ tag.id }}</td>
+                <td>{{ tag.name }}</td>
+                <td>
+                  <router-link :to="`/setting/tags?cate=${ tag.category.id }`">
+                    {{ tag.category.name }}
+                  </router-link>
+                </td>
+                <td>{{ tag.taggable_count }}</td>
+                <td>{{ tag.weight }}</td>
+                <td>
+                  <!-- 编辑 -->
+                  <router-link type="button" class="btn btn-primary btn-sm" :to="`/setting/updatetag/${tag.id}`">编辑</router-link>
+                  <!-- 删除 -->
+                  <button type="button" class="btn btn-danger btn-sm" @click="deleteTag(tag.id)">删除</button>
+                </td>
+              </tr>    
+            </template>
+            <template v-else>
+              <tr><td colspan="6" class="text-center">无相关记录</td></tr>
+            </template>
 
+          </tbody>
+        </table>
+        <ul class="pager" v-show="page >= 1 && last_page > 1">
+          <li class="previous" :class="page <= 1 ? 'disabled' : ''">
+            <router-link :to="{ path: '/setting/tags', query: prevQuery }">
+              <span aria-hidden="true">&larr;</span>
+              上一页
+            </router-link>
+          </li>
+          <li>
+            共 {{ total }}个标签，总共{{ last_page }}页，当前为第{{ page }}页
+          </li>
+          <li class="next" :class="page >= last_page ? 'disabled': ''">
+            <router-link :to="{ path: '/setting/tags', query: nextQuery }">
+              下一页
+              <span aria-hidden="true">&rarr;</span>
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
 <script>
   import _ from 'lodash';
   import { mapGetters } from 'vuex';
   import request, { createRequestURI } from '../../util/request';
-  
+  import plusMessageBundle from 'plus-message-bundle';
   const Tags = {
     data: () => ({
       tags: [],
@@ -100,12 +112,13 @@
       per_page: 20,
       loadding: true,
       cate: 0,
-      error: false,
-      message: ''
+      message: {
+        error: null,
+        success: null,
+      }
     }),
     methods: {
       getTags () {
-
         request.get(createRequestURI(`site/tags`) , {
           params: { ...this.queryParams }
         }, {
@@ -118,8 +131,8 @@
           this.loadding = false;
         }).catch(({ response: { data: { message = '加载失败' } = {} } = {} }) => {
           this.loadding = false;
-          this.error = true;
-          window.alert(message);
+          let Message = new plusMessageBundle(data);
+          this.message.error = Message.getMessage();
         });
       },
 
