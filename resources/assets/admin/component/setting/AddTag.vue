858d3aa805e38f1f10a221d2baa07a968d16e7b1
huhao <915664508@qq.com>
2017-09-27 10:47:19 +0800

    标签管理
    1、修改标签添加ui
    2、增加标签添加报错详细提示

--- resources/assets/admin/component/setting/AddTag.vue
+++ resources/assets/admin/component/setting/AddTag.vue
@@ -1,44 +1,65 @@
-<template>
+  <template>
   <div class="container-fluid">
-    <div class="page-header">
-      <h4>添加标签</h4>
-    </div>
-    <form style="margin-bottom: 16px;">
-      <div class="form-group">
-        <label for="exampleInputEmail1">标签名字</label>
-        <input v-model="name" type="text" class="form-control" id="exampleInputEmail1" placeholder="标签名称">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        添加标签
       </div>
-      <div class="form-group">
-        <label for="exampleInputEmail1">权重<small>(越大越靠前)</small></label>
-        <input v-model="weight" type="text" class="form-control" id="exampleInputEmail1" placeholder="标签权重">
-      </div>
-      <div class="form-group">
-        <label for="exampleInputPassword1">标签分类</label>
-        <div  class="btn-toolbar" role="group" aria-label="cate">
-          <button
-            type="button" 
-            @click="setCategory(cate.id)" 
-            :class="{ 'btn-info': category === cate.id}"
-            v-for="cate in categories" 
-            aria-label="cate" 
-            :key="cate.id" 
-            class=" btn btn-group btn-group-sm btn-default" 
-            role="group" 
-          >
-            {{cate.name}}
-          </button>
+      <div class="panel-body form-horizontal">
+        <div class="row">
+          <div class="col-md-11 col-md-offset-1">
+            <div class="form-group">
+              <label for="exampleInputEmail1" class="control-label col-md-1">标签名字</label>
+              <div class="col-md-6">
+                <input v-model="name" type="text" class="form-control" id="exampleInputEmail1" placeholder="标签名称">
+              </div>
+              <div class="col-md-5">
+                <div class="help-block">输入标签名字</div>
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="exampleInputEmail1" class="control-label col-md-1">标签权重</label>
+              <div class="col-md-6">
+                <input v-model="weight" type="text" class="form-control" placeholder="标签权重">
+              </div>
+              <div class="col-md-5">
+                <div class="help-block">输入标签权重<small>（越大越靠前）</small></div>
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1" class="control-label col-md-1">标签分类</label>
+              <div  class="btn-toolbar col-md-6" role="group" aria-label="cate">
+                <button
+                  type="button" 
+                  @click="setCategory(cate.id)" 
+                  :class="{ 'btn-info': category === cate.id}"
+                  v-for="cate in categories" 
+                  aria-label="cate" 
+                  :key="cate.id" 
+                  class=" btn btn-group btn-group-sm btn-default" 
+                  role="group" 
+                >
+                  {{cate.name}}
+                </button>
+              </div>
+              <div class="col-md-5">
+                <div class="help-block">请选择标签分类</div>
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="" class="control-label col-md-1"></label>
+              <div class="col-md-6">
+                <button type="submit" @click="send()" id="myButton" data-complete-text="添加成功" data-loading-text="提交中..." class="btn btn-primary" autocomplete="off">
+                 添加
+                </button>
+              </div>
+              <div class="col-md-5">
+                 <span class="text-success"  v-show="message.success">{{ message.success }}</span>
+                 <span class="text-danger" v-show="message.error">{{ message.error }}</span>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      <button type="submit" @click="send()" id="myButton" data-complete-text="添加成功" data-loading-text="提交中..." class="btn btn-default" autocomplete="off" :disabled="!canSend">
-        添加
-      </button>
-    </form>
-    <div v-show="add.error" class="alert alert-danger alert-dismissible" role="alert">
-      <button type="button" class="close" @click.prevent="dismisAddAreaError">
-        <span aria-hidden="true">&times;</span>
-      </button>
-      <strong>Error:</strong>
-      <p>{{ add.error_message }}</p>
     </div>
   </div>
 </template>
@@ -49,7 +70,7 @@
 </style>
 <script>
   import request, { createRequestURI } from '../../util/request';
-
+  import plusMessageBundle from 'plus-message-bundle';
   const AddTag = {
     data: () => ({
       name: '',
@@ -60,23 +81,25 @@
         loadding: false,
         error: false,
         error_message: ''
-      }
+      },
+      message: {
+        success: null,
+        error: null,
+      },
     }),
-
+    watch: {
+      'categories'() {
+        if (this.categories.length <= 0) {
+          this.message.error = '请先添加标签分类，在进行添加标签';
+        }
+      }
+    },
     methods: {
       send () {
-        const {
-          name = '',
-          category = 0,
-          weight = 0
-        } = this;
-        if(!name || !category) {
-          this.add.error = true;
-          this.add.error_message = '参数不完整';
-          return false;
-        }
+        this.resetMessage();
+        if (!this.validate())  return;
+        const { name = '', category = 0, weight = 0 } = this;
         let btn = $("#myButton").button('loading');
-
         request.post(createRequestURI('site/tags'), {
           name,category,weight
         }, {
@@ -84,23 +107,15 @@
         })
         .then ( response => {
           this.sendComplate(btn);
+          this.message.success = '添加成功';
+          this.$router.replace({ path: '/setting/tags' });
         })
         .catch(({ response: { data = {} } = {} }) => {
           btn.button('reset');
-
-          let error = '添加标签失败';
-          if(data.name) {
-            error = data.name[0];
-          }
-          if(data.category) {
-            error = data.category[0];
-          }
-          this.add.loadding = false;
-          this.add.error = true;
-          this.add.error_message = error;
+          let Message = new plusMessageBundle(data);
+          this.message.error = Message.getMessage();
         });
       },
-
       sendComplate(btn) {
         btn.button('complete');
         setTimeout(() => {
@@ -110,15 +125,12 @@
           this.weight = 0;
         }, 1500);
       },
-
       dismisAddAreaError () {
         this.add.error = false;
       },
-
       setCategory (id) {
         this.category = id;
       },
-
       // 获取标签分类
       getCategories () {
         request.get(createRequestURI('site/tags/categories'),{
@@ -130,13 +142,22 @@
         .catch( () => {
 
         });
-      }
-    },
-
-    computed: {
-      // 按钮是否处于激活状态
-      canSend() {
-        return (this.name != '') && (this.category != 0);
+      },
+      validate () {
+        const { name = '', category = 0, weight = 0 } = this;
+        if (!name) {
+          this.message.error = '请输入标签名字';
+          return false;
+        }
+        if (!category) {
+          this.message.error = '请选择标签分类';
+          return false;
+        }
+        return true;
+      },
+      resetMessage () {
+        let msg = this.message;
+        msg.error = msg.success = null;
       }
     },
     created () {
