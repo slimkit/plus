f7b91d7c50992a3e36e8b4d60dc41ccb50b011ba
huhao <915664508@qq.com>
2017-09-30 15:22:19 +0800

    删除wangeditor,怎家markdown-it
    删除wangeditor,怎家markdown-it

--- resources/assets/admin/component/edit.md/components/index.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by admin on 2017/4/15.
- */
-import VueEditor from './main.vue';
-export default VueEditor;
--- resources/assets/admin/component/user/Register.vue
+++ resources/assets/admin/component/user/Register.vue
@@ -12,131 +12,160 @@
   animation-timing-function: linear;
   animation-iteration-count: infinite;
 }
-.editor {
-	width: 100%;
+.question-editor {
+  resize: none;
+}
+.question-editor-preview {
+  width: 100%;
+  height: auto;
 }
 </style>
 
 <template>
   <div class="container-fluid" :class="$style.container">
-		<div class="well well-sm">
-      用户注册设置
-    </div>
-    <div class="form-horizontal">
-      <div class="form-group">
-				<label class="col-sm-3 control-label">注册方式</label>
-      	<div class="col-sm-7">
-      		<div class="input-group">
-      			<div class="">
-					    <label class="radio-inline">
-					    	<input type="radio" name="type" checked="checked" value="all" v-model="type" /> 开放注册
-					    </label>
-							<label class="radio-inline">
-					    	<input type="radio" name="type" disabled="disabled" value="invited" v-model="type" /> 仅邀请注册
-					    </label>
-					    <label class="radio-inline">
-					    	<input type="radio" name="type" disabled="disabled" value="thirdPart" v-model="type" /> 仅第三方绑定
-					    </label>
-					  </div>
-					</div>
-	      </div>
-     	</div>
-     	<div class="form-group">
-				<label class="col-sm-3 control-label">注册类型</label>
-      	<div class="col-sm-7">
-      		<div class="input-group">
-      			<div class="">
-					    <label class="radio-inline">
-					    	<input type="radio" name="method" disabled="disabled" value="mobile-only" v-model="method" /> 仅手机
-					    </label>
-							<label class="radio-inline">
-					    	<input type="radio" name="method" disabled="disabled" value="mail-only" v-model="method" /> 仅邮箱
-					    </label>
-					    <label class="radio-inline">
-					    	<input type="radio" name="method" checked="checked" value="all" v-model="method" /> 手机或邮箱
-					    </label>
-					  </div>
-					</div>
-	      </div>
-     	</div>
-     	<div class="form-group">
-				<label class="col-sm-3 control-label">完善资料</label>
-      	<div class="col-sm-7">
-      		<div class="input-group">
-      			<div class="">
-					    <label class="radio-inline">
-					    	<input type="radio" name="fixed" v-model="fixed" value="need" /> 注册时强制完善
-					    </label>
-							<label class="radio-inline">
-					    	<input type="radio" name="fixed" checked="checked" value="no-need" v-model="fixed"/> 不需要强制完善
-					    </label>
-					  </div>
-					</div>
-	      </div>
-     	</div>
-     	<div class="form-group">
-				<label class="col-sm-3 control-label">服务条款和隐私政策</label>
-      	<div class="col-sm-7">
-      		<div class="input-group">
-      			<div class="">
-					    <label class="radio-inline">
-					    	<input type="radio" name="rules" value="open" v-model="rules" /> 开启
-					    </label>
-							<label class="radio-inline">
-					    	<input type="radio" name="rules" checked="checked" value="close" v-model="rules" /> 关闭
-					    </label>
-					  </div>
-					</div>
-	      </div>
-     	</div>
-     	<div class="form-group" v-if="rules === 'open'">
-     		<label  class="col-sm-3 control-label" for="rule-content">条款内容</label>
-     		<div class="col-sm-9">
-     			<div class="input-group" style="height: 400px;">
-     				<VueEditor v-model="content" :value="content" @input="input" ref="editor" class="editor" />
-     			</div>
-     		</div>
-     	</div>
-     	<!-- Button -->
-      <div class="form-group">
-        <div class="col-sm-offset-3 col-sm-4">
-          <button v-if="loading" type="button" class="btn btn-primary" disabled="disabled">
-            <span class="glyphicon glyphicon-refresh component-loadding-icon"></span>
-          </button>
-          <button v-else type="button" class="btn btn-primary" @click="saveConfig">保存设置</button>
-        </div>
-        <div class="col-sm-4">
-        	<p class="text-success">{{ message }}</p>
-        </div>
-      </div>
-    </div>
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				用户注册设置
+			</div>
+			<div class="panel-body">
+			    <div class="form-horizontal">
+			      <div class="form-group">
+					<label class="col-sm-3 control-label">注册方式</label>
+			      	<div class="col-sm-5">
+			      		<div class="input-group">
+			      			<div class="">
+							    <label class="radio-inline">
+							    	<input type="radio" name="type" checked="checked" value="all" v-model="type" /> 开放注册
+							    </label>
+									<label class="radio-inline">
+							    	<input type="radio" name="type" disabled="disabled" value="invited" v-model="type" /> 仅邀请注册
+							    </label>
+							    <label class="radio-inline">
+							    	<input type="radio" name="type" disabled="disabled" value="thirdPart" v-model="type" /> 仅第三方绑定
+							    </label>
+							</div>
+						</div>
+				      </div>
+		     		<div class="col-md-4">
+		     			<span class="help-block">注册方式</span>
+		     		</div>	
+			     	</div>
+			     	<div class="form-group">
+						<label class="col-sm-3 control-label">注册类型</label>
+				      	<div class="col-sm-5">
+				      		<div class="input-group">
+				      			<div class="">
+								    <label class="radio-inline">
+								    	<input type="radio" name="method" disabled="disabled" value="mobile-only" v-model="method" /> 仅手机
+								    </label>
+										<label class="radio-inline">
+								    	<input type="radio" name="method" disabled="disabled" value="mail-only" v-model="method" /> 仅邮箱
+								    </label>
+								    <label class="radio-inline">
+								    	<input type="radio" name="method" checked="checked" value="all" v-model="method" /> 手机或邮箱
+								    </label>
+								</div>
+							</div>
+					    </div>
+			     		<div class="col-md-4">
+			     			<span class="help-block">账号注册时使用的类型</span>
+			     		</div>	
+			     	</div>
+			     	<div class="form-group">
+						<label class="col-sm-3 control-label">完善资料</label>
+				      	<div class="col-sm-5">
+				      		<div class="input-group">
+			      				<div class="">
+								    <label class="radio-inline">
+								    	<input type="radio" name="fixed" v-model="fixed" value="need" /> 注册时强制完善
+								    </label>
+										<label class="radio-inline">
+								    	<input type="radio" name="fixed" checked="checked" value="no-need" v-model="fixed"/> 不需要强制完善
+								    </label>
+								  </div>
+							</div>
+					    </div>
+			     		<div class="col-md-4">
+			     			<span class="help-block">完善资料，注册时强制完善，不需要强制完善</span>
+			     		</div>	
+			     	</div>
+			     	<div class="form-group">
+						<label class="col-sm-3 control-label">服务条款和隐私政策</label>
+				      	<div class="col-sm-5">
+				      		<div class="input-group">
+				      			<div class="">
+								    <label class="radio-inline">
+								    	<input type="radio" name="rules" value="open" v-model="rules" /> 开启
+								    </label>
+										<label class="radio-inline">
+								    	<input type="radio" name="rules" checked="checked" value="close" v-model="rules" /> 关闭
+								    </label>
+								</div>
+							</div>
+					    </div>
+			     		<div class="col-md-4">
+			     			<span class="help-block">服务条款和隐私政策</span>
+			     		</div>		
+			     	</div>
+			     	<div class="form-group" v-if="rules === 'open'">
+			     		<label  class="col-sm-3 control-label" for="rule-content">条款内容</label>
+			     		<div class="col-sm-5">
+				          <textarea 
+				          class="form-control question-editor" 
+				          rows="12" 
+				          placeholder="输入内容..." 
+				          v-model="content" 
+				          autofocus 
+				          required>
+				          </textarea>
+			     		</div>
+			     		<div class="col-md-4">
+			     			<span class="help-block">支持 Markdown 格式</span>
+			     		</div>		
+			     	</div>
+			     	<!-- Button -->
+			      <div class="form-group">
+			        <div class="col-sm-offset-3 col-sm-4">
+			          <button v-if="loading" type="button" class="btn btn-primary" disabled="disabled">
+			            <span class="glyphicon glyphicon-refresh component-loadding-icon"></span>
+			          </button>
+			          <button v-else type="button" class="btn btn-primary" @click="saveConfig">保存设置</button>
+			        </div>
+			        <div class="col-sm-4">
+			        	<p class="text-success">{{ message }}</p>
+			        </div>
+			      </div>
+			    </div>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
 	import request, { createRequestURI } from '../../util/request';
 	import lodash from 'lodash';
-	import VueEditor from '../edit.md/components/index';
-	
-	const RegisterSetting = {
-		components: {
-			VueEditor
-		},
+	import markdownIt from 'markdown-it';
 
+	const md = markdownIt({
+	  html: false
+	});
+	const RegisterSetting = {
+    	name: 'question-edit',
 		data: () => ({
 			rules: 'close',
 			fixed: 'need',
 			method: 'all',
 			type: 'all',
-			content: '# 服务条款及隐私政策',
 			loading: false,
-			message: ''
+			message: '',
+			content: '',
 		}),
+		computed: {
+		  preview() {
+		    return md.render(this.content);
+		  }
+		},
 		methods: {
-			input(val) {
-				this.content = val;
-			},
-
 			saveConfig() {
 				this.loading = true;
 				const { rules, fixed, method, type, content } = this;
@@ -176,7 +205,7 @@
 				this.method = data.method;
 				this.fixed = data.fixed;
 				this.type = data.type;
-				this.content = data.content;
+				this.content = data.content ? data.content : '# 服务条款及隐私政策';
 			})
 		}
 	};
