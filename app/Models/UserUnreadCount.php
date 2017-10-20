1dccfb4e44f885d825a66b9941a520bad8f14c62
白石 <414606094@qq.com>
2017-10-20 10:44:57 +0800

    设置用户未读模型默认主键

--- app/Models/UserUnreadCount.php
+++ app/Models/UserUnreadCount.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserUnreadCount extends Model
 {
+
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'user_id';
+
     protected $table = 'user_unread_counts';
 }
