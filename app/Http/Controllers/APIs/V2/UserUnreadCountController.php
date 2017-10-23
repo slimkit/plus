9cc7921ad33b8d967d639a034dce349f4de5cc39
白石 <414606094@qq.com>
2017-10-23 16:59:44 +0800

    增加未处理审核统计

--- app/Http/Controllers/APIs/V2/UserUnreadCountController.php
+++ app/Http/Controllers/APIs/V2/UserUnreadCountController.php
@@ -6,6 +6,7 @@ use DB;
 use Illuminate\Http\Request;
 use Zhiyi\Plus\Models\Like as LikeModel;
 use Zhiyi\Plus\Models\Comment as CommentModel;
+use Zhiyi\Plus\Support\PinnedsNotificationEventer;
 
 class UserUnreadCountController extends Controller
 {
@@ -18,7 +19,7 @@ class UserUnreadCountController extends Controller
      * @return mixed
      * @author BS <414606094@qq.com>
      */
-    public function index(Request $request, CommentModel $commentModel, LikeModel $likeModel)
+    public function index(Request $request, CommentModel $commentModel, LikeModel $likeModel, PinnedsNotificationEventer $eventer)
     {
         $user = $request->user();
         $counts = $user->unreadCount;
@@ -46,10 +47,20 @@ class UserUnreadCountController extends Controller
             ->orderBy('id', 'desc')
             ->get();
 
+        // 未处理审核统计
+        $pinneds = $eventer->dispatch()->mapWithKeys(function ($pinnedModels) use ($user) {
+            $model = new $pinnedModels['namespace']();
+
+            $pinneds[$pinnedModels['name']] = $model->where($pinnedModels['owner_prefix'], $user->id)->where($pinnedModels['audit_prefix'], $pinnedModels['unaudited_value'])->count();
+
+            return $pinneds;
+        });
+
         return response()->json([
             'counts' => $counts,
             'comments' => $comments,
             'likes' => $likes,
+            'pinneds' => $pinneds,
         ], 200);
     }
 }
