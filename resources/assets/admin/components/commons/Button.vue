1b3a7d28130734f5dc5614e7939bf17c5f805735
Seven Du <shiweidu@outlook.com>
2017-10-24 09:51:06 +0800

    增加高度封装按钮组件。

---
+++ resources/assets/admin/components/commons/Button.vue
@@ -0,0 +1,28 @@
+<template>
+  <ui-process-button v-bind="$props" @click="handleClick">
+    <template scope="{ processing }">
+      <template v-if="processing">
+        <ui-loadding></ui-loadding>
+        {{ procesLable }}
+      </template>
+      <template v-else>
+        {{ lable }}
+      </template>
+    </template>
+  </ui-process-button>
+</template>
+
+<script>
+export default {
+  name:'ui-button',
+  props: {
+    lable: { type: String, default: '提交' },
+    procesLable: { type: String, default: '提交中...' },
+  },
+  methods: {
+    handleClick (event) {
+      this.$emit('click', event);
+    }
+  },
+};
+</script>
