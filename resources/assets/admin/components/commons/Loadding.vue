67a1941e5890235923f6b1ea5c95aeca5287585d
Seven Du <shiweidu@outlook.com>
2017-10-24 09:50:45 +0800

    增加 loadding 组件

---
+++ resources/assets/admin/components/commons/Loadding.vue
@@ -0,0 +1,26 @@
+<template>
+  <span class="glyphicon glyphicon-refresh loadding-icon" aria-hidden="true" v-bind="$props"></span>
+</template>
+
+<script>
+export default { name: 'ui-loadding' };
+</script>
+
+<style>
+/* 旋转动画 */
+@keyframes turn-around {
+  from {
+    transform: rotate(1deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+.loadding-icon {
+  animation-name: turn-around;
+  animation-duration: 1.4s;
+  animation-timing-function: linear;
+  animation-iteration-count: infinite;
+}
+</style>
