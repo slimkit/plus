a5301b49dbf37bc0d4b3520c356330cbb1fddf63
Seven Du <shiweidu@outlook.com>
2017-10-25 14:08:35 +0800

    更新公开磁盘描述，增加更准确的提示。

--- resources/assets/admin/components/modules/cdn/filesystems/Public.vue
+++ resources/assets/admin/components/modules/cdn/filesystems/Public.vue
@@ -1,13 +1,15 @@
 <template>
   <!-- 提交按钮 -->
   <div class="form-group">
-    <div class="col-sm-offset-2 col-sm-4">
+    <div class="col-sm-offset-2 col-sm-10">
       <ui-button type="button" class="btn btn-primary" @click="handleSubmit"></ui-button>
     </div>
-    <span class="col-sm-6 help-block">
-      设置公开磁盘，储存目录为 storage/app/public 如果你切换了磁盘，请手动迁移文件。
-    </span>
+    <div class="col-sm-12 help-block">
+      设置公开磁盘，储存目录为 storage/app/public 如果你切换了磁盘，请手动迁移文件。<br />
+      公开磁盘为了能在 public 下访问，你需要在运行：<code>php artisan storage:link</code>
+    </div>
   </div>
+
 </template>
 
 <script>
