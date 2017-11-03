8ec637563952522e7340f197cea25b2049b2f176
Seven <shiweidu@outlook.com>
2017-11-03 03:34:39 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,6 @@
 
 namespace Zhiyi\Plus\Http\Controllers;
 
-use Zhiyi\Plus\Support\BootstrapAPIsEventer;
-use Illuminate\Contracts\Routing\ResponseFactory;
-
 class HomeController
 {
     public function welcome()
@@ -13,33 +10,31 @@ class HomeController
         $h5Config = config('h5');
 
         if ($h5Config['installed'] && $this->isMobile()) {
-
             return redirect()->route($h5Config['routeName']);
         }
 
-        if ($pcConfig['installed'] && !$this->isMobile()) {
-
+        if ($pcConfig['installed'] && ! $this->isMobile()) {
             return redirect()->route($pcConfig['routeName']);
         }
 
         return view('welcome');
     }
 
-    public function isMobile() { 
+    public function isMobile()
+    {
         // 如果有HTTP_X_WAP_PROFILE则一定是移动设备
         if (isset($_SERVER['HTTP_X_WAP_PROFILE'])) {
-            
             return true;
-        } 
+        }
         // 如果via信息含有wap则一定是移动设备,部分服务商会屏蔽该信息
-        if (isset($_SERVER['HTTP_VIA'])) { 
+        if (isset($_SERVER['HTTP_VIA'])) {
             // 找不到为flase,否则为true
-            
-            return stristr($_SERVER['HTTP_VIA'], "wap") ? true : false;
-        } 
+
+            return stristr($_SERVER['HTTP_VIA'], 'wap') ? true : false;
+        }
         // 脑残法，判断手机发送的客户端标志,兼容性有待提高。其中'MicroMessenger'是电脑微信
         if (isset($_SERVER['HTTP_USER_AGENT'])) {
-            $clientkeywords = array(
+            $clientkeywords = [
                 'nokia',
                 'sony',
                 'ericsson',
@@ -72,24 +67,22 @@ class HomeController
                 'midp',
                 'wap',
                 'mobile',
-                'MicroMessenger'
-            ); 
+                'MicroMessenger',
+            ];
             // 从HTTP_USER_AGENT中查找手机浏览器的关键字
-            if (preg_match("/(" . implode('|', $clientkeywords) . ")/i", strtolower($_SERVER['HTTP_USER_AGENT']))) {
-                
+            if (preg_match('/('.implode('|', $clientkeywords).')/i', strtolower($_SERVER['HTTP_USER_AGENT']))) {
                 return true;
-            } 
-        } 
+            }
+        }
         // 协议法，因为有可能不准确，放到最后判断
-        if (isset ($_SERVER['HTTP_ACCEPT'])) { 
+        if (isset($_SERVER['HTTP_ACCEPT'])) {
             // 如果只支持wml并且不支持html那一定是移动设备
             // 如果支持wml和html但是wml在html之前则是移动设备
             if ((strpos($_SERVER['HTTP_ACCEPT'], 'vnd.wap.wml') !== false) && (strpos($_SERVER['HTTP_ACCEPT'], 'text/html') === false || (strpos($_SERVER['HTTP_ACCEPT'], 'vnd.wap.wml') < strpos($_SERVER['HTTP_ACCEPT'], 'text/html')))) {
-                
                 return true;
-            } 
-        } 
-        
+            }
+        }
+
         return false;
     }
 }
