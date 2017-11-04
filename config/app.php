49029a4a49eb17438618fea95a98554f5f1eb65f
boxshadow <idafoo@sina.com>
2017-11-05 01:13:24 +0800

    主干增加微信服务号授权登陆测试

--- config/app.php
+++ config/app.php
@@ -228,6 +228,7 @@ return [
         'Validator'    => Illuminate\Support\Facades\Validator::class,
         'View'         => Illuminate\Support\Facades\View::class,
         'Image'        => Intervention\Image\Facades\Image::class,
+        'EasyWeChat'   => Overtrue\LaravelWeChat\Facade::class,
     ],
 
 ];
