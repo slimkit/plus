d3849b16ce933c77fbee7c8286100a7d3ac57eec
Seven Du <shiweidu@outlook.com>
2017-12-14 13:19:57 +0800

    增加数据获取测试代码。

--- app/Http/Controllers/APIs/V2/PingPlusPlusChargeWebHooks.php
+++ app/Http/Controllers/APIs/V2/PingPlusPlusChargeWebHooks.php
@@ -3,13 +3,22 @@
 namespace Zhiyi\Plus\Http\Controllers\APIs\V2;
 
 use Illuminate\Http\Request;
+use Zhiyi\Plus\Repository\WalletPingPlusPlus;
 
 class PingPlusPlusChargeWebHooks
 {
-    public function webhook(Request $request)
+    public function webhook(Request $request, WalletPingPlusPlus $repository)
     {
-        dd(
-            $request
-        );
+        $signature = $request->headers->get('x-pingplusplus-signature');
+        $pingPlusPlusPublicCertificate = $repository->get()['secret_key'] ?? null;
+        $signed = openssl_verify($request->getContent(), base64_decode($signature), $pingPlusPlusPublicCertificate, OPENSSL_ALGO_SHA256);
+
+        if (! $signed) {
+            return response('加密验证失败', 422);
+        }
+
+        $pingPlusPlusCharge = $request->json('data.object');
+
+        return response()->json($pingPlusPlusCharge);
     }
 }
