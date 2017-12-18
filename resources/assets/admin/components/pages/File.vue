09e037e465318da70b63eeb41c07d064ab1eb724
白石 <414606094@qq.com>
2017-12-18 17:56:01 +0800

    fix: 移除错误的nav

--- resources/assets/admin/components/pages/File.vue
+++ resources/assets/admin/components/pages/File.vue
@@ -14,11 +14,6 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <!-- The settings nav. -->
-          <ul class="nav navbar-nav">
-            <router-link to="/setting" tag="li" active-class="active" exact>
-              <a href="#">上传设置</a>
-            </router-link>
-          </ul>
         </div> 
       </div>   
     </nav>
