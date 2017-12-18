c47830403cec8c4309bf7c382892c634f3d18d58
Seven Du <shiweidu@outlook.com>
2017-12-18 16:44:50 +0800

    fix: 修复填充 news 广告数据错误

--- packages/slimkit-plus-news/database/seeds/AdvertisingSpaceTableSeeder.php
+++ packages/slimkit-plus-news/database/seeds/AdvertisingSpaceTableSeeder.php
@@ -3,6 +3,7 @@
 namespace SlimKit\Plus\Packages\News\Seeds;
 
 use Illuminate\Database\Seeder;
+use Zhiyi\Plus\Models\AdvertisingSpace;
 
 class AdvertisingSpaceTableSeeder extends Seeder
 {
@@ -17,39 +18,88 @@ class AdvertisingSpaceTableSeeder extends Seeder
         AdvertisingSpace::create([
             'channel' => 'news',
             'space' => 'news:list:top',
-            'alias' => '资讯列表顶部广告',
+            'alias' => '移动端资讯列表顶部广告',
             'allow_type' => 'image',
             'format' => [
                 'image' => [
-                    'image' => '图片|string',
-                    'link' => '链接|string',
+                    'image' => '图片|string|必填，广告图',
+                    'link' => '链接|string|必填，广告链接',
+                ],
+            ],
+            'rule' => [
+                'image' => [
+                    'image' => 'required|url',
+                    'link'  => 'required|url',
+                ],
+            ],
+            'message' => [
+                'image' => [
+                    'image.required' => '广告图链接不能为空',
+                    'image.url' => '广告图链接无效',
+                    'link.required' => '广告链接不能为空',
+                    'link.url' => '广告链接无效',
                 ],
             ],
         ]);
         AdvertisingSpace::create([
             'channel' => 'news',
             'space' => 'news:single',
-            'alias' => '资讯详情广告',
+            'alias' => '移动端资讯详情广告',
             'allow_type' => 'image',
             'format' => [
                 'image' => [
-                    'image' => '图片|string',
-                    'link' => '链接|string',
+                    'image' => '图片|string|必填，广告图',
+                    'link' => '链接|string|必填，广告链接',
+                ],
+            ],
+            'rule' => [
+                'image' => [
+                    'image' => 'required|url',
+                    'link'  => 'required|url',
+                ],
+            ],
+            'message' => [
+                'image' => [
+                    'image.required' => '广告图链接不能为空',
+                    'image.url' => '广告图链接无效',
+                    'link.required' => '广告链接不能为空',
+                    'link.url' => '广告链接无效',
                 ],
             ],
         ]);
         AdvertisingSpace::create([
             'channel' => 'news',
             'space' => 'news:list:analog',
-            'alias' => '资讯列表模拟数据广告',
+            'alias' => '移动端资讯列表模拟数据广告',
             'allow_type' => 'news:analog',
             'format' => [
                 'news:analog' => [
-                    'title' => '标题|string',
-                    'image' => '图片|image',
-                    'from' => '来源|string',
-                    'time' => '时间|date',
-                    'link' => '链接|string',
+                    'title' => '标题|string|必填，广告资讯标题',
+                    'image' => '图片|image|必填，广告图片链接',
+                    'from' => '来源|string|必填，广告资讯来源',
+                    'time' => '时间|date|必填，广告资讯时间',
+                    'link' => '链接|string|必填，广告链接',
+                ],
+            ],
+            'rule' => [
+                'news:analog' => [
+                    'image' => 'required|url',
+                    'link'  => 'required|url',
+                    'time' => 'required|date',
+                    'title' => 'required',
+                    'from' => 'required',
+                ],
+            ],
+            'message' => [
+                'news:analog' => [
+                   'image.required' => '广告图链接不能为空',
+                   'image.url' => '广告图链接无效',
+                   'link.required' => '广告链接不能为空',
+                   'link.url' => '广告链接无效',
+                   'time.required' => '广告资讯时间必填',
+                   'time.date' => '时间格式错误',
+                   'from.required' => '广告资讯来源必填',
+                   'title.required' => '广告资讯标题必填',
                 ],
             ],
         ]);
