532e1eb9db9dd30dec9fc4f96f9d90306eece3e0
ZsyD <1251992018@qq.com>
2017-12-20 09:16:32 +0800

    chore: 环信未配置时的兼容

--- app/EaseMobIM/Controllers/EaseMobController.php
+++ app/EaseMobIM/Controllers/EaseMobController.php
@@ -26,19 +26,34 @@ class EaseMobController
     // 环信注册类型  0-开放注册，1-授权注册
     protected $register_type;
 
-    public function __construct()
+    // 环信是否开启
+    protected $open = false;
+
+    private function getConfig($callback)
     {
+        $this->open = config('easemob.open');
+        $this->client_id = config('easemob.client_id');
+        $this->client_secret = config('easemob.client_secret');
+
+        if (!$this->open || !$this->client_id || !$this->client_secret) {
+
+            return response()->json([
+                'message' => ['环信未配置'],
+            ])->setStatusCode(201);
+        }
+        $this->open = config('easemob.open');
         $this->client_id = config('easemob.client_id');
         $this->client_secret = config('easemob.client_secret');
         // 应用标识
         $app_key = explode('#', config('easemob.app_key'));
-
         $this->org_name = $app_key[0];
         $this->app_name = $app_key[1];
-        $this->register_type = 0;
+        $this->register_type = config('easemob.register_type');
         if (! empty($this->org_name) && ! empty($this->app_name)) {
             $this->url = 'https://a1.easemob.com/'.$this->org_name.'/'.$this->app_name.'/';
         }
+
+        return $callback();
     }
 
     public function getToken()
@@ -67,27 +82,31 @@ class EaseMobController
      */
     public function openRegister(Request $request)
     {
-        $options['username'] = $request->user_id;
-        $options['password'] = $this->getImPwdHash($request->user_id);
-        $url = $this->url.'users';
+        $callback = function () use ($request) {
+            $options['username'] = $request->user_id;
+            $options['password'] = $this->getImPwdHash($request->user_id);
+            $url = $this->url.'users';
 
-        $data['headers'] = ['Content-Type' => 'application/json'];
-        $data['body'] = json_encode($options);
-        $data['http_errors'] = false;
-        $Client = new Client();
-        $result = $Client->request('post', $url, $data);
+            $data['headers'] = ['Content-Type' => 'application/json'];
+            $data['body'] = json_encode($options);
+            $data['http_errors'] = false;
+            $Client = new Client();
+            $result = $Client->request('post', $url, $data);
 
-        if ($result->getStatusCode() != 200) {
-            $error = $result->getBody()->getContents();
+            if ($result->getStatusCode() != 200) {
+                $error = $result->getBody()->getContents();
 
-            return response()->json([
-                'message' => [
-                    json_decode($error)->error_description,
-                ],
-            ])->setStatusCode(500);
-        }
+                return response()->json([
+                    'message' => [
+                        json_decode($error)->error_description,
+                    ],
+                ])->setStatusCode(500);
+            }
 
-        return response()->json([])->setStatusCode(201);
+            return response()->json([])->setStatusCode(201);
+        };
+
+        return $this->getConfig($callback);
     }
 
     /**
@@ -113,32 +132,37 @@ class EaseMobController
      */
     public function createUser(Request $request)
     {
-        if ($this->register_type == 0) {
-            return $this->openRegister($request);
-        }
+        $callback = function () use ($request) {
+            if ($this->register_type == 0) {
 
-        $url = $this->url.'users';
-        $options = [
-            'username' => $request->user_id,
-            'password' => $this->getImPwdHash($request->user_id),
-        ];
-        $data['body'] = json_encode($options);
-        $data['headers'] = [$this->getToken()];
-        $data['http_errors'] = false;
-        $Client = new Client();
-        $result = $Client->request('post', $url, $data);
+                return $this->openRegister($request);
+            }
 
-        if ($result->getStatusCode() != 200) {
-            $error = $result->getBody()->getContents();
+            $url = $this->url.'users';
+            $options = [
+                'username' => $request->user_id,
+                'password' => $this->getImPwdHash($request->user_id),
+            ];
+            $data['body'] = json_encode($options);
+            $data['headers'] = [$this->getToken()];
+            $data['http_errors'] = false;
+            $Client = new Client();
+            $result = $Client->request('post', $url, $data);
 
-            return response()->json([
-                'message' => [
-                    json_decode($error)->error_description,
-                ],
-            ])->setStatusCode(500);
-        }
+            if ($result->getStatusCode() != 200) {
+                $error = $result->getBody()->getContents();
 
-        return response()->json([])->setStatusCode(201);
+                return response()->json([
+                    'message' => [
+                        json_decode($error)->error_description,
+                    ],
+                ])->setStatusCode(500);
+            }
+
+            return response()->json([])->setStatusCode(201);
+        };
+
+        return $this->getConfig($callback);
     }
 
     /**
@@ -150,42 +174,46 @@ class EaseMobController
      */
     public function createUsers(Request $request)
     {
-        $user_ids = $request->input('user_ids');
-        $user_ids = is_array($user_ids) ? $user_ids : explode(',', $user_ids);
-        $options = [];
-        $users = User::when($user_ids, function ($query) use ($user_ids) {
-            return $query->whereIn('id', $user_ids);
-        })
-            ->select('id', 'password')
-            ->get();
+        $callback = function () use ($request) {
+            $user_ids = $request->input('user_ids');
+            $user_ids = is_array($user_ids) ? $user_ids : explode(',', $user_ids);
+            $options = [];
+            $users = User::when($user_ids, function ($query) use ($user_ids) {
+                return $query->whereIn('id', $user_ids);
+            })
+                ->select('id', 'password')
+                ->get();
 
-        foreach ($users as $user) {
-            $options[] = [
-                'username' => $user->id,
-                'password' => $user->getImPwdHash(),
+            foreach ($users as $user) {
+                $options[] = [
+                    'username' => $user->id,
+                    'password' => $user->getImPwdHash()
+                ];
+            }
+            $url = $this->url.'users';
+            $data['body'] = json_encode($options);
+            $data['headers'] = [
+                'Authorization' => $this->getToken()
             ];
-        }
-        $url = $this->url.'users';
-        $data['body'] = json_encode($options);
-        $data['headers'] = [
-            'Authorization' => $this->getToken(),
-        ];
-        $data['http_errors'] = false;
+            $data['http_errors'] = false;
 
-        $Client = new Client();
-        $result = $Client->request('post', $url, $data);
+            $Client = new Client();
+            $result = $Client->request('post', $url, $data);
 
-        if ($result->getStatusCode() != 200) {
-            $error = $result->getBody()->getContents();
+            if ($result->getStatusCode() != 200) {
+                $error = $result->getBody()->getContents();
 
-            return response()->json([
-                'message' => [
-                    json_decode($error)->error_description,
-                ],
-            ])->setStatusCode(500);
-        }
+                return response()->json([
+                    'message' => [
+                        json_decode($error)->error_description,
+                    ],
+                ])->setStatusCode(500);
+            }
 
-        return response()->json([])->setStatusCode(201);
+            return response()->json([])->setStatusCode(201);
+        };
+
+        return $this->getConfig($callback);
     }
 
     /**
@@ -197,32 +225,36 @@ class EaseMobController
      */
     public function resetPassword(Request $request)
     {
-        $url = $this->url.'users/'.$request->user_id.'/password';
+        $callback = function () use ($request) {
+            $url = $this->url.'users/'.$request->user_id.'/password';
 
-        $options = [
-            'oldpassword' => $request->old_pwd_hash,
-            'newpassword' => $this->getImPwdHash($request->user_id),
-        ];
-        $data['body'] = json_encode($options);
-        $data['headers'] = [
-            'Authorization' => $this->getToken(),
-        ];
-        $data['http_errors'] = false;
+            $options = [
+                'oldpassword' => $request->old_pwd_hash,
+                'newpassword' => $this->getImPwdHash($request->user_id),
+            ];
+            $data['body'] = json_encode($options);
+            $data['headers'] = [
+                'Authorization' => $this->getToken()
+            ];
+            $data['http_errors'] = false;
 
-        $Client = new Client();
-        $result = $Client->request('put', $url, $data);
+            $Client = new Client();
+            $result = $Client->request('put', $url, $data);
 
-        if ($result->getStatusCode() != 200) {
-            $error = $result->getBody()->getContents();
+            if ($result->getStatusCode() != 200) {
+                $error = $result->getBody()->getContents();
 
-            return response()->json([
-                'message' => [
-                    json_decode($error)->error_description,
-                ],
-            ])->setStatusCode(500);
-        }
+                return response()->json([
+                    'message' => [
+                        json_decode($error)->error_description,
+                    ],
+                ])->setStatusCode(500);
+            }
 
-        return response()->json([])->setStatusCode(201);
+            return response()->json([])->setStatusCode(201);
+        };
+
+        return $this->getConfig($callback);
     }
 
     /**
@@ -234,29 +266,34 @@ class EaseMobController
      */
     public function getUser(Request $request)
     {
-        $url = $this->url.'users/'.$request->user_id;
-        $data['headers'] = [
-            'Authorization' => $this->getToken(),
-        ];
-        $data['http_errors'] = false;
+        $callback = function () use ($request) {
+            $url = $this->url.'users/'.$request->user_id;
+            $data['headers'] = [
+                'Authorization' => $this->getToken()
+            ];
+            $data['http_errors'] = false;
 
-        $Client = new Client();
-        $result = $Client->request('get', $url, $data);
+            $Client = new Client();
+            $result = $Client->request('get', $url, $data);
 
-        if ($result->getStatusCode() == 404) {
-            $result = $this->createUser($request);
-        }
+            if ($result->getStatusCode() == 404) {
 
-        if ($result->getStatusCode() != 200) {
-            $error = $result->getBody()->getContents();
+                $result = $this->createUser($request);
+            }
 
-            return response()->json([
-                'message' => [
-                    json_decode($error)->error_description,
-                ],
-            ])->setStatusCode(500);
-        }
+            if ($result->getStatusCode() != 200) {
+                $error = $result->getBody()->getContents();
 
-        return response()->json([])->setStatusCode(201);
+                return response()->json([
+                    'message' => [
+                        json_decode($error)->error_description,
+                    ],
+                ])->setStatusCode(500);
+            }
+
+            return response()->json([])->setStatusCode(201);
+        };
+
+        return $this->getConfig($callback);
     }
 }
--- config/easemob.php
+++ config/easemob.php
@@ -3,5 +3,7 @@
 return [
     'client_id' => '',
     'client_secret' => '',
-    'app_key' => '',
+    'app_key' => '#',
+    'register_type' => 0,
+    'open' => false,
 ];
