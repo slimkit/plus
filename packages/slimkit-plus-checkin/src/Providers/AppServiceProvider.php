d3bedb080453b5c912c94788c905f533d86fb773
Seven Du <shiweidu@outlook.com>
2017-12-21 09:27:25 +0800

    chore: Removed checkin package handles

--- packages/slimkit-plus-checkin/src/Providers/AppServiceProvider.php
+++ packages/slimkit-plus-checkin/src/Providers/AppServiceProvider.php
@@ -18,7 +18,6 @@
 
 namespace SlimKit\PlusCheckIn\Providers;
 
-use Zhiyi\Plus\Support\PackageHandler;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -38,9 +37,6 @@ class AppServiceProvider extends ServiceProvider
 
         // Register view namespace.
         $this->loadViewsFrom($this->app->make('path.checkin.view'), 'plus-checkin');
-
-        // Register singletons.
-        $this->registerSingletions();
 
         // Publish public resource.
         $this->publishes([
@@ -68,75 +64,7 @@ class AppServiceProvider extends ServiceProvider
         $this->app->instance('path.checkin.lang', $path.'/resource/lang');
         $this->app->instance('path.checkin.view', $path.'/resource/views');
 
-        // register cntainer aliases
-        $this->registerContainerAliases();
-
-        // Register Plus package handlers.
-        $this->registerPackageHandlers();
-
         // Merge config.
         $this->mergeConfigFrom($configFilename, 'checkin');
-    }
-
-    /**
-     * Register singletons.
-     *
-     * @return void
-     */
-    protected function registerSingletions()
-    {
-        // Owner handler.
-        $this->app->singleton('plus-checkin:handler', function () {
-            return new \SlimKit\PlusCheckIn\Handlers\PackageHandler();
-        });
-
-        // Develop handler.
-        $this->app->singleton('plus-checkin:dev-handler', function ($app) {
-            return new \SlimKit\PlusCheckIn\Handlers\DevPackageHandler($app);
-        });
-    }
-
-    /**
-     * Register container aliases.
-     *
-     * @return void
-     */
-    protected function registerContainerAliases()
-    {
-        foreach ([
-            'plus-checkin:handler' => [
-                \SlimKit\PlusCheckIn\Handlers\PackageHandler::class,
-            ],
-            'plus-checkin:dev-handler' => [
-                \SlimKit\PlusCheckIn\Handlers\DevPackageHandler::class,
-            ],
-        ] as $key => $aliases) {
-            foreach ($aliases as $alias) {
-                $this->app->alias($key, $alias);
-            }
-        }
-    }
-
-    /**
-     * Register Plus package handlers.
-     *
-     * @return void
-     */
-    protected function registerPackageHandlers()
-    {
-        $this->loadHandleFrom('checkin', 'plus-checkin:handler');
-        $this->loadHandleFrom('checkin-dev', 'plus-checkin:dev-handler');
-    }
-
-    /**
-     * Register handler.
-     *
-     * @param string $name
-     * @param \Zhiyi\Plus\Support\PackageHandler|string $handler
-     * @return void
-     */
-    private function loadHandleFrom(string $name, $handler)
-    {
-        PackageHandler::loadHandleFrom($name, $handler);
     }
 }
