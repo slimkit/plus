c4f73ff138c118ad14805f21dfc0e97cfeef03cf
Seven Du <shiweidu@outlook.com>
2017-12-26 17:19:30 +0800

    chore: Add a new wallet model

---
+++ app/Models/NewWallet.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Zhiyi\Plus\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class NewWallet extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'wallet';
+
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'owner_id';
+
+    /**
+     * The "type" of the auto-incrementing ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'int';
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The wallet owner.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function owner(): HasOne
+    {
+        return $this->hasOne(User::class, 'id', 'owner_id');
+    }
+}
