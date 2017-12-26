3213e881830f146e947646bfc8b5c8215d8bc4f3
Seven Du <shiweidu@outlook.com>
2017-12-26 17:28:36 +0800

    chore: Init WalletOrder model

--- app/Models/WalletOrder.php
+++ app/Models/WalletOrder.php
@@ -19,8 +19,36 @@
 namespace Zhiyi\Plus\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class WalletOrder extends Model
 {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'wallet_orders';
+
+    /**
+     * The order owner.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function owner(): HasOne
+    {
+        return $this->hasOne(User::class, 'id', 'owner_id');
+    }
+
+    /**
+     * The order owner wallet.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function wallet(): HasOne
+    {
+        return $this->hasOne(NewWallet::class, 'owner_id', 'owner_id');
+    }
 }
