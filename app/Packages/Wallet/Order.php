16c452d179c04e6d4d07a5dd24703a6f407ca471
Seven <shiweidu@outlook.com>
2017-12-28 13:51:14 +0000

    Apply fixes from StyleCI

--- app/Packages/Wallet/Order.php
+++ app/Packages/Wallet/Order.php
@@ -1,8 +1,23 @@
 <?php
 
+/*
+ * +----------------------------------------------------------------------+
+ * |                          ThinkSNS Plus                               |
+ * +----------------------------------------------------------------------+
+ * | Copyright (c) 2017 Chengdu ZhiYiChuangXiang Technology Co., Ltd.     |
+ * +----------------------------------------------------------------------+
+ * | This source file is subject to version 2.0 of the Apache license,    |
+ * | that is bundled with this package in the file LICENSE, and is        |
+ * | available through the world-wide-web at the following url:           |
+ * | http://www.apache.org/licenses/LICENSE-2.0.html                      |
+ * +----------------------------------------------------------------------+
+ * | Author: Slim Kit Group <master@zhiyicx.com>                          |
+ * | Homepage: www.thinksns.com                                           |
+ * +----------------------------------------------------------------------+
+ */
+
 namespace Zhiyi\Plus\Packages\Wallet;
 
-use Zhiyi\Plus\Models\User as UserModel;
 use Zhiyi\Plus\Models\WalletOrder as WallerOrderModel;
 
 class Order
@@ -50,7 +65,7 @@ class Order
     }
 
     /**
-     * Set order model
+     * Set order model.
      *
      * @param \Zhiyi\Plus\Models\WalletOrder $order
      * @author Seven Du <shiweidu@outlook.com>
