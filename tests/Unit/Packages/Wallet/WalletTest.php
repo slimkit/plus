fa463684dad5c5e3760de3427749f789caf76b12
Seven Du <shiweidu@outlook.com>
2017-12-29 13:11:59 +0800

    test: Reactor Wallet::class unit test.

--- tests/Unit/Packages/Wallet/WalletTest.php
+++ tests/Unit/Packages/Wallet/WalletTest.php
@@ -21,12 +21,10 @@ namespace Zhiyi\Plus\Tests\Unit\Packages\Wallet;
 use Zhiyi\Plus\Tests\TestCase;
 use Zhiyi\Plus\Packages\Wallet\Wallet;
 use Zhiyi\Plus\Models\User as UserModel;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Zhiyi\Plus\Models\NewWallet as WalletModel;
 
 class WalletTest extends TestCase
 {
-    use DatabaseTransactions;
-
     /**
      * Test setUser method.
      *
@@ -37,17 +35,83 @@ class WalletTest extends TestCase
      */
     public function testSetUser()
     {
-        $user = factory(UserModel::class)->create();
-        $wallet = new TestWalletSetUser($user);
+        $user =factory(UserModel::class)->make();
+        $user->id = 1;
 
-        $this->assertSame($user->id, $wallet->getUser()->id);
+        // Create a wallet mock.
+        $wallet = $this->getMockBuilder(TestWalletSetUser::class)
+                       ->setMethods(['userFindOrFail', 'resolveWallet'])
+                       ->getMock();
+        $wallet->expects($this->exactly(1))
+               ->method('userFindOrFail')
+               ->will($this->returnValue($user));
+        $wallet->expects($this->exactly(2))
+               ->method('resolveWallet')
+               ->will($this->returnArgument(0));
 
         $wallet->setUser($user->id);
         $this->assertInstanceOf(UserModel::class, $wallet->getUser());
         $this->assertSame($user->id, $wallet->getUser()->id);
 
+        $wallet->setUser($user);
+        $this->assertSame($user->id, $wallet->getUser()->id);
+
         // @expectedException \Exception
         $wallet->setUser([]);
+    }
+
+    /**
+     * Test getWalletModel method.
+     *
+     * @expectedException \Exception
+     * @expectedExceptionMessage 没有设置钱包用户
+     * @return void
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function testGetWalletModel()
+    {
+        $user =factory(UserModel::class)->make();
+        $user->id = 1;
+
+        // Create a wallet mock.
+        $wallet = $this->getMockBuilder(Wallet::class)
+                       ->setMethods(['walletFind'])
+                       ->getMock();
+        $wallet->expects($this->exactly(1))
+               ->method('walletFind')
+               ->willReturn(null);
+
+        $wallet->setUser($user);
+        $this->assertInstanceOf(WalletModel::class, $wallet->getWalletModel());
+
+        // test exception.
+        $wallet = new Wallet();
+        $wallet->getWalletModel();
+    }
+
+    public function testIncrement()
+    {
+        // Create a wallet model mock.
+        $model = $this->getMockBuilder(WalletModel::class)
+                      ->setMethods(['save'])
+                      ->getMock();
+        $model->expects($this->once())
+              ->method('save')
+              ->willReturn($model);
+
+        // Create a Wallet mock.
+        $wallet = $this->getMockBuilder(Wallet::class)
+                       ->setMethods(['getWalletModel'])
+                       ->getMock();
+        $wallet->expects($this->exactly(3))
+               ->method('getWalletModel')
+               ->willReturn($model);
+
+        $amount = 100;
+        $wallet->increment($amount);
+
+        $this->assertSame($amount, $wallet->getWalletModel()->balance);
+        $this->assertSame($amount, $wallet->getWalletModel()->total_income);
     }
 }
 
