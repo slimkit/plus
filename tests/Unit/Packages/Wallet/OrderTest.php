93a55d2c9e726a8fc55eefb2540bd13f35b08354
Seven <shiweidu@outlook.com>
2017-12-29 09:33:31 +0000

    Apply fixes from StyleCI

--- tests/Unit/Packages/Wallet/OrderTest.php
+++ tests/Unit/Packages/Wallet/OrderTest.php
@@ -1,12 +1,27 @@
 <?php
 
+/*
+ * +----------------------------------------------------------------------+
+ * |                          ThinkSNS Plus                               |
+ * +----------------------------------------------------------------------+
+ * | Copyright (c) 2017 Chengdu ZhiYiChuangXiang Technology Co., Ltd.     |
+ * +----------------------------------------------------------------------+
+ * | This source file is subject to version 2.0 of the Apache license,    |
+ * | that is bundled with this package in the file LICENSE, and is        |
+ * | available through the world-wide-web at the following url:           |
+ * | http://www.apache.org/licenses/LICENSE-2.0.html                      |
+ * +----------------------------------------------------------------------+
+ * | Author: Slim Kit Group <master@zhiyicx.com>                          |
+ * | Homepage: www.thinksns.com                                           |
+ * +----------------------------------------------------------------------+
+ */
+
 namespace Zhiyi\Plus\Tests\Unit\Packages\Wallet;
 
 use Zhiyi\Plus\Tests\TestCase;
 use Zhiyi\Plus\Packages\Wallet\Order;
 use Zhiyi\Plus\Packages\Wallet\TargetTypeManager;
 use Zhiyi\Plus\Models\WalletOrder as WalletOrderModel;
-use Zhiyi\Plus\Packages\Wallet\TargetTypes\UserTarget;
 
 class OrderTest extends TestCase
 {
