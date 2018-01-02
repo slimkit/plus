9e89cb312c026e3362150a48922c5d9e9a9107f9
白石 <414606094@qq.com>
2018-01-02 14:02:58 +0800

    feat: 完成提现记录保存

--- app/Packages/Wallet/TargetTypes/WidthdrawTarget.php
+++ app/Packages/Wallet/TargetTypes/WidthdrawTarget.php
@@ -18,14 +18,71 @@
 
 namespace Zhiyi\Plus\Packages\Wallet\TargetTypes;
 
+use DB;
+use Zhiyi\Plus\Packages\Wallet\Order;
+use Zhiyi\Plus\Packages\Wallet\Wallet;
+use Zhiyi\Plus\Models\WalletOrder as WalletOrderModel;
+
 class WidthdrawTarget extends Target
 {
     const ORDER_TITLE = '提现';
-    protected $ownerWallet;
+    protected $wallet;
 
+    protected $method = [
+        Order::TYPE_INCOME => 'increment',
+        Order::TYPE_EXPENSES => 'decrement',
+    ];
+
+    /**
+     * Handle
+     *
+     * @return mixed
+     * @author BS <414606094@qq.com>
+     */
     public function handle(): bool
     {
-        dd(1);
+        if (! $this->order->hasWait()) {
+
+            return true;
+        }
+
+        $this->initWallet();
+
+        $orderHandle = function () {
+            $this->order->saveStateSuccess();
+            $this->wallet->{$this->method[$this->order->getOrderModel()->type]}($this->order->getOrderModel()->amount);
+
+            return true;
+        };
+        $orderHandle->bindTo($this);
+
+        if (($result = DB::transaction($orderHandle)) === true) {
+            $this->sendNotification();
+        }
+
+        return $result;
+    }
+
+    /**
+     * 完成后的通知操作.
+     *
+     * @return void
+     * @author BS <414606094@qq.com>
+     */
+    protected function sendNotification()
+    {
         // TODO
     }
+
+    /**
+     * 初始化钱包.
+     *
+     * @return void
+     * @author BS <414606094@qq.com>
+     */
+    protected function initWallet()
+    {
+        $this->wallet = new Wallet($this->order->getOrderModel()->owner_id);
+    }
+
 }
