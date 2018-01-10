850ae9f4a95bb941cf4b60630f83834223bc232e
Seven Du <shiweidu@outlook.com>
2018-01-10 16:47:33 +0800

    chore: Remove a junk code

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -20,7 +20,6 @@ declare(strict_types=1);
 
 namespace Zhiyi\Plus\Providers;
 
-use Zhiyi\Plus\Auth\TokenGuard;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -42,9 +41,5 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
-        $this->app->auth->extend('token', function ($app) {
-            return $app->make(TokenGuard::class);
-        });
     }
 }
