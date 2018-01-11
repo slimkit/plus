920fc3349a05b670effce0ef1dd77b087da35171
Seven Du <shiweidu@outlook.com>
2018-01-11 15:20:43 +0800

    chore(Music): Deleted Music package not using routes.

--- packages/slimkit-plus-music/routes/web.php
+++ packages/slimkit-plus-music/routes/web.php
@@ -18,8 +18,8 @@ declare(strict_types=1);
  * +----------------------------------------------------------------------+
  */
 
-Route::any('/component-example', 'ExampleWebController@example');
-Route::any('/component-example/admin', 'ExampleWebController@admin')
-    ->middleware('auth:web')
-    ->name('example.admin');
+// Route::any('/component-example', 'ExampleWebController@example');
+// Route::any('/component-example/admin', 'ExampleWebController@admin')
+//     ->middleware('auth:web')
+//     ->name('example.admin');
     // ->middleware('role:admin');
