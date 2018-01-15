d7d2ef7031a13c17047430416ef96c82a8106fed
Seven Du <shiweidu@outlook.com>
2018-01-15 08:59:38 +0000

    Apply fixes from StyleCI

--- app/Packages/Currency/Order.php
+++ app/Packages/Currency/Order.php
@@ -2,6 +2,22 @@
 
 declare(strict_types=1);
 
+/*
+ * +----------------------------------------------------------------------+
+ * |                          ThinkSNS Plus                               |
+ * +----------------------------------------------------------------------+
+ * | Copyright (c) 2017 Chengdu ZhiYiChuangXiang Technology Co., Ltd.     |
+ * +----------------------------------------------------------------------+
+ * | This source file is subject to version 2.0 of the Apache license,    |
+ * | that is bundled with this package in the file LICENSE, and is        |
+ * | available through the world-wide-web at the following url:           |
+ * | http://www.apache.org/licenses/LICENSE-2.0.html                      |
+ * +----------------------------------------------------------------------+
+ * | Author: Slim Kit Group <master@zhiyicx.com>                          |
+ * | Homepage: www.thinksns.com                                           |
+ * +----------------------------------------------------------------------+
+ */
+
 namespace Zhiyi\Plus\Packages\Currency;
 
 use Zhiyi\Plus\Models\CurrencyOrder as CurrencyOrderModel;
@@ -9,7 +25,7 @@ use Zhiyi\Plus\Models\CurrencyOrder as CurrencyOrderModel;
 class Order
 {
     /**
-     *  Target types
+     *  Target types.
      */
     const TARGET_TYPE_COMMON = 'default'; // 默认流程
     const TARGET_TYPE_COMMODITY = 'commodity'; // 商品流程
--- app/Packages/Currency/Process.php
+++ app/Packages/Currency/Process.php
@@ -2,6 +2,22 @@
 
 declare(strict_types=1);
 
+/*
+ * +----------------------------------------------------------------------+
+ * |                          ThinkSNS Plus                               |
+ * +----------------------------------------------------------------------+
+ * | Copyright (c) 2017 Chengdu ZhiYiChuangXiang Technology Co., Ltd.     |
+ * +----------------------------------------------------------------------+
+ * | This source file is subject to version 2.0 of the Apache license,    |
+ * | that is bundled with this package in the file LICENSE, and is        |
+ * | available through the world-wide-web at the following url:           |
+ * | http://www.apache.org/licenses/LICENSE-2.0.html                      |
+ * +----------------------------------------------------------------------+
+ * | Author: Slim Kit Group <master@zhiyicx.com>                          |
+ * | Homepage: www.thinksns.com                                           |
+ * +----------------------------------------------------------------------+
+ */
+
 namespace Zhiyi\Plus\Packages\Currency;
 
 use Zhiyi\Plus\Models\User as UserModel;
@@ -11,7 +27,7 @@ use Zhiyi\Plus\Models\CurrencyOrder as CurrencyOrderModel;
 abstract class Process
 {
     /**
-     * 货币类型
+     * 货币类型.
      *
      * @var [CurrencyTypeModel]
      */
@@ -27,7 +43,7 @@ abstract class Process
     }
 
     /**
-     * 检测用户模型
+     * 检测用户模型.
      *
      * @param $user
      * @return UserModel
@@ -45,7 +61,7 @@ abstract class Process
     }
 
     /**
-     * 检测用户货币模型，防止后续操作出现错误
+     * 检测用户货币模型，防止后续操作出现错误.
      *
      * @param UserModel $user
      * @return UserModel
