a927ad60ce00d9eb8de885d05bcac342810f7d8c
Seven Du <shiweidu@outlook.com>
2018-01-17 09:43:45 +0800

    chore(Test Group Worker): Add auth middleware to entry.

--- packages/zhiyicx-test-group-worker/src/Web/Controllers/HomeController.php
+++ packages/zhiyicx-test-group-worker/src/Web/Controllers/HomeController.php
@@ -4,8 +4,26 @@ declare(strict_types=1);
 
 namespace Zhiyi\Plus\Packages\TestGroupWorker\Web\Controllers;
 
-class HomeController
+use Illuminate\Routing\Controller as BaseController;
+
+class HomeController extends BaseController
 {
+    /**
+     * Create the Controller instance.
+     *
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:web');
+    }
+
+    /**
+     * The test group worker entry.
+     *
+     * @return mixed
+     * @author Seven Du <shiweidu@outlook.com>
+     */
     public function index()
     {
         return view('test-group-worker::welcome');
