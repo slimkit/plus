7da8932541c0b350fc720259510019c40467fd04
Seven Du <shiweidu@outlook.com>
2018-01-18 10:18:03 +0800

    chore(Test Group Worker):  重写账号表

--- packages/zhiyicx-test-group-worker/database/migrations/2018_01_16_101950_create_test_group_worker_accesses_table.php
+++ packages/zhiyicx-test-group-worker/database/migrations/2018_01_16_101950_create_test_group_worker_accesses_table.php
@@ -14,11 +14,12 @@ class CreateTestGroupWorkerAccessesTable extends Migration
     public function up()
     {
         Schema::create('test_group_worker_accesses', function (Blueprint $table) {
-            $table->increments('id');
             $table->integer('owner')->unsgined()->comment('The Access be User');
-            $table->string('username')->comment('GitHub Access Username');
+            $table->string('login')->comment('GitHub Access Username');
             $table->string('password')->comment('GitHub Access Password');
             $table->timestamps();
+            $table->primary('owner');
+            $table->unique('login');
         });
     }
 
