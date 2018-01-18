ef52e2acb2ae3ecfcd2a2ca098eccbad4567b9ef
Seven Du <shiweidu@outlook.com>
2018-01-18 14:12:34 +0800

    chore(Test Group Worker): Add response use method

--- packages/zhiyicx-test-group-worker/resources/assets/api/axios.js
+++ packages/zhiyicx-test-group-worker/resources/assets/api/axios.js
@@ -1,12 +1,15 @@
 import axios from 'axios';
 
-// const api = axios.create({
-//   baseURL: window.apiBaseUrl,
-// });
-
-// api.headers['Authorization'] = 'Bearer '+window.accessToken;
-
 axios.defaults.baseURL = window.apiBaseUrl;
+
+axios.interceptors.response.use(function (response) {
+  return response;
+}, function (error) {
+  if (error.response && error.response.status === 401) {
+    window.location.reload();
+  }
+  return Promise.reject(error);
+});
 
 export default function() {
   axios.defaults.headers.common['Authorization'] = 'Bearer ' + window.accessToken;
