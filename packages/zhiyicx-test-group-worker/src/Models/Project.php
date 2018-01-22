70b601ace74b151dc17ffe19506342ec1b9d0138
Seven Du <shiweidu@outlook.com>
2018-01-22 11:13:11 +0800

    chore(Test Group Worker): Add project issues Relation

--- packages/zhiyicx-test-group-worker/src/Models/Project.php
+++ packages/zhiyicx-test-group-worker/src/Models/Project.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Zhiyi\Plus\Packages\TestGroupWorker\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Project extends Model
 {
@@ -14,4 +15,15 @@ class Project extends Model
      * @var string
      */
     protected $table = 'test_group_worker_projects';
+
+    /**
+     * the project issues.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function isseus(): HasMany
+    {
+        return $this->hasMany(Issue::class, 'project_id', 'id');
+    }
 }
