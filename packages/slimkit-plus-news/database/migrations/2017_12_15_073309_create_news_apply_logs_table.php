aebb5d4bd002490bb9bf949082e2c0aafd679d57
白石 <414606094@qq.com>
2018-01-23 15:12:02 +0800

    feat: 补充资讯部分缺失表迁移

---
+++ packages/slimkit-plus-news/database/migrations/2017_12_15_073309_create_news_apply_logs_table.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * +----------------------------------------------------------------------+
+ * |                          ThinkSNS Plus                               |
+ * +----------------------------------------------------------------------+
+ * | Copyright (c) 2017 Chengdu ZhiYiChuangXiang Technology Co., Ltd.     |
+ * +----------------------------------------------------------------------+
+ * | This source file is subject to version 2.0 of the Apache license,    |
+ * | that is bundled with this package in the file LICENSE, and is        |
+ * | available through the world-wide-web at the following url:           |
+ * | http://www.apache.org/licenses/LICENSE-2.0.html                      |
+ * +----------------------------------------------------------------------+
+ * | Author: Slim Kit Group <master@zhiyicx.com>                          |
+ * | Homepage: www.thinksns.com                                           |
+ * +----------------------------------------------------------------------+
+ */
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNewsApplyLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news_apply_logs', function (Blueprint $table) {
+            $table->integer('news_id')->unsigned()->comment('资讯id');
+            $table->integer('user_id')->unsigned()->comment('用户id');
+            $table->integer('status')->unsigned()->comment('审核状态，0-待审核 1-已处理 2-已驳回');
+            $table->integer('mark')->nullable()->comment('备注');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('news_apply_logs');
+    }
+}
