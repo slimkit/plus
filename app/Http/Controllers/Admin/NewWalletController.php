9b5b983cbcc7f6dbc4c159b7fafbc6e72d290337
Seven Du <shiweidu@outlook.com>
2018-01-31 06:49:35 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/Admin/NewWalletController.php
+++ app/Http/Controllers/Admin/NewWalletController.php
@@ -1,5 +1,21 @@
 <?php
 
+/*
+ * +----------------------------------------------------------------------+
+ * |                          ThinkSNS Plus                               |
+ * +----------------------------------------------------------------------+
+ * | Copyright (c) 2017 Chengdu ZhiYiChuangXiang Technology Co., Ltd.     |
+ * +----------------------------------------------------------------------+
+ * | This source file is subject to version 2.0 of the Apache license,    |
+ * | that is bundled with this package in the file LICENSE, and is        |
+ * | available through the world-wide-web at the following url:           |
+ * | http://www.apache.org/licenses/LICENSE-2.0.html                      |
+ * +----------------------------------------------------------------------+
+ * | Author: Slim Kit Group <master@zhiyicx.com>                          |
+ * | Homepage: www.thinksns.com                                           |
+ * +----------------------------------------------------------------------+
+ */
+
 namespace Zhiyi\Plus\Http\Controllers\Admin;
 
 use DB;
@@ -14,8 +30,8 @@ class NewWalletController extends Controller
      *
      * @return \Illuminate\Http\JsonResponse
      */
-	public function statistics()
-	{
+    public function statistics()
+    {
         $expenditure = WalletOrder::where('type', -1)->select(DB::raw('count(id) as count, sum(amount) as sum'))->first();
         $income = WalletOrder::where('type', 1)->select(DB::raw('count(id) as count, sum(amount) as sum'))->first();
 
@@ -23,7 +39,7 @@ class NewWalletController extends Controller
             'expenditure' => $expenditure,
             'income' => $income,
         ], 200);
-	}
+    }
 
     /**
      * 新版钱包流水.
@@ -31,26 +47,26 @@ class NewWalletController extends Controller
      * @param Request $request
      * @return \Illuminate\Http\JsonResponse
      */
-	public function waters(Request $request)
-	{
-	    $user = $request->query('user');
-	    $state = $request->query('state');
+    public function waters(Request $request)
+    {
+        $user = $request->query('user');
+        $state = $request->query('state');
 
-		$limit = (int) $request->query('limit', 15);
-		$offset = (int) $request->query('offset', 0);
+        $limit = (int) $request->query('limit', 15);
+        $offset = (int) $request->query('offset', 0);
 
-		$query = (new WalletOrder)->newQuery();
+        $query = (new WalletOrder)->newQuery();
 
-		$query->when($user, function ($query) use ($user) {
-		    return $query->where('owner_id', $user);
+        $query->when($user, function ($query) use ($user) {
+            return $query->where('owner_id', $user);
         })
         ->when($state, function ($query) use ($state) {
             return $query->where('state', $state);
         });
 
-		$count = $query->count();
-		$items = $query->limit($limit)->offset($offset)->latest()->get();
+        $count = $query->count();
+        $items = $query->limit($limit)->offset($offset)->latest()->get();
 
-		return response()->json($items, 200, ['x-total' => $count]);
-	}    
+        return response()->json($items, 200, ['x-total' => $count]);
+    }
 }
