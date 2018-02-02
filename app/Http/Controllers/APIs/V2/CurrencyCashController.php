dc9c575f3e263741a53abb9d365ce7822b7b9995
白石 <414606094@qq.com>
2018-02-02 09:50:44 +0800

    chore: ( zhiyicx/thinksns-plus-ios#1895 )修改提交积分提取申请的成功提示语

--- app/Http/Controllers/APIs/V2/CurrencyCashController.php
+++ app/Http/Controllers/APIs/V2/CurrencyCashController.php
@@ -40,7 +40,7 @@ class CurrencyCashController extends Controller
         $cash = new CashProcess();
 
         if (($result = $cash->createOrder($user->id, (int) $amount) !== false)) {
-            return response()->json(['message' => ['操作成功']], 201);
+            return response()->json(['message' => ['积分提取申请已提交，请等待审核']], 201);
         }
 
         return response()->json(['message' => ['操作失败']], 500);
