293f296bdee45dc91200ec6d6e2642f19c168891
Seven Du <shiweidu@outlook.com>
2018-02-05 02:28:48 +0000

    Apply fixes from StyleCI

--- tests/Feature/API2/AuthLoginTest.php
+++ tests/Feature/API2/AuthLoginTest.php
@@ -2,6 +2,22 @@
 
 declare(strict_types=1);
 
+/*
+ * +----------------------------------------------------------------------+
+ * |                          ThinkSNS Plus                               |
+ * +----------------------------------------------------------------------+
+ * | Copyright (c) 2017 Chengdu ZhiYiChuangXiang Technology Co., Ltd.     |
+ * +----------------------------------------------------------------------+
+ * | This source file is subject to version 2.0 of the Apache license,    |
+ * | that is bundled with this package in the file LICENSE, and is        |
+ * | available through the world-wide-web at the following url:           |
+ * | http://www.apache.org/licenses/LICENSE-2.0.html                      |
+ * +----------------------------------------------------------------------+
+ * | Author: Slim Kit Group <master@zhiyicx.com>                          |
+ * | Homepage: www.thinksns.com                                           |
+ * +----------------------------------------------------------------------+
+ */
+
 namespace Zhiyi\Plus\Tests\Feature\API2;
 
 use Zhiyi\Plus\Tests\TestCase;
@@ -31,7 +47,7 @@ class AuthLoginTest extends TestCase
     {
         $response = $this->json('POST', 'api/v2/auth/login', [
             'login' => $this->user->id,
-            'password' => 'secret'
+            'password' => 'secret',
         ]);
 
         $this->assertLoginResponse($response);
