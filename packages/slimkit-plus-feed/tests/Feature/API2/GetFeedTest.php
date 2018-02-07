ce4695313f76c6f456d73893ee1dbbf365af9639
huhao <915664508@qq.com>
2018-02-07 16:23:14 +0800

    test(API): 修改类名错误

--- packages/slimkit-plus-feed/tests/Feature/API2/GetFeedTest.php
+++ packages/slimkit-plus-feed/tests/Feature/API2/GetFeedTest.php
@@ -26,7 +26,7 @@ use Zhiyi\Plus\Models\User as UserModel;
 use Zhiyi\Plus\Models\Ability as AbilityModel;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class SendImageFeedTest extends TestCase
+class GetFeedTest extends TestCase
 {
     use DatabaseTransactions;
 
