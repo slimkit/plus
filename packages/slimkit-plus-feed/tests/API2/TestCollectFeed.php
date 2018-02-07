09a5aa1733212cec629bf69c1442565745cc07ce
huhao <915664508@qq.com>
2018-02-07 10:53:10 +0800

    test(API): 修改动态收藏接口测试

--- packages/slimkit-plus-feed/tests/API2/TestCollectFeed.php
+++ packages/slimkit-plus-feed/tests/API2/TestCollectFeed.php
@@ -22,10 +22,7 @@ namespace Zhiyi\Component\ZhiyiPlus\PlusComponentFeed\Tests\API2;
 
 use Zhiyi\Plus\Models\User;
 use Zhiyi\Plus\Tests\TestCase;
-use Illuminate\Contracts\Auth\Guard;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
-use Zhiyi\Component\ZhiyiPlus\PlusComponentFeed\Models\Feed;
 
 class TestCollectFeed extends TestCase
 {
@@ -37,6 +34,8 @@ class TestCollectFeed extends TestCase
 
     /**
      * 前置条件.
+     *
+     * @return void
      */
     public function setUp()
     {
@@ -45,32 +44,60 @@ class TestCollectFeed extends TestCase
         $this->user = factory(User::class)->create();
 
         $this->user->roles()->sync([2]);
+
+        $this->addTestFeedData($this->user);
     }
 
+    /**
+     * 获取收藏列表.
+     *
+     * @return void
+     */
+    public function testCollectFeedList()
+    {
+        $response = $this->actingAs($this->user, 'api')->get($this->api.'/collections');
+
+        $response
+            ->assertStatus(200)
+            ->assertJsonStructure([]);
+    }
+
+    /**
+     * 动态收藏.
+     *
+     * @return void
+     */
     public function testCollectFeed()
     {
-        $token = $this->guard()->login($this->user);
+        $response = $this->actingAs($this->user, 'api')
+                         ->post($this->api."/{$this->feed['id']}/collections");
 
-        $this->seedTestFeed($token);
-
-        // 动态收藏接口: POST /feeds/:feed/collections
-        $res = $this->post($this->api."/{$this->feed['id']}/collections?token=".$token);
-        $res->assertStatus(201)
-            ->assertJsonStructure(['message']);
-
-        // 动态收藏列表: GET /feeds/:feed/collections
-        $res = $this->get($this->api.'/collections?token='.$token)
-            ->assertStatus(200)
-            ->assertJsonStructure([
-                ['id', 'user_id', 'feed_content', 'feed_from', 'like_count', 'feed_view_count', 'feed_comment_count', 'feed_latitude', 'feed_longtitude', 'feed_geohash', 'audit_status', 'feed_mark', 'created_at', 'updated_at', 'deleted_at', 'paid_node', 'comments', 'has_collect', 'has_like', 'images', 'user'],
-            ]);
-
-        // 取消动态收藏: DELETE /feeds/:feed/uncollectD
-        $res = $this->delete($this->api."/{$this->feed['id']}/uncollect?token=".$token)
-            ->assertStatus(204);
+        $response->assertStatus(201)
+                 ->assertJsonStructure(['message']);
     }
 
-    protected function seedTestFeed($token)
+
+    /**
+     * 取消动态收藏.
+     *
+     * @return void
+     */
+    public function testUnCollectFeed()
+    {
+        $response = $this->actingAs($this->user, 'api')
+                         ->delete($this->api."/{$this->feed['id']}/uncollect");
+
+        $response->assertStatus(204);
+    }
+
+
+    /**
+     * 填充动态数据.
+     *
+     * @param $user
+     * @return void
+     */
+    protected function addTestFeedData($user)
     {
         $data = [
             'feed_content' => '单元测试动态数据',
@@ -83,16 +110,6 @@ class TestCollectFeed extends TestCase
             'images' => [],
         ];
 
-        $this->feed = $this->post($this->api.'?token='.$token, $data)->json();
-    }
-
-    /**
-     * Get the guard to be used during authentication.
-     *
-     * @return \Illuminate\Contracts\Auth\Guard
-     */
-    protected function guard(): Guard
-    {
-        return Auth::guard('api');
+        $this->feed = $this->actingAs($user, 'api')->post($this->api, $data)->json();
     }
 }
