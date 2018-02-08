6276df14f4f6ae0a46fc9eefb4722b35354fc96e
huhao <915664508@qq.com>
2018-02-08 10:57:59 +0800

    test(API): 添加旧版和新版接口测试

---
+++ packages/slimkit-plus-feed/tests/Feature/API2/NewRewardFeedTest.php
@@ -0,0 +1,101 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * +----------------------------------------------------------------------+
+ * |                          ThinkSNS Plus                               |
+ * +----------------------------------------------------------------------+
+ * | Copyright (c) 2017 Chengdu ZhiYiChuangXiang Technology Co., Ltd.     |
+ * +----------------------------------------------------------------------+
+ * | This source file is subject to version 2.0 of the Apache license,    |
+ * | that is bundled with this package in the file LICENSE, and is        |
+ * | available through the world-wide-web at the following url:           |
+ * | http://www.apache.org/licenses/LICENSE-2.0.html                      |
+ * +----------------------------------------------------------------------+
+ * | Author: Slim Kit Group <master@zhiyicx.com>                          |
+ * | Homepage: www.thinksns.com                                           |
+ * +----------------------------------------------------------------------+
+ */
+
+namespace SlimKit\PlusFeed\Tests\Feature\API2;
+
+use Zhiyi\Plus\Tests\TestCase;
+use Zhiyi\Plus\Models\Role as RoleModel;
+use Zhiyi\Plus\Models\User as UserModel;
+use Zhiyi\Plus\Models\Ability as AbilityModel;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class NewRewardFeedTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /**
+     * Create the test need user.
+     *
+     * @return \Zhiyi\Plus\Models\User
+     */
+    protected function createUser(): UserModel
+    {
+        $user = factory(UserModel::class)->create();
+        $user->newWallet()->firstOrCreate([
+            'balance' => 1000,
+            'total_income' => 0,
+            'total_expenses' => 0
+        ]);
+        $ability = AbilityModel::where('name', 'feed-post')->firstOr(function () {
+            return factory(AbilityModel::class)->create([
+                'name' => 'feed-post',
+            ]);
+        });
+        $role = RoleModel::where('name', 'test')->firstOr(function () {
+            return factory(RoleModel::class)->create([
+                'name' => 'test',
+            ]);
+        });
+        $role
+            ->abilities()
+            ->sync($ability);
+        $user->roles()->sync($role);
+
+        return $user;
+    }
+
+    /**
+     * 添加测试动态.
+     *
+     * @param $user
+     * @return mixed
+     */
+    protected function addFeed($user)
+    {
+        $response = $this->actingAs($user, 'api')
+            ->json('POST', '/api/v2/feeds', [
+                'feed_content' => 'test',
+                'feed_from' => 5,
+                'feed_mark' => intval(time().rand(1000, 9999)),
+            ])
+            ->decodeResponseJson();
+
+        return $response['id'];
+    }
+
+    /**
+     * 测试新版打赏接口
+     *
+     * @return mixed
+     */
+    public function testRewardFeed()
+    {
+        $owner = $this->createUser();
+        $other = $this->createUser();
+        $feed = $this->addFeed($owner);
+
+        $response = $this
+            ->actingAs($other, 'api')
+            ->json('POST', "/api/v2/feeds/{$feed}/new-rewards", ['amount' => 10]);
+        $response
+            ->assertStatus(201)
+            ->assertJsonStructure(['message']);
+    }
+}
