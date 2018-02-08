14ac7f97e3ea1b587c1b67654ff9184f574dc065
huhao <915664508@qq.com>
2018-02-08 16:05:04 +0800

    test(API): 新增积分置顶动态接口测试。

--- packages/slimkit-plus-feed/tests/Feature/API2/CurrencyPinnedFeedTest.php
+++ packages/slimkit-plus-feed/tests/Feature/API2/CurrencyPinnedFeedTest.php
@@ -25,7 +25,7 @@ use Zhiyi\Plus\Models\User as UserModel;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Zhiyi\Component\ZhiyiPlus\PlusComponentFeed\Models\Feed;
 
-class DeleteFeedTest extends TestCase
+class CurrencyPinnedFeedTest extends TestCase
 {
     use DatabaseTransactions;
 
@@ -45,58 +45,55 @@ class DeleteFeedTest extends TestCase
     }
 
     /**
-     * 测试删除自己动态
+     * 不传置顶天数和置顶金额.
      *
      * @return mixed
      */
-    public function testDeleteFeed()
+    public function testNonParamsCurrencyPinnedFeed()
     {
         $response = $this
             ->actingAs($this->user, 'api')
-            ->json('DELETE', '/api/v2/feeds/'.$this->feed->id);
+            ->json('POST', "/api/v2/feeds/{$this->feed->id}/currency-pinneds");
         $response
-            ->assertStatus(204);
+            ->assertStatus(422);
     }
 
     /**
-     * 测试删除他人动态.
+     * 余额不足.
      *
      * @return mixed
      */
-    public function testDeleteOtherFeed()
-    {
-        $response = $this
-            ->actingAs(factory(UserModel::class)->create(), 'api')
-            ->json('DELETE', '/api/v2/feeds/'.$this->feed->id);
-        $response
-            ->assertStatus(403);
-    }
-
-    /**
-     * 删除不存在的动态.
-     *
-     * @return mixed
-     */
-    public function testDeleteNonExistFeed()
+    public function testNotEnoughCurrencyPinnedFeed()
     {
         $response = $this
             ->actingAs($this->user, 'api')
-            ->json('DELETE', '/api/v2/feeds/0');
+            ->json('POST', "/api/v2/feeds/{$this->feed->id}/currency-pinneds", [
+                'amount' => 1000,
+                'day' => 10,
+            ]);
         $response
-            ->assertStatus(404);
+            ->assertStatus(422);
     }
 
     /**
-     * 新版删除动态 退还积分.
+     * 置顶动态.
      *
      * @return mixed
      */
-    public function testNewDeleteFeed()
+    public function testCurrencyPinnedFeed()
     {
+        $this->user->currency()->firstOrCreate([
+            'sum' => 1000,
+            'type' => 1
+        ]);
+
         $response = $this
             ->actingAs($this->user, 'api')
-            ->json('DELETE', "/api/v2/feeds/{$this->feed->id}/currency");
+            ->json('POST', "/api/v2/feeds/{$this->feed->id}/currency-pinneds", [
+                'amount' => 1000,
+                'day' => 10,
+            ]);
         $response
-            ->assertStatus(204);
+            ->assertStatus(201);
     }
 }
