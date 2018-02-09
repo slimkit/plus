3cb06b8c59b9733db68f5c79b75b11395a67ebe3
huhao <915664508@qq.com>
2018-02-09 17:16:43 +0800

    test(API): 添加咨询举报测试接口。

---
+++ packages/slimkit-plus-news/tests/Feature/API2/ReportNewsTest.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * +----------------------------------------------------------------------+
+ * |                          ThinkSNS Plus                               |
+ * +----------------------------------------------------------------------+
+ * | Copyright (c) 2017 Chengdu ZhiYiChuangXiang Technology Co., Ltd.     |
+ * +----------------------------------------------------------------------+
+ * | This source file is subject to version 2.0 of the Apache license,    |
+ * | that is bundled with this package in the file LICENSE, and is        |
+ * | available through the world-wide-web at the following url:           |
+ * | http://www.apache.org/licenses/LICENSE-2.0.html                      |
+ * +----------------------------------------------------------------------+
+ * | Author: Slim Kit Group <master@zhiyicx.com>                          |
+ * | Homepage: www.thinksns.com                                           |
+ * +----------------------------------------------------------------------+
+ */
+
+namespace Zhiyi\Component\ZhiyiPlus\PlusComponentNews\Feature\API2;
+
+use Zhiyi\Plus\Tests\TestCase;
+use Zhiyi\Plus\Models\User as UserModel;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Zhiyi\Component\ZhiyiPlus\PlusComponentNews\Models\News as NewsModel;
+use Zhiyi\Component\ZhiyiPlus\PlusComponentNews\Models\NewsCate as NewsCateModel;
+
+class ReportNewsTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    protected $user;
+
+    protected $cate;
+
+    protected $news;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->user = factory(UserModel::class)->create();
+        $this->cate = factory(NewsCateModel::class)->create();
+        $this->news = factory(NewsModel::class)->create([
+            'title' => 'test',
+            'user_id' => $this->user->id,
+            'cate_id' => $this->cate->id,
+            'audit_status' => 0,
+        ]);
+    }
+
+    /**
+     * 举报资讯.
+     *
+     * @return mixed
+     */
+    public function testReportNews()
+    {
+        $response = $this
+            ->actingAs($this->user, 'api')
+            ->json('POST', "/api/v2/news/{$this->news->id}/reports", [
+                'reason' => 'test report',
+            ]);
+        $response
+            ->assertStatus(201)
+            ->assertJsonStructure(['message']);
+    }
+}
