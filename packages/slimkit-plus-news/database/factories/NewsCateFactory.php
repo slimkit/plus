c6f64adac477a1851110543c879a14f3cb94ec4c
huhao <915664508@qq.com>
2018-02-09 11:58:24 +0800

    test(API): 修改发布咨询接口

--- packages/slimkit-plus-news/database/factories/NewsCateFactory.php
+++ packages/slimkit-plus-news/database/factories/NewsCateFactory.php
@@ -20,7 +20,7 @@ use Faker\Generator as Faker;
 
 $factory->define(\Zhiyi\Component\ZhiyiPlus\PlusComponentNews\Models\NewsCate::class, function (Faker $faker) {
     return [
-        'name' => $faker->shuffle(),
+        'name' => $faker->firstName,
         'rank' => $faker->biasedNumberBetween(100, 999),
     ];
 });
