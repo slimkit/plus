0dcb5feee8379ccfb92405dcbcf7004da63546d8
boxshadow <idafoo@sina.com>
2018-02-27 00:26:03 +0800

    chore: 添加注册用户默认用户组

---
+++ database/seeds/CommonConfigSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * +----------------------------------------------------------------------+
+ * |                          ThinkSNS Plus                               |
+ * +----------------------------------------------------------------------+
+ * | Copyright (c) 2017 Chengdu ZhiYiChuangXiang Technology Co., Ltd.     |
+ * +----------------------------------------------------------------------+
+ * | This source file is subject to version 2.0 of the Apache license,    |
+ * | that is bundled with this package in the file LICENSE, and is        |
+ * | available through the world-wide-web at the following url:           |
+ * | http://www.apache.org/licenses/LICENSE-2.0.html                      |
+ * +----------------------------------------------------------------------+
+ * | Author: Slim Kit Group <master@zhiyicx.com>                          |
+ * | Homepage: www.thinksns.com                                           |
+ * +----------------------------------------------------------------------+
+ */
+
+use Illuminate\Database\Seeder;
+use Zhiy\Plus\Models\CommonConfig;
+
+class CommonConfigSeeder extends Seeder
+{
+    /**
+     * 添加注册用户的默认用户组
+     *
+     * @return void
+     */
+    public function run()
+    {
+        CommonConfig::create([
+            'name' => 'default_role',
+            'namespace' => 'user',
+            'value' => 2
+        ]);
+    }
+}
