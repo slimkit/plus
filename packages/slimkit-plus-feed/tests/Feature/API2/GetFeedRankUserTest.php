f4a1a3a1587559ca8fa61fe434e2fa6ac7aad7ec
Seven Du <shiweidu@outlook.com>
2018-03-07 02:26:17 +0000

    Apply fixes from StyleCI

--- packages/slimkit-plus-feed/tests/Feature/API2/GetFeedRankUserTest.php
+++ packages/slimkit-plus-feed/tests/Feature/API2/GetFeedRankUserTest.php
@@ -21,7 +21,6 @@ declare(strict_types=1);
 namespace SlimKit\PlusFeed\Tests\Feature\API2;
 
 use Zhiyi\Plus\Tests\TestCase;
-use Zhiyi\Plus\Models\User as UserModel;
 
 class GetFeedRankUserTest extends TestCase
 {
