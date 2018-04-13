68a81756eac3b5a9ba90fc66632db500711a780b
boxshadow <idafoo@sina.com>
2018-04-13 16:34:47 +0800

    fix: 修正阿里存储endpoint导致的错误url, 以及模糊图片阀值

--- app/Cdn/Adapter/AliOss.php
+++ app/Cdn/Adapter/AliOss.php
@@ -81,6 +81,7 @@ class AliOss implements FileUrlGeneratorContract
      */
     public function url(string $filename, array $extra = []):string
     {
+        dd($extra);
         // 过滤目录分隔符.
         $filename = $this->filterSlash($filename);
 
@@ -184,7 +185,7 @@ class AliOss implements FileUrlGeneratorContract
                 ],
                 'blur,r_50,s_%d' => [
                     'confirm' => (bool) $blur,
-                    'params' => [$blur],
+                    'params' => [intval($blur / 2)],
                 ],
             ])->map(function ($value, $key) {
                 if (! $value['confirm']) {
@@ -298,7 +299,7 @@ class AliOss implements FileUrlGeneratorContract
      */
     protected function getBaseURI(string $endpoint = ''): string
     {
-        $endpoint = $endpoint ?: $this->endpoint;
+        $endpoint = $this->isCname ? $this->endpoint : $this->bucket . '.' . $this->endpoint;
         // 去除协议头，保留 hostname 部分。
         if (strpos($endpoint, 'http://') === 0) {
             $endpoint = substr($endpoint, strlen('http://'));
