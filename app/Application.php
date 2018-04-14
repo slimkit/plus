e0f5365695017f741d3273e61a7fd80df17bd063
Seven Du <shiweidu@outlook.com>
2018-04-14 10:42:09 +0800

    chore: Build 1.7.4 version

--- app/Application.php
+++ app/Application.php
@@ -30,7 +30,7 @@ class Application extends LaravelApplication
      *
      * @var string
      */
-    const VERSION = '1.7.3';
+    const VERSION = '1.7.4';
 
     /**
      * The core vendor YAML file.
