52bc4567dd06e29641bbd8cf81157cfe2ca88238
boxshadow <idafoo@sina.com>
2018-04-18 15:46:05 +0800

    chore: 修改UserCount模型,自定义主键造成更新用户所有未读数的bug

--- app/Models/UserCount.php
+++ app/Models/UserCount.php
@@ -22,17 +22,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserCount extends Model
 {
-    /**
-     * The primary key for the model.
-     *
-     * @var string
-     */
-    protected $primaryKey = 'user_id';
-
-    /**
-     * Indicates if the IDs are auto-incrementing.
-     *
-     * @var bool
-     */
-    public $incrementing = false;
+    protected $guarded = ['total'];
 }
