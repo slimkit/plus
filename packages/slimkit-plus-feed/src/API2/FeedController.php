97dddef10478c6c7462efdd95f715b980d835ac4
boxshadow <idafoo@sina.com>
2018-05-07 17:28:22 +0800

    Merge branch 'master' into 'plus'

--- packages/slimkit-plus-feed/src/API2/FeedController.php
+++ packages/slimkit-plus-feed/src/API2/FeedController.php
@@ -89,7 +89,9 @@ class FeedController extends Controller
             ->get();
 
         $user = $request->user('api')->id ?? 0;
-
+        $ids = $feeds->pluck('id');
+        $feedModel->whereIn('id', $ids)->increment('feed_view_count');
+        
         return $feedModel->getConnection()->transaction(function () use ($feeds, $repository, $user) {
             return $feeds->map(function (FeedModel $feed) use ($repository, $user) {
                 $repository->setModel($feed);
@@ -120,7 +122,7 @@ class FeedController extends Controller
         $after = $request->query('after');
         $user = $request->user('api')->id ?? 0;
         $search = $request->query('search');
-
+        
         $feeds = $feedModel->when($after, function ($query) use ($after) {
             return $query->where('id', '<', $after);
         })
@@ -142,6 +144,9 @@ class FeedController extends Controller
         ->limit($limit)
         ->get();
 
+        $ids = $feeds->pluck('id');
+        $feedModel->whereIn('id', $ids)->increment('feed_view_count');
+
         return $feedModel->getConnection()->transaction(function () use ($feeds, $repository, $user) {
             return $feeds->map(function (FeedModel $feed) use ($repository, $user) {
                 $repository->setModel($feed);
@@ -151,7 +156,7 @@ class FeedController extends Controller
 
                 $feed->has_collect = $feed->collected($user);
                 $feed->has_like = $feed->liked($user);
-
+                
                 return $feed;
             });
         });
@@ -202,12 +207,13 @@ class FeedController extends Controller
             ->orderBy('id', 'desc')
             ->get();
 
-        return $model->getConnection()->transaction(function () use ($feeds, $repository, $user) {
+        $feedModel->whereIn('id', $ids)->increment('feed_view_count');
+
+        return $model->getConnection()->transaction(function () use ($feeds, $repository, $user, $ids, $feedModel) {
             return $feeds->map(function ($feed) use ($repository, $user) {
                 if (! $feed) {
                     return null;
                 }
-
                 $repository->setModel($feed);
                 $repository->images();
                 $repository->format($user);
@@ -265,8 +271,11 @@ class FeedController extends Controller
         ->orderBy('feeds.id', 'desc')
         ->limit($limit)
         ->get();
+        $ids = $feeds->pluck('id');
+        $feedModel->whereIn('id', $ids)->increment('feed_view_count');
 
         return $model->getConnection()->transaction(function () use ($repository, $user, $feeds) {
+            
             return $feeds->map(function (FeedModel $feed) use ($repository, $user) {
                 $repository->setModel($feed);
                 $repository->images();
