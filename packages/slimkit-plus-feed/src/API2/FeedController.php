97c15f19c9c46c98afc9af5767c8579aa64693f6
boxshadow <idafoo@sina.com>
2018-05-07 17:23:11 +0800

    feat: 列表查询后, 每条增加浏览量

--- packages/slimkit-plus-feed/src/API2/FeedController.php
+++ packages/slimkit-plus-feed/src/API2/FeedController.php
@@ -84,7 +84,9 @@ class FeedController extends Controller
             ->get();
 
         $user = $request->user('api')->id ?? 0;
-
+        $ids = $feeds->pluck('id');
+        $feedModel->whereIn('id', $ids)->increment('feed_view_count');
+        
         return $feedModel->getConnection()->transaction(function () use ($feeds, $repository, $user) {
             return $feeds->map(function (FeedModel $feed) use ($repository, $user) {
                 $repository->setModel($feed);
@@ -115,7 +117,7 @@ class FeedController extends Controller
         $after = $request->query('after');
         $user = $request->user('api')->id ?? 0;
         $search = $request->query('search');
-
+        
         $feeds = $feedModel->when($after, function ($query) use ($after) {
             return $query->where('id', '<', $after);
         })->when(isset($search), function ($query) use ($search) {
@@ -131,6 +133,9 @@ class FeedController extends Controller
         ->limit($limit)
         ->get();
 
+        $ids = $feeds->pluck('id');
+        $feedModel->whereIn('id', $ids)->increment('feed_view_count');
+
         return $feedModel->getConnection()->transaction(function () use ($feeds, $repository, $user) {
             return $feeds->map(function (FeedModel $feed) use ($repository, $user) {
                 $repository->setModel($feed);
@@ -140,7 +145,7 @@ class FeedController extends Controller
 
                 $feed->has_collect = $feed->collected($user);
                 $feed->has_like = $feed->liked($user);
-
+                
                 return $feed;
             });
         });
@@ -186,12 +191,13 @@ class FeedController extends Controller
             ->orderBy('id', 'desc')
             ->get();
 
-        return $model->getConnection()->transaction(function () use ($feeds, $repository, $user) {
+        $feedModel->whereIn('id', $ids)->increment('feed_view_count');
+
+        return $model->getConnection()->transaction(function () use ($feeds, $repository, $user, $ids, $feedModel) {
             return $feeds->map(function ($feed) use ($repository, $user) {
                 if (! $feed) {
                     return null;
                 }
-
                 $repository->setModel($feed);
                 $repository->images();
                 $repository->format($user);
@@ -244,8 +250,11 @@ class FeedController extends Controller
         ->orderBy('feeds.id', 'desc')
         ->limit($limit)
         ->get();
+        $ids = $feeds->pluck('id');
+        $feedModel->whereIn('id', $ids)->increment('feed_view_count');
 
         return $model->getConnection()->transaction(function () use ($repository, $user, $feeds) {
+            
             return $feeds->map(function (FeedModel $feed) use ($repository, $user) {
                 $repository->setModel($feed);
                 $repository->images();
@@ -465,10 +474,11 @@ class FeedController extends Controller
      * @return mixed
      * @author Seven Du <shiweidu@outlook.com>
      */
-    public function destroy(Request $request,
-                            ResponseContract $response,
-                            FeedModel $feed)
-    {
+    public function destroy(
+        Request $request,
+        ResponseContract $response,
+        FeedModel $feed
+    ) {
         $user = $request->user();
 
         if ($user->id !== $feed->user_id) {
@@ -508,10 +518,11 @@ class FeedController extends Controller
      * @return mixed
      * @author BS <414606094@qq.com>
      */
-    public function newDestroy(Request $request,
-                            ResponseContract $response,
-                            FeedModel $feed)
-    {
+    public function newDestroy(
+        Request $request,
+        ResponseContract $response,
+        FeedModel $feed
+    ) {
         $user = $request->user();
 
         if ($user->id !== $feed->user_id) {
@@ -520,7 +531,6 @@ class FeedController extends Controller
 
         $feed->getConnection()->transaction(function () use ($feed, $user) {
             if ($pinned = $feed->pinned()->where('user_id', $user->id)->where('expires_at', null)->first()) { // 存在未审核的置顶申请时退款
-
                 $process = new UserProcess();
                 $process->reject(0, $pinned->amount, $user->id, '动态申请置顶退款', sprintf('退还申请置顶动态《%s》的款项', str_limit($feed->feed_content, 100)));
             }
