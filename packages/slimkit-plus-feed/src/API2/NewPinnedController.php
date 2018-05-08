8ec664f30d4f4f2b15b0060a90a1e5e66dd089a5
Seven Du <shiweidu@outlook.com>
2018-05-08 02:33:54 +0000

    Apply fixes from StyleCI

--- packages/slimkit-plus-feed/src/API2/NewPinnedController.php
+++ packages/slimkit-plus-feed/src/API2/NewPinnedController.php
@@ -74,7 +74,7 @@ class NewPinnedController extends Controller
                     $dateTime = new Carbon();
                     $pinned->expires_at = $dateTime->addDay($pinned->day);
                     $message = '置顶成功';
-                    $order = !$pinned->amount
+                    $order = ! $pinned->amount
                         ? true
                         : $process->prepayment($user->id, $pinned->amount, $feed->user_id, '申请动态评论置顶', sprintf('申请评论《%s》置顶', $comment->body));
                 } else {
