1f61114bc2c447c0336be035821346d057fd0c02
boxshadow <idafoo@sina.com>
2018-05-13 14:54:00 +0800

    chore: 更细手机号验证规则

--- app/helpers.php
+++ app/helpers.php
@@ -28,7 +28,7 @@ namespace Zhiyi\Plus;
  */
 function validateChinaPhoneNumber(string $number): bool
 {
-    return (bool) preg_match('/^(\+?0?86\-?)?((13\d|14[57]|15[^4,\D]|17[3678]|18\d)\d{8}|170[059]\d{7})$/', $number);
+    return (bool) preg_match('/^(\+?0?86\-?)?1[3-9]\d{9}$/', $number);
 }
 
 /**
