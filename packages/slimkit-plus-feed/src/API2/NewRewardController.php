c438b413cfe9098feaa53233834da8e72bd07e46
boxshadow <idafoo@sina.com>
2018-05-25 17:36:07 +0800

    chore: 修改打赏
    打赏由余额转为使用积分进行打赏

--- packages/slimkit-plus-feed/src/API2/NewRewardController.php
+++ packages/slimkit-plus-feed/src/API2/NewRewardController.php
@@ -21,11 +21,12 @@ declare(strict_types=1);
 namespace Zhiyi\Component\ZhiyiPlus\PlusComponentFeed\API2;
 
 use Illuminate\Http\Request;
-use Zhiyi\Plus\Packages\Wallet\Order;
+use Zhiyi\Plus\Models\GoldType;
+use Zhiyi\Plus\Models\CommonConfig;
 use Zhiyi\Plus\Http\Controllers\Controller;
-use Zhiyi\Plus\Packages\Wallet\TypeManager;
 use Zhiyi\Plus\Models\UserCount as UserCountModel;
 use Zhiyi\Component\ZhiyiPlus\PlusComponentFeed\Models\Feed;
+use Zhiyi\Plus\Packages\Currency\Processes\User as UserProcess;
 
 class NewRewardController extends Controller
 {
@@ -38,12 +39,14 @@ class NewRewardController extends Controller
      * @param  WalletCharge $charge
      * @return mix
      */
-    public function reward(Request $request, Feed $feed, TypeManager $manager)
+    public function reward(Request $request, Feed $feed, UserProcess $process, GoldType $goldModel, CommonConfig $configModel)
     {
+        $goldName = $goldModel->where('status', 1)->select('name', 'unit')->value('name') ?? '积分';
+        $this->goldName = $goldModel->where('status', 1)->select('name', 'unit')->value('name') ?? '积分';
         $amount = (int) $request->input('amount');
         if (! $amount || $amount < 0) {
             return response()->json([
-                'amount' => '请输入正确的打赏金额',
+                'amount' => '请输入正确的'.$goldName.'数量',
             ], 422);
         }
 
@@ -54,34 +57,22 @@ class NewRewardController extends Controller
             return response()->json(['message' => '不能打赏自己的发布的动态'], 422);
         }
 
-        if (! $user->newWallet || $user->newWallet->balance < $amount) {
+        if (! $user->currency || $user->currency->sum < $amount) {
             return response()->json([
-                'message' => '余额不足',
+                'message' => $goldName.'不足',
             ], 403);
         }
 
         $feedTitle = str_limit($feed->feed_content, 100, '...');
         $feedTitle = ($feedTitle ? "“${feedTitle}”" : '');
-        $money = $amount / 100;
 
-        // 记录订单
-        $status = $manager->driver(Order::TARGET_TYPE_REWARD)->reward([
-            'reward_resource' => $feed,
-            'order' => [
-                'user' => $user,
-                'target' => $target,
-                'amount' => $amount,
-                'user_order_body' => sprintf('打赏“%s”的动态%s，钱包扣除%s元', $target->name, $feedTitle, $money),
-                'target_order_body' => sprintf('“%s”打赏了你的动态%s，钱包增加%s元', $user->name, $feedTitle, $money),
-            ],
-            'notice' => [
-                'type' => 'feed:reward',
-                'detail' => ['feed' => $feed, 'user' => $user],
-                'message' => sprintf('“%s”打赏了你的动态%s%s元', $user->name, $feedTitle, $money),
-            ],
-        ]);
-
-        if ($status === true) {
+        $pay = $process->prepayment($user->id, $amount, $target->id, sprintf('打赏“%s”的动态', $target->name, $feedTitle, $amount), sprintf('打赏“%s”的动态，%s扣除%s', $target->name, $goldName, $amount));
+        $paid = $process->receivables($target->id, $amount, $user->id, sprintf('“%s”打赏了你的动态', $user->name), sprintf('“%s”打赏了你的动态，%s增加%s', $user->name, $goldName, $amount));
+        if ($pay && $paid) {
+            $target->sendNotifyMessage('feed:reward', sprintf('“%s”打赏了你的动态', $target->name), [
+                'feed' => $feed,
+                'user' => $user
+            ]);
             // 增加被打赏未读数
             $userCount = UserCountModel::firstOrNew([
                 'user_id' => $target->id,
