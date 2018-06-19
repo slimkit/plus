7beef1fef1aa33ef003eb11d029e5db102e9ed6f
boxshadow <idafoo@sina.com>
2018-06-19 15:15:16 +0800

    fix: (issue zhiyicx/thinksns-plus-android#2119) 写入错误时，记录错误，并返回简洁易懂的提示语句

--- packages/zhiyicx-plus-question/src/API2/Controllers/TopicApplicationController.php
+++ packages/zhiyicx-plus-question/src/API2/Controllers/TopicApplicationController.php
@@ -26,9 +26,9 @@ class TopicApplicationController extends Controller
 {
     /**
      * @author bs<414606094@qq.com>
-     * @param  SlimKit\PlusQuestion\API2\Requests\TopicApplication $request
-     * @param  SlimKit\PlusQuestion\Models\TopicApplication $topicApplicationModel
-     * @param  SlimKit\PlusQuestion\Models\Topic $topicModel
+     * @param TopicApplicationRequest $request
+     * @param TopicApplicationModel   $topicApplicationModel
+     * @param TopicModel              $topicModel
      * @return mixed
      */
     public function store(TopicApplicationRequest $request, TopicApplicationModel $topicApplicationModel, TopicModel $topicModel)
@@ -37,16 +37,21 @@ class TopicApplicationController extends Controller
         $name = $request->input('name');
         $description = $request->input('description');
         if ($topicModel->where('name', '=', $name)->first() || $topicApplicationModel->where('name', '=', $name)->first()) {
-            return response()->json(['message' => [trans('plus-question::topics.applied')]], 422);
+            return response()->json(['message' => trans('plus-question::topics.applied')], 422);
         }
 
         $topicApplicationModel->user_id = $user->id;
         $topicApplicationModel->name = $name;
         $topicApplicationModel->description = $description;
-        $topicApplicationModel->save();
+        try {
+            $topicApplicationModel->save();
+        } catch (\Exception $e) {
+            \Log::debug($e);
+            return response()->json(['message' => '申请创建话题失败'], 500);
+        }
 
         return response()->json([
-            'message' => [trans('plus-question::messages.success')],
+            'message' => trans('plus-question::messages.success'),
         ], 201);
     }
 }
