60847494263aaa616595434d441028d08247ea2d
Seven Du <shiweidu@outlook.com>
2018-07-20 07:15:04 +0000

    Apply fixes from StyleCI

--- scripts/circle-ci-settings.php
+++ scripts/circle-ci-settings.php
@@ -3,16 +3,15 @@
 
 declare(strict_types=1);
 
-
 $env = [
-	'APP_ENV' => 'testing',
-	'APP_DEBUG' => true,
-	'DB_CONNECTION' => 'mysql',
-	'DB_HOST' => '127.0.0.1',
-	'DB_PORT' => 3306,
-	'DB_USERNAME' => 'root',
-	'DB_PASSWORD' => null,
-	'DB_DATABASE' => 'plus',
+    'APP_ENV' => 'testing',
+    'APP_DEBUG' => true,
+    'DB_CONNECTION' => 'mysql',
+    'DB_HOST' => '127.0.0.1',
+    'DB_PORT' => 3306,
+    'DB_USERNAME' => 'root',
+    'DB_PASSWORD' => null,
+    'DB_DATABASE' => 'plus',
 ];
 
 $basePath = dirname(__DIR__);
