4e1726404a6aecb27439454c012f76dd55b4dce9
Seven Du <shiweidu@outlook.com>
2018-08-02 17:19:46 +0800

    chore(feed): 完成基本布局

--- packages/slimkit-plus-feed/admin/pages/topic/List.styles.js
+++ packages/slimkit-plus-feed/admin/pages/topic/List.styles.js
@@ -2,10 +2,18 @@ export default theme => ({
   root: {
     width: '100%',
   },
-  headerBarButton: {
-    marginRight: theme.spacing.unit,
-    '&:last-child': {
-      marginRight: 0
+  baseMargin: {
+    margin: theme.spacing.unit,
+  },
+  toolbar: {
+    paddingTop: theme.spacing.unit * 3,
+    marginBottom: theme.spacing.unit * 2
+  },
+  formControl: {
+    margin: `0 ${theme.spacing.unit}px`,
+    minWidth: 120,
+    '&:first-child': {
+      marginLeft: 0
     }
   }
 });
--- packages/slimkit-plus-feed/admin/pages/topic/List.view.jsx
+++ packages/slimkit-plus-feed/admin/pages/topic/List.view.jsx
@@ -3,9 +3,17 @@ import PropTypes from 'prop-types';
 import withStyles from '@material-ui/core/styles/withStyles';
 import Paper from '@material-ui/core/Paper';
 import Toolbar from '@material-ui/core/Toolbar';
-import Input from '@material-ui/core/Input';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Divider from '@material-ui/core/Divider';
 import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 import HeaderBar from './modules/ListContentHeaderBar';
 import styles from './List.styles';
 
@@ -14,18 +22,64 @@ class ListView extends React.Component {
       classes: PropTypes.object.isRequired
     }
 
+    state = {
+      name: '',
+      hot: 0
+    }
+
+    handleChangeInput = event => {
+      this.setState({ [event.target.name]: event.target.value });
+    }
+
+    handleResetSearch = () => {
+      this.setState({
+        name: '',
+        hot: 0
+      });
+    }
+
     render() {
       let { classes } = this.props;
       return (
         <div>
           <HeaderBar />
           <Paper className={classes.root}>
-            <Toolbar>
-              <FormControl>
-                <InputLabel htmlFor="name-simple">名称</InputLabel>
-                <Input />
+            <Toolbar className={classes.toolbar}>
+              <TextField
+                name="name"
+                value={this.state.name}
+                className={classes.formControl}
+                label="名称"
+                placeholder="输入名称关键词..."
+                onChange={this.handleChangeInput}
+              />
+              <FormControl className={classes.formControl}>
+                <InputLabel>热门</InputLabel>
+                <Select
+                  value={this.state.hot}
+                  onChange={this.handleChangeInput}
+                  name="hot"
+                >
+                  <MenuItem value={0}>否</MenuItem>
+                  <MenuItem value={1}>是</MenuItem>
+                </Select>
               </FormControl>
+              <Button className={classes.baseMargin} variant="contained" color="primary">搜&nbsp;索</Button>
+              <Button className={classes.baseMargin} variant="contained" onClick={this.handleResetSearch}>重&nbsp;置</Button>
             </Toolbar>
+            <Divider />
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>ID</TableCell>
+                  <TableCell>名称</TableCell>
+                  <TableCell>动态数</TableCell>
+                  <TableCell>关注数</TableCell>
+                  <TableCell>创建者</TableCell>
+                  <TableCell>操作</TableCell>
+                </TableRow>
+              </TableHead>
+            </Table>
           </Paper>
         </div>
       );
