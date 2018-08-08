639c044c8575ea0816d85053804391c7165cd4d9
Seven Du <shiweidu@outlook.com>
2018-08-08 17:33:31 +0800

    chore: Change version to 2.0.0

--- app/Application.php
+++ app/Application.php
@@ -29,7 +29,7 @@ class Application extends LaravelApplication
      *
      * @var string
      */
-    const VERSION = '1.9.0';
+    const VERSION = '2.0.0';
 
     /**
      * Create a new Illuminate application instance.
