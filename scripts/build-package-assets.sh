c70306a1d13328ff15c4cabefc89079c4e765482
boxshadow <idafoo@sina.com>
2018-08-08 16:54:15 +0800

    chore: 发布1.9.0版本

--- scripts/build-package-assets.sh
+++ scripts/build-package-assets.sh
@@ -17,3 +17,15 @@ cd packages/slimkit-plus-news/
 yarn
 yarn dist
 cd -
+
+# build groups
+cd packages/zhiyicx-plus-group/
+yarn
+yarn prod
+cd -
+
+# build questions
+cd packages/zhiyicx-plus-question/
+yarn
+yarn prod
+cd -
