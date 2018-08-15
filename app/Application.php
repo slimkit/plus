c4408f2f15f00247e0abc9160408a8073e26da77
Seven Du <shiweidu@outlook.com>
2018-08-15 11:54:33 +0800

    chore: publish 1.8.3 version

--- app/Application.php
+++ app/Application.php
@@ -30,7 +30,7 @@ class Application extends LaravelApplication
      *
      * @var string
      */
-    const VERSION = '1.8.2';
+    const VERSION = '1.8.3';
 
     /**
      * The core vendor YAML file.
