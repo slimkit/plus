ac53ed34caa3127a296dd122011d01553d196f3c
Seven Du <shiweidu@outlook.com>
2018-09-04 10:32:42 +0000

    Apply fixes from StyleCI

--- app/Admin/Controllers/FileStorage/ImageDimension.php
+++ app/Admin/Controllers/FileStorage/ImageDimension.php
@@ -2,6 +2,22 @@
 
 declare(strict_types=1);
 
+/*
+ * +----------------------------------------------------------------------+
+ * |                          ThinkSNS Plus                               |
+ * +----------------------------------------------------------------------+
+ * | Copyright (c) 2018 Chengdu ZhiYiChuangXiang Technology Co., Ltd.     |
+ * +----------------------------------------------------------------------+
+ * | This source file is subject to version 2.0 of the Apache license,    |
+ * | that is bundled with this package in the file LICENSE, and is        |
+ * | available through the world-wide-web at the following url:           |
+ * | http://www.apache.org/licenses/LICENSE-2.0.html                      |
+ * +----------------------------------------------------------------------+
+ * | Author: Slim Kit Group <master@zhiyicx.com>                          |
+ * | Homepage: www.thinksns.com                                           |
+ * +----------------------------------------------------------------------+
+ */
+
 namespace Zhiyi\Plus\Admin\Controllers\FileStorage;
 
 use Illuminate\Http\Request;
@@ -34,7 +50,7 @@ class ImageDimension
             'height' => [
                 'min' => intval($configure['image-min-height'] ?? 0),
                 'max' => intval($configure['image-max-height'] ?? 0),
-            ]
+            ],
         ];
 
         return new JsonResponse($result, JsonResponse::HTTP_OK);
