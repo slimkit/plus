972ae9a472e0f19893e77cda31c2081a36dc18ee
Seven Du <shiweidu@outlook.com>
2018-09-04 05:59:56 +0000

    Apply fixes from StyleCI

--- app/FileStorage/Untitled-1.php
+++ app/FileStorage/Untitled-1.php
@@ -1,10 +1,26 @@
 <?php
 
+/*
+ * +----------------------------------------------------------------------+
+ * |                          ThinkSNS Plus                               |
+ * +----------------------------------------------------------------------+
+ * | Copyright (c) 2018 Chengdu ZhiYiChuangXiang Technology Co., Ltd.     |
+ * +----------------------------------------------------------------------+
+ * | This source file is subject to version 2.0 of the Apache license,    |
+ * | that is bundled with this package in the file LICENSE, and is        |
+ * | available through the world-wide-web at the following url:           |
+ * | http://www.apache.org/licenses/LICENSE-2.0.html                      |
+ * +----------------------------------------------------------------------+
+ * | Author: Slim Kit Group <master@zhiyicx.com>                          |
+ * | Homepage: www.thinksns.com                                           |
+ * +----------------------------------------------------------------------+
+ */
+
 return [
     'default-filsystem' => 'local',
     'filesystems' => [
         'local' => [
-            'disk' => 'local'
+            'disk' => 'local',
         ],
         'aliyun-oss' => [
             'bucket' => null,
@@ -17,13 +33,13 @@ return [
     ],
     'channels' => [
         'public' => [
-            'filesystem' => ''
+            'filesystem' => '',
         ],
         'protected' => [
-            'filesystem' => ''
+            'filesystem' => '',
         ],
         'private' => [
-            'filesystem' => ''
+            'filesystem' => '',
         ],
-    ]
+    ],
 ];
