ced9535087c680b53a174f3a3fab43a0f346a711
Seven Du <shiweidu@outlook.com>
2018-09-06 11:31:17 +0800

    fix(feed): 修复今日动态排行时间错误，修改为零点为起点
    zhiyicx/thinksns-plus-android#2282

--- packages/slimkit-plus-feed/src/API2/RankController.php
+++ packages/slimkit-plus-feed/src/API2/RankController.php
@@ -48,7 +48,7 @@ class RankController extends Controller
 
         switch ($type) {
             case 'day':
-                $date = $datetime->subDay();
+                $date = new Carbon($datetime->format('Y-m-d 00:00:00'));
                 break;
             case 'week':
                 $date = $datetime->subWeek();
@@ -62,18 +62,20 @@ class RankController extends Controller
         }
 
         $feeds = $feedModel->select('user_id', DB::raw('sum(like_count) as count'))
-        ->where('feeds.created_at', '>', $date)
-        ->where('audit_status', 1)
-        ->with(['user' => function ($query) {
-            return $query->select('id', 'name', 'sex')
-                ->withTrashed();
-        }, 'user.extra'])
-        ->groupBy('user_id')
-        ->orderBy('count', 'desc')
-        ->orderBy('user_id', 'asc')
-        ->offset($offset)
-        ->take($limit)
-        ->get();
+            ->where('created_at', '>', $date)
+            ->where('audit_status', 1)
+            ->with([
+                'user' => function ($query) {
+                    return $query->select('id', 'name', 'sex')->withTrashed();
+                },
+                'user.extra'
+            ])
+            ->groupBy('user_id')
+            ->orderBy('count', 'desc')
+            ->orderBy('user_id', 'asc')
+            ->offset($offset)
+            ->take($limit)
+            ->get();
 
         return $response->json($feedModel->getConnection()->transaction(function () use ($feeds, $user, $date, $feedModel, $offset) {
             return $feeds->map(function ($feed, $key) use ($user, $offset) {
