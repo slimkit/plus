73171d2d453ea1d62dcfb904b86c830137668040
Seven Du <shiweidu@outlook.com>
2018-09-07 03:40:21 +0000

    Apply fixes from StyleCI

--- app/Http/Middleware/VerifyUserPassword.php
+++ app/Http/Middleware/VerifyUserPassword.php
@@ -2,6 +2,22 @@
 
 declare(strict_types=1);
 
+/*
+ * +----------------------------------------------------------------------+
+ * |                          ThinkSNS Plus                               |
+ * +----------------------------------------------------------------------+
+ * | Copyright (c) 2018 Chengdu ZhiYiChuangXiang Technology Co., Ltd.     |
+ * +----------------------------------------------------------------------+
+ * | This source file is subject to version 2.0 of the Apache license,    |
+ * | that is bundled with this package in the file LICENSE, and is        |
+ * | available through the world-wide-web at the following url:           |
+ * | http://www.apache.org/licenses/LICENSE-2.0.html                      |
+ * +----------------------------------------------------------------------+
+ * | Author: Slim Kit Group <master@zhiyicx.com>                          |
+ * | Homepage: www.thinksns.com                                           |
+ * +----------------------------------------------------------------------+
+ */
+
 namespace Zhiyi\Plus\Http\Middleware;
 
 use Closure;
@@ -27,7 +43,7 @@ class VerifyUserPassword
     {
         $this->auth = $auth;
     }
-    
+
     /**
      * The middleware handler.
      * @param \Illuminate\Http\Request $request
@@ -43,7 +59,7 @@ class VerifyUserPassword
         } elseif ($this->auth->getProvider()->validateCredentials(
             $this->auth->user(),
             [
-                'password' => $request->input('password')
+                'password' => $request->input('password'),
             ]
         )) {
             return $next($request);
