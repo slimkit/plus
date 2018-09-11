47f447cea057fb611858ba87602e3cf2e7ed1271
Seven Du <shiweidu@outlook.com>
2018-09-11 18:15:43 +0800

    chore: 测试 travis

--- scripts/publish-docs-gh-pages.sh
+++ scripts/publish-docs-gh-pages.sh
@@ -25,13 +25,13 @@ if [ "@$@" != "@bot" ]; then
     fi
 
     # Copy dist
-    cp -R "$WORKER_DIR/docs/.vuepress/dist/" "$GIT_DIR/"
+    cp -R "$WORKER_DIR/docs/.vuepress/dist/" "$GIT_DIR"
     git add .
     git commit -m "deploy"
     git push --set-upstream origin gh-pages
 else
-    cp -R "$WORKER_DIR/docs/.vuepress/dist/" "$GIT_DIR/"
-    ls "$GIT_DIR"
+    cp -R "$WORKER_DIR/docs/.vuepress/dist/" "$GIT_DIR"
+    ls -al "$GIT_DIR"
 fi
 
 cd -
