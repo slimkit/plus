ad5eff54ff47334a165bfb98557a895f59486d59
Seven Du <shiweidu@outlook.com>
2018-09-11 18:21:26 +0800

    chore: 完成所有自动构建！

--- scripts/publish-docs-gh-pages.sh
+++ scripts/publish-docs-gh-pages.sh
@@ -13,25 +13,21 @@ yarn docs:build
 mkdir "$GIT_DIR"
 cd "$GIT_DIR"
 
-if [ "@$@" != "@bot" ]; then
-    GITBRANCHDATA=$(curl https://api.github.com/repos/slimkit/plus/branches)
-    if echo "$GITBRANCHDATA" | grep -q "gh-pages"
-    then
-        git clone --branch=gh-pages https://github.com/slimkit/plus "$GIT_DIR"
-    else
-        git clone --branch=master https://github.com/slimkit/plus "$GIT_DIR"
-        git checkout --orphan gh-pages
-        git reset --hard
-    fi
-
-    # Copy dist
-    cp -R "$WORKER_DIR/docs/.vuepress/dist/" "$GIT_DIR"
-    git add .
-    git commit -m "deploy"
-    git push --set-upstream origin gh-pages
+GITBRANCHDATA=$(curl https://api.github.com/repos/slimkit/plus/branches)
+if echo "$GITBRANCHDATA" | grep -q "gh-pages"
+then
+    git clone --branch=gh-pages https://github.com/slimkit/plus "$GIT_DIR"
 else
-    cp -R "$WORKER_DIR/docs/.vuepress/dist/" "$GIT_DIR"
-    ls -al "$GIT_DIR"
+    git clone --branch=master https://github.com/slimkit/plus "$GIT_DIR"
+    git checkout --orphan gh-pages
+    git reset --hard
 fi
+
+# Copy dist
+cp -R "$WORKER_DIR/docs/.vuepress/dist/" "$GIT_DIR/"
+git add .
+git commit -m "deploy"
+
+git push --set-upstream origin gh-pages
 
 cd -
