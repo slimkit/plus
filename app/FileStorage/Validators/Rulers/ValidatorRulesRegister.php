7bf8bc7d57de7af23faa4e01a0dd10051fd0f892
Seven Du <shiweidu@outlook.com>
2018-09-12 07:05:16 +0000

    Apply fixes from StyleCI

--- app/FileStorage/Validators/Rulers/ValidatorRulesRegister.php
+++ app/FileStorage/Validators/Rulers/ValidatorRulesRegister.php
@@ -2,6 +2,22 @@
 
 declare(strict_types=1);
 
+/*
+ * +----------------------------------------------------------------------+
+ * |                          ThinkSNS Plus                               |
+ * +----------------------------------------------------------------------+
+ * | Copyright (c) 2018 Chengdu ZhiYiChuangXiang Technology Co., Ltd.     |
+ * +----------------------------------------------------------------------+
+ * | This source file is subject to version 2.0 of the Apache license,    |
+ * | that is bundled with this package in the file LICENSE, and is        |
+ * | available through the world-wide-web at the following url:           |
+ * | http://www.apache.org/licenses/LICENSE-2.0.html                      |
+ * +----------------------------------------------------------------------+
+ * | Author: Slim Kit Group <master@zhiyicx.com>                          |
+ * | Homepage: www.thinksns.com                                           |
+ * +----------------------------------------------------------------------+
+ */
+
 namespace Zhiyi\Plus\FileStorage\Validators\Rulers;
 
 use Zhiyi\Plus\AppInterface;
@@ -26,7 +42,7 @@ class ValidatorRegister
      * @var array
      */
     protected $rules = [
-        'file_storage'
+        'file_storage',
     ];
 
     /**
@@ -47,7 +63,7 @@ class ValidatorRegister
     public function register(): void
     {
         $app = $this->app;
-        foreach($this->rules as $ruleName => $rulerClassname) {
+        foreach ($this->rules as $ruleName => $rulerClassname) {
             $this->validator->extend($ruleName, function (...$params) use ($app, $rulerClassname): bool {
                 return $app->make($rulerClassname)->handle($params);
             });
