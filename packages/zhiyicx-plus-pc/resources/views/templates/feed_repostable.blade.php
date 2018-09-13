3cb8e1b66c28d21baf09babea662fe592439990f
mutoe <mutoe@foxmail.com>
2018-09-13 15:51:07 +0800

    fix(pc): 转发资讯的引用只显示了标题
    zhiyicx/plus-component-pc#906

--- packages/zhiyicx-plus-pc/resources/views/templates/feed_repostable.blade.php
+++ packages/zhiyicx-plus-pc/resources/views/templates/feed_repostable.blade.php
@@ -1,31 +1,45 @@
 {{-- 转发内容预览 --}}
-@if($feed['repostable'] && !$feed['repostable']['exception'])
+@php
+    use function Zhiyi\Component\ZhiyiPlus\PlusComponentPc\getTime;
+    $repostable = $feed['repostable'];
+@endphp
+
+@if($repostable && !$repostable['exception'])
     @switch($feed['repostable_type'])
 
         @case('news')
-        <a class="feed_repostable" href="{{ route('pc:newsread', ['news_id' => $feed['repostable_id']]) }}">
-            <p class="title">{{$feed['repostable']['title']}}</p>
-            <p class="description">{{$feed['repostable']['subject']}}</p>
+        <a class="feed_repostable news" href="{{ route('pc:newsread', ['news_id' => $feed['repostable_id']]) }}">
+            <div class="news-left">
+                <img src='{{ url("/api/v2/files/{$repostable['image']['id']}") }}' alt="">
+            </div>
+            <div class="news-right">
+                <p class="title">{{$repostable['title']}}</p>
+                <p class="description">{{$repostable['subject']}}</p>
+                <div class="info">
+                    <span class="category">{{ $repostable['category']['name'] }}</span>
+                    <span class="information">{{ $repostable['author'] }} · {{ $repostable['hits'] }}浏览 · {{ getTime($repostable['created_at']) }}</span>
+                </div>
+            </div>
         </a>
         @break
 
         @case('feeds')
         <a class="feed_repostable" href="{{ route('pc:feedread', ['feed' => $feed['repostable_id']]) }}">
-            <p class="description"><strong>{{$feed['repostable']['user']['name']}}: </strong>{{$feed['repostable']['feed_content']}}</p>
+            <p class="description"><strong>{{$repostable['user']['name']}}: </strong>{{$repostable['feed_content']}}</p>
         </a>
         @break
 
         @case('groups')
         <a class="feed_repostable" href="{{ route('pc:groupread', ['group_id' => $feed['repostable_id']]) }}">
-            <p class="description"><strong>{{$feed['repostable']['name']}}</strong></p>
-            <p class="description">{{$feed['repostable']['summary']}}</p>
+            <p class="description"><strong>{{$repostable['name']}}</strong></p>
+            <p class="description">{{$repostable['summary']}}</p>
         </a>
         @break
 
         @case('posts')
         <a class="feed_repostable" href="{{ route('pc:grouppost', ['group_id' => 1, 'post_id' => $feed['repostable_id']]) }}">
-            <p class="description"><strong>{{$feed['repostable']['user']['name']}}: {{$feed['repostable']['title']}}</strong></p>
-            <p class="description">{{$feed['repostable']['summary']}}</p>
+            <p class="description"><strong>{{$repostable['user']['name']}}: {{$repostable['title']}}</strong></p>
+            <p class="description">{{$repostable['summary']}}</p>
         </a>
         @break
 
@@ -35,7 +49,3 @@
         <p class="description">该内容已被删除</p>
     </span>
 @endif
-
-<script>
-console.log(@json($feed));
-</script>
