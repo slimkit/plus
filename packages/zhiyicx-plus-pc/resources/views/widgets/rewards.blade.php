e6fca630467449dd1a472484eacafe0422d5281e
hhhcode <missu082500@163.com>
2018-09-18 21:22:52 +0800

    fix: ( #958 )无打赏时不显示箭头

--- packages/zhiyicx-plus-pc/resources/views/widgets/rewards.blade.php
+++ packages/zhiyicx-plus-pc/resources/views/widgets/rewards.blade.php
@@ -10,7 +10,7 @@
         </p>
 
         {{-- 打賞 --}}
-        @if (!empty($rewards_data))
+        @if (!$rewards_data->isEmpty())
             <div class="reward_user">
                 @foreach ($rewards_data as $key => $reward)
                     @if ($key < 10)
