7cd54512aaccf0df04b9f8eb6d08101f9428493d
Seven Du <shiweidu@outlook.com>
2018-09-21 07:43:22 +0000

    Apply fixes from StyleCI

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -21,10 +21,10 @@ declare(strict_types=1);
 namespace Zhiyi\Plus\Providers;
 
 use Illuminate\Support\Facades\Event;
-use Zhiyi\Plus\Support\BootstrapAPIsEventer;
-use Illuminate\Contracts\Events\Dispatcher as EventsDispatcherContract;
 use Illuminate\Auth\Events\Registered;
+use Zhiyi\Plus\Support\BootstrapAPIsEventer;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
+use Illuminate\Contracts\Events\Dispatcher as EventsDispatcherContract;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
