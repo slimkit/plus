1efa5a2ac56d3ae216d07979b4f53f54f56c8e2c
SlimKit Bot (from Travis CI) <deploy@travis-ci.org>
2018-10-22 02:03:53 +0000

    Deploy slimkit/plus to github.com/slimkit/plus.git:gh-pages

--- assets/js/1.3c3b5234.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{145:function(n,w,o){}}]);
---
+++ assets/js/2.a2bd3099.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{145:function(n,w,o){}}]);
