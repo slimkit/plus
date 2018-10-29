783be12a0bd49d03e06c281baaa5e1881933291e
mutoe <mutoe@foxmail.com>
2018-10-29 18:04:02 +0800

    refactor(admin): 移除后台管理-plusid
    issue zhiyicx/plus-component-pc#1094

--- packages/slimkit-plus-id/resources/assets/components/pages/Client.vue
+++ packages/slimkit-plus-id/resources/assets/components/pages/Client.vue
@@ -2,17 +2,13 @@
   <div class="panel panel-default">
     <div class="panel-heading">
       客户端列表
-      <router-link :to="{ path: '/client-add' }" class="pull-right" replace>
+      <a href="javascript:;" class="pull-right">
         <span class="glyphicon glyphicon-plus"></span>
         添加
-      </router-link>
+      </a>
     </div>
 
-    <div class="panel-body" v-if="loadding">
-      <bootstrap-ui-kit:ui-loading></bootstrap-ui-kit:ui-loading>加载中...
-    </div>
-
-    <table v-else class="table table-striped table-hover">
+    <table class="table table-striped table-hover">
       <thead>
         <tr>
           <th>ID</th>
@@ -24,38 +20,20 @@
         </tr>
       </thead>
       <tbody>
-        <module-client-row v-for="client in clients" :key="client.id" :client="client"></module-client-row>
       </tbody>
     </table>
+
+    <p>开源版无此功能，需要使用此功能，请购买正版授权源码，详情访问www.thinksns.com，也可直接咨询：QQ3515923610；电话：18108035545。</p>
 
   </div>
 </template>
 
 <script>
-import { admin } from '../../axios';
-import ClientRow from '../modules/ClientRow';
 export default {
   name: 'client',
-  components: { [ClientRow.name]: ClientRow },
   data: () => ({
     clients: [],
     loadding: false,
   }),
-  methods: {
-    fetchClients () {
-      this.loadding = true;
-      admin.get('/clients', {
-        validateStatus: status => status === 200,
-      }).then(({ data: clients }) => {
-        this.loadding = false;
-        this.clients = clients;
-      }).catch(({ response: { data = { message: '加载失败...' } } = {} }) => {
-        this.$store.dispatch('alert-open', { type: 'danger', message: data });
-      });
-    }
-  },
-  created () {
-    this.fetchClients();
-  }
 };
 </script>
