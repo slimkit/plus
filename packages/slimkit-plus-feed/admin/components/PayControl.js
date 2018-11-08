b2d8db44c5a120bf55933633794afd853347c36a
mutoe <mutoe@foxmail.com>
2018-11-08 16:37:26 +0800

    fix(pc): 管理后台动态模块 jsx 语法错误的问题
    issue zhiyicx/plus-component-pc#1109

--- packages/slimkit-plus-feed/admin/components/PayControl.js
+++ packages/slimkit-plus-feed/admin/components/PayControl.js
@@ -39,7 +39,7 @@ class PayControl extends Component
 
   render () {
     return (
-      <div style="padding: 20px;">
+      <div style={{padding: '20px'}}>
         <p>开源版无此功能，需要使用此功能，请购买正版授权源码，详情访问www.thinksns.com，也可直接咨询：QQ3515923610；电话：18108035545。</p>
       </div>
     );
