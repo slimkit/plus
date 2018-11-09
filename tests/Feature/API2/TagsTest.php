b641c6583c2dc6f27d7f6391decc817575aaf557
Seven Du <shiweidu@outlook.com>
2018-11-09 14:49:10 +0800

    test(Core): 删除错误的测试内容
    看着代码明明不是我写的，注释确实我的名字真的是心累。

--- tests/Feature/API2/TagsTest.php
+++ tests/Feature/API2/TagsTest.php
@@ -28,20 +28,6 @@ class TagsTest extends TestCase
     use DatabaseTransactions;
 
     /**
-     * The test set up.
-     *
-     * @author Seven Du <shiweidu@outlook.com>
-     */
-    protected function setUp()
-    {
-        parent::setUp();
-
-        app(TagCategoryModel::class)->insert(['id' => '1', 'name' => '测试分类']);
-        app(TagModel::class)->insert(['name' => '标签1', 'tag_category_id' => 1]);
-        app(TagModel::class)->insert(['name' => '标签2', 'tag_category_id' => 1]);
-    }
-
-    /**
      * 测试获取标签.
      *
      * @return void
