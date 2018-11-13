2f2dc3246d0d6b7639efdcf359607f68f9bf685f
Seven Du <shiweidu@outlook.com>
2018-11-13 15:47:15 +0800

    refactor(Core): 动态发布权限默认安装的时候填充搭配创始人和普通用户角色

--- packages/slimkit-plus-feed/database/seeds/AbilityTableSeeder.php
+++ packages/slimkit-plus-feed/database/seeds/AbilityTableSeeder.php
@@ -20,6 +20,7 @@ declare(strict_types=1);
 
 namespace SlimKit\Plus\Packages\Feed\Seeds;
 
+use Zhiyi\Plus\Models\Role;
 use Zhiyi\Plus\Models\Ability;
 use Illuminate\Database\Seeder;
 
@@ -33,46 +34,13 @@ class AbilityTableSeeder extends Seeder
      */
     public function run()
     {
-        foreach ($this->perms() as $name => $items) {
-            $this->validateOr($name, function (Ability $perm) use ($items) {
-                foreach ($items as $key => $value) {
-                    $perm->$key = $value;
-                }
-                $perm->save();
-            });
-        }
-    }
+        $ability = new Ability();
+        $ability->name = 'feed-post';
+        $ability->display_name = '发送分享';
+        $ability->description = '用户发送分享权限';
+        $ability->save();
 
-    /**
-     * 验证是否存在，不存在则执行回调.
-     *
-     * @param string $name
-     * @param callable $call
-     * @return void
-     * @author Seven Du <shiweidu@outlook.com>
-     */
-    protected function validateOr(string $name, callable $call)
-    {
-        Ability::where('name', $name)->firstOr(function () use ($name, $call) {
-            $perm = new Ability();
-            $perm->name = $name;
-            call_user_func_array($call, [$perm]);
-        });
-    }
-
-    /**
-     * Get all perms.
-     *
-     * @return array
-     * @author Seven Du <shiweidu@outlook.com>
-     */
-    protected function perms(): array
-    {
-        return [
-            'feed-post' => [
-                'display_name' => '发送分享',
-                'description' => '用户发送分享权限',
-            ],
-        ];
+        $roles = Role::whereIn('name', ['founder', 'owner'])->get();
+        $roles->each->abilities()->syncWithoutDetaching([$ability]);
     }
 }
