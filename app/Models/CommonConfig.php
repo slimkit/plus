16d0ae9a7277a5cceaa06054c60a92c0e8a97f7e
Seven Du <shiweidu@outlook.com>
2018-11-29 17:27:20 +0800

    fix(后台): 修复老的公用配置查询 key 出错
    #458

--- app/Models/CommonConfig.php
+++ app/Models/CommonConfig.php
@@ -60,19 +60,4 @@ class CommonConfig extends Model
     {
         return $query->where('name', $name);
     }
-
-    /**
-     * Set the keys for a save update query.
-     *
-     * @param  \Illuminate\Database\Eloquent\Builder  $query
-     * @return \Illuminate\Database\Eloquent\Builder
-     */
-    protected function setKeysForSaveQuery(Builder $query)
-    {
-        foreach ($this->getKeyName() as $key) {
-            $query->where($key, '=', $this->original[$key] ?? $this->getAttribute($key));
-        }
-
-        return $query;
-    }
 }
