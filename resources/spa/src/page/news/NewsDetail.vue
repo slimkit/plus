f7611472cc0d69422caf92cbcc514fd6ad1d7a8f
mutoe <mutoe@foxmail.com>
2018-12-04 18:14:57 +0800

    fix(SPA): 后台关闭打赏开关后前端仍然后打赏的问题
    issue slimkit/plus#475

--- resources/spa/src/page/news/NewsDetail.vue
+++ resources/spa/src/page/news/NewsDetail.vue
@@ -47,7 +47,7 @@
             <span>{{ news.hits || 0 | formatNum }}浏览</span>
           </div>
         </div>
-        <div class="m-box-model m-box-center m-box-center-a m-art-reward">
+        <div v-if="allowReward" class="m-box-model m-box-center m-box-center-a m-art-reward">
           <button class="m-art-rew-btn" @click="rewardNews">打 赏</button>
           <p class="m-art-rew-label">
             <a href="javascript:;">{{ reward.count | formatNum }}</a>人打赏，共
@@ -180,6 +180,9 @@ export default {
   },
   computed: {
     ...mapState(['CURRENTUSER']),
+    allowReward () {
+      return this.$store.state.CONFIG.site.reward.status
+    },
     firstImage () {
       let images = this.news.image
       if (!Object.keys(images).length) {
