d127005b750b8bb96f8933bd5af5aace92c708d3
Seven Du <shiweidu@outlook.com>
2018-12-07 02:11:04 +0000

    Apply fixes from StyleCI

--- guide/dev/blog/codes/routes/web.php
+++ guide/dev/blog/codes/routes/web.php
@@ -1,8 +1,24 @@
 <?php
 
+/*
+ * +----------------------------------------------------------------------+
+ * |                          ThinkSNS Plus                               |
+ * +----------------------------------------------------------------------+
+ * | Copyright (c) 2018 Chengdu ZhiYiChuangXiang Technology Co., Ltd.     |
+ * +----------------------------------------------------------------------+
+ * | This source file is subject to version 2.0 of the Apache license,    |
+ * | that is bundled with this package in the file LICENSE, and is        |
+ * | available through the world-wide-web at the following url:           |
+ * | http://www.apache.org/licenses/LICENSE-2.0.html                      |
+ * +----------------------------------------------------------------------+
+ * | Author: Slim Kit Group <master@zhiyicx.com>                          |
+ * | Homepage: www.thinksns.com                                           |
+ * +----------------------------------------------------------------------+
+ */
+
+use Illuminate\Contracts\Routing\Registrar as RouteRegisterContract;
 use Illuminate\Support\Facades\Route;
 use SlimKit\Plus\Packages\Blog\Web\Controllers as Web;
-use Illuminate\Contracts\Routing\Registrar as RouteRegisterContract;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,12 +33,11 @@ use Illuminate\Contracts\Routing\Registrar as RouteRegisterContract;
 
 Route::get('/', Web\HomeController::class.'@index');
 Route::group(['prefix' => 'blogs'], function (RouteRegisterContract $route) {
-    
+
     // 我的博客
     $route
         ->get('me', Web\HomeController::class.'@me')
-        ->name('blog:me')
-    ;
+        ->name('blog:me');
     $route->post('me', Web\HomeController::class.'@createBlog');
 
     // 博客主页
--- guide/dev/blog/codes/src/Providers/RouteServiceProvider.php
+++ guide/dev/blog/codes/src/Providers/RouteServiceProvider.php
@@ -2,6 +2,22 @@
 
 declare(strict_types=1);
 
+/*
+ * +----------------------------------------------------------------------+
+ * |                          ThinkSNS Plus                               |
+ * +----------------------------------------------------------------------+
+ * | Copyright (c) 2018 Chengdu ZhiYiChuangXiang Technology Co., Ltd.     |
+ * +----------------------------------------------------------------------+
+ * | This source file is subject to version 2.0 of the Apache license,    |
+ * | that is bundled with this package in the file LICENSE, and is        |
+ * | available through the world-wide-web at the following url:           |
+ * | http://www.apache.org/licenses/LICENSE-2.0.html                      |
+ * +----------------------------------------------------------------------+
+ * | Author: Slim Kit Group <master@zhiyicx.com>                          |
+ * | Homepage: www.thinksns.com                                           |
+ * +----------------------------------------------------------------------+
+ */
+
 namespace SlimKit\Plus\Packages\Blog\Providers;
 
 use Illuminate\Support\ServiceProvider;
@@ -31,7 +47,7 @@ class RouteServiceProvider extends ServiceProvider
         // Publish admin menu.
         $this->app->make(ManageRepository::class)->loadManageFrom('plus-blog', 'plus-blog:admin-home', [
             'route' => true,
-            'icon' => '📦',
+            'icon'  => '📦',
         ]);
     }
 }
