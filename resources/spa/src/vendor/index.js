2da1bae557a5975381efde03a5d2980289ad539d
mutoe <mutoe@foxmail.com>
2018-12-17 11:22:49 +0800

    chore(SPA): 修复高德地图 SDK 需要同步加载

--- resources/spa/src/vendor/index.js
+++ resources/spa/src/vendor/index.js
@@ -18,11 +18,9 @@ if (ENABLE_MOBLINK) {
 
 // 加载高德地图脚本 用于定位
 if (ENABLE_GAODE) {
-  window.addEventListener('load', () => {
-    const key = process.env.VUE_APP_LBS_GAODE_KEY || ''
-    const script = document.createElement('script')
-    script.type = 'text/javascript'
-    script.src = `//webapi.amap.com/maps?v=1.4.5&key=${key}`
-    document.querySelector('body').appendChild(script)
-  })
+  const key = process.env.VUE_APP_LBS_GAODE_KEY || ''
+  const script = document.createElement('script')
+  script.type = 'text/javascript'
+  script.src = `//webapi.amap.com/maps?v=1.4.5&key=${key}`
+  document.querySelector('body').appendChild(script)
 }
