9b975fa787550bd62915277b292996cf5e4c26b5
mutoe <mutoe@foxmail.com>
2018-12-19 17:29:02 +0800

    refactor(SPA): 优化通用搜索框的搜索体验

--- resources/spa/src/components/common/SearchBar.vue
+++ resources/spa/src/components/common/SearchBar.vue
@@ -9,7 +9,7 @@
         :value="value"
         :placeholder="placeholder"
         type="search"
-        @input="$emit('input', $event.target.value)"
+        @input="$emit('input', $event.target.value.trim())"
       >
     </form>
 
