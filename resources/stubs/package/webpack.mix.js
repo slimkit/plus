ff86b0f94d80afd56ed005666ed583fdfebe02ce
Seven Du <shiweidu@outlook.com>
2018-12-25 10:38:29 +0800

    refactor(Package): 重构拓展包模板

--- resources/stubs/package/webpack.mix.js
+++ resources/stubs/package/webpack.mix.js
@@ -1,4 +1,5 @@
 let mix = require('laravel-mix');
+let fs = require('fs');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,9 +12,21 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.setPublicPath('assets');
+mix.disableNotifications();
+mix.sourceMaps(!mix.inProduction());
 mix.setResourceRoot('/assets/{name}/');
-mix.sourceMaps(! mix.inProduction());
 
 // More documents see: https://laravel.com/docs/master/mix
-mix.js('resources/assets/main.js', 'assets/app.js');
+if (mix.inProduction()) {
+  mix.setPublicPath('assets');
+  mix.js('resources/assets/main.js', 'assets/app.js');
+  
+// Dev build.
+} else {
+  mix.setPublicPath('../../public/assets/{name}/');
+  if (mix.config.hmr === true) {
+    mix.setResourceRoot('http://127.0.0.1:8080/');
+  }
+  mix.js('resources/assets/main.js', '../../public/assets/{name}/app.js');
+}
+
