9430f5c0a9b2732d447b9ad3a4be55c0b5e222bd
mutoe <mutoe@foxmail.com>
2019-02-28 11:29:27 +0800

    fix: 转发时at用户没有反应的问题

--- packages/zhiyicx-plus-pc/resources/assets/web/js/common.js
+++ packages/zhiyicx-plus-pc/resources/assets/web/js/common.js
@@ -2105,7 +2105,7 @@ $(function() {
       var name = $(this).data('user-name')
       $el = $('.ev-ipt-repostable-content');
 
-      $el.val($el.val() + "@" + name + " ")
+      $el.html($el.html() + "@" + name + " ")
       repostable.showMention(false);
     })
 
