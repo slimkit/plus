df5845428afbbb829c8304e9adc0aad6a2d9841c
Seven Du <shiweidu@outlook.com>
2019-03-07 15:04:24 +0800

    chore(*): generate 2.3 branch version

--- app/Application.php
+++ app/Application.php
@@ -29,7 +29,7 @@ class Application extends LaravelApplication implements AppInterface
      *
      * @var string
      */
-    const VERSION = '2.2.3';
+    const VERSION = '2.3.0';
 
     /**
      * Create a new Illuminate application instance.
