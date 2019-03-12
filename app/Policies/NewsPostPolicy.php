1f01ec42c8301ee39ad666d7ab7ba46719beb1fc
Seven Du <shiweidu@outlook.com>
2019-03-12 07:02:51 +0000

    Apply fixes from StyleCI

--- app/Policies/NewsPostPolicy.php
+++ app/Policies/NewsPostPolicy.php
@@ -1,10 +1,26 @@
 <?php
 
+/*
+ * +----------------------------------------------------------------------+
+ * |                          ThinkSNS Plus                               |
+ * +----------------------------------------------------------------------+
+ * | Copyright (c) 2016-Present ZhiYiChuangXiang Technology Co., Ltd.     |
+ * +----------------------------------------------------------------------+
+ * | This source file is subject to enterprise private license, that is   |
+ * | bundled with this package in the file LICENSE, and is available      |
+ * | through the world-wide-web at the following url:                     |
+ * | https://github.com/slimkit/plus/blob/master/LICENSE                  |
+ * +----------------------------------------------------------------------+
+ * | Author: Slim Kit Group <master@zhiyicx.com>                          |
+ * | Homepage: www.thinksns.com                                           |
+ * +----------------------------------------------------------------------+
+ */
+
 namespace Zhiyi\Plus\Policies;
 
 use Zhiyi\Plus\Models\User;
-use Zhiyi\Plus\Zhiyi\Component\ZhiyiPlus\PlusComponentNews\Models\News;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Zhiyi\Plus\Zhiyi\Component\ZhiyiPlus\PlusComponentNews\Models\News;
 
 class NewsPostPolicy
 {
