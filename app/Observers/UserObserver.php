2c04d5039a1e626cd523cb3481d6d0d54ba97b4e
boxshadow <idafoo@sina.com>
2019-06-18 11:51:46 +0800

    fix: 增加用户注册成功后的关注内容 #699

---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * +----------------------------------------------------------------------+
+ * |                          ThinkSNS Plus                               |
+ * +----------------------------------------------------------------------+
+ * | Copyright (c) 2016-Present ZhiYiChuangXiang Technology Co., Ltd.     |
+ * +----------------------------------------------------------------------+
+ * | This source file is subject to enterprise private license, that is   |
+ * | bundled with this package in the file LICENSE, and is available      |
+ * | through the world-wide-web at the following url:                     |
+ * | https://github.com/slimkit/plus/blob/master/LICENSE                  |
+ * +----------------------------------------------------------------------+
+ * | Author: Slim Kit Group <master@zhiyicx.com>                          |
+ * | Homepage: www.thinksns.com                                           |
+ * +----------------------------------------------------------------------+
+ */
+
+namespace Zhiyi\Plus\Observers;
+
+use Zhiyi\Plus\Models\User;
+use Zhiyi\Plus\Models\Famous;
+
+class UserObserver
+{
+    /**
+     * Handle the user "created" event.
+     *
+     * @param  User  $user
+     *
+     * @return void
+     */
+    public function created(User $user)
+    {
+        // 处理默认关注和默认相互关注
+        Famous::query()->with('user')->get()->groupBy('type')
+            ->map(function ($type, $key) use ($user) {
+                $users = $type->pluck('user');
+                $user->followings()->attach($users->pluck('id'));
+                // 相互关注
+                if ($key === 'each') {
+                    $users->map(function ($source) use ($user) {
+                        $source->followings()->attach($user);
+                    });
+                }
+            });
+    }
+}
