052819f51f288f62879689e3ffc69155d0b95b52
boxshadow <idafoo@sina.com>
2019-07-04 11:48:04 +0800

    add: 积分类型增加2个静态方法,勇于缓存配置
    减少硬件资源开销

--- app/Models/CurrencyType.php
+++ app/Models/CurrencyType.php
@@ -21,6 +21,8 @@ declare(strict_types=1);
 namespace Zhiyi\Plus\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cache;
+use Zhiyi\Plus\CacheNames;
 
 class CurrencyType extends Model
 {
@@ -30,4 +32,41 @@ class CurrencyType extends Model
      * @var bool
      */
     public $timestamps = false;
+
+    /**
+     * 获取当前使用的积分名称
+     *
+     * @param  null  $field
+     *
+     * @return mixed
+     */
+    public static function current($field = null)
+    {
+        $gold = Cache::rememberForever(CacheNames::CURRENCY_NAME, function
+        () {
+            $current =  self::query()->where('enable', 1)
+                ->select('id', 'name', 'unit')
+                ->first();
+
+            return collect($current ?
+                $current->toArray() :
+                ['id' => 999, 'name' => '金币', 'unit'=> '个']);
+        });
+
+        return $field ? $gold->get($field) : $gold;
+    }
+
+    /**
+     * 设置当前使用的积分名称
+     *
+     * @param  string  $name
+     * @param  string  $unit
+     */
+    public static function setCurrent(string $name, string $unit)
+    {
+        Cache::forever(CacheNames::CURRENCY_NAME, collect([
+            'name' => $name,
+            'unit' => $unit
+        ]));
+    }
 }
