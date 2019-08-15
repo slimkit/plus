b2eb75f09df6b7aa71ca9305507385c43d0abecd
boxshadow <idafoo@sina.com>
2019-08-15 18:44:55 +0800

    fix: 修复语法错误

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -34,7 +34,6 @@ use Faker\Generator as Faker;
 
 $factory->define(User::class, function (Faker $faker) {
     static $password;
-}
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
